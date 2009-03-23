5ed14c9009898c31e5d8063901ab7f40fb4842dc
San Mehat <san@android.com>
2009-03-23 12:20:37 -0700

    mmc: core: Hold a wake lock accross delayed work + mmc rescan
    Signed-off-by: San Mehat <san@android.com>
    
    mmc: core: Rework mmc_delayed_work wakelock so that the wakelock is only extended if a card is added or removed.
    
    Signed-off-by: San Mehat <san@google.com>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -22,6 +22,7 @@
 #include <linux/scatterlist.h>
 #include <linux/log2.h>
 #include <linux/regulator/consumer.h>
+#include <linux/wakelock.h>
 
 #include <linux/mmc/card.h>
 #include <linux/mmc/host.h>
@@ -38,6 +39,7 @@
 #include "sdio_ops.h"
 
 static struct workqueue_struct *workqueue;
+static struct wake_lock mmc_delayed_work_wake_lock;
 
 /*
  * Enabling software CRCs on the data blocks can be a significant (30%)
@@ -70,6 +72,7 @@ MODULE_PARM_DESC(
 static int mmc_schedule_delayed_work(struct delayed_work *work,
 				     unsigned long delay)
 {
+	wake_lock(&mmc_delayed_work_wake_lock);
 	return queue_delayed_work(workqueue, work, delay);
 }
 
@@ -1433,6 +1436,7 @@ void mmc_rescan(struct work_struct *work)
 	unsigned long flags;
 	int i;
 	const unsigned freqs[] = { 400000, 300000, 200000, 100000 };
+	int extend_wakelock = 0;
 
 	spin_lock_irqsave(&host->lock, flags);
 
@@ -1509,6 +1513,7 @@ void mmc_rescan(struct work_struct *work)
 
 				if (mmc_attach_sd(host, ocr))
 					mmc_power_off(host);
+				extend_wakelock = 1;
 			}
 			goto out;
 		}
@@ -1520,6 +1525,7 @@ void mmc_rescan(struct work_struct *work)
 		if (!err) {
 			if (mmc_attach_sd(host, ocr))
 				mmc_power_off(host);
+			extend_wakelock = 1;
 			goto out;
 		}
 
@@ -1530,6 +1536,7 @@ void mmc_rescan(struct work_struct *work)
 		if (!err) {
 			if (mmc_attach_mmc(host, ocr))
 				mmc_power_off(host);
+			extend_wakelock = 1;
 			goto out;
 		}
 
@@ -1538,6 +1545,11 @@ out_fail:
 		mmc_power_off(host);
 	}
 out:
+	if (extend_wakelock)
+		wake_lock_timeout(&mmc_delayed_work_wake_lock, HZ / 2);
+	else
+		wake_unlock(&mmc_delayed_work_wake_lock);
+
 	if (host->caps & MMC_CAP_NEEDS_POLL)
 		mmc_schedule_delayed_work(&host->detect, HZ);
 }
@@ -1806,6 +1818,8 @@ static int __init mmc_init(void)
 {
 	int ret;
 
+	wake_lock_init(&mmc_delayed_work_wake_lock, WAKE_LOCK_SUSPEND, "mmc_delayed_work");
+
 	workqueue = create_singlethread_workqueue("kmmcd");
 	if (!workqueue)
 		return -ENOMEM;
@@ -1840,6 +1854,7 @@ static void __exit mmc_exit(void)
 	mmc_unregister_host_class();
 	mmc_unregister_bus();
 	destroy_workqueue(workqueue);
+	wake_lock_destroy(&mmc_delayed_work_wake_lock);
 }
 
 subsys_initcall(mmc_init);
