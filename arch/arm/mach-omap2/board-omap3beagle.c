19fe60d2d723c403620caaf297956a3303c5d155
Ricardo Salveti de Araujo <ricardo.salveti@canonical.com>
2011-01-28 02:10:37 -0200

    omap3: beaglexm: fix DVI initialization
    Function beagle_twl_gpio_setup is called after beagle_display_init, what
    makes lets reset_gpio with an invalid value at the time it request the
    gpio. As a side effect the DVI reset GPIO is not properly set.
    
    Also removing old code that power down DVI in a hardcoded way, as it's
    not necessary anymore.
    
    Tested with Beagle-xM and C4.
    
    Signed-off-by: Ricardo Salveti de Araujo <ricardo.salveti@canonical.com>

--- arch/arm/mach-omap2/board-omap3beagle.c
+++ arch/arm/mach-omap2/board-omap3beagle.c
@@ -244,6 +244,12 @@ static void __init beagle_display_init(void)
 {
 	int r;
 
+	/* DVI reset GPIO is different between beagle revisions */
+	if (omap3_beagle_get_rev() == OMAP3BEAGLE_BOARD_XM)
+		beagle_dvi_device.reset_gpio = 129;
+	else
+		beagle_dvi_device.reset_gpio = 170;
+
 	r = gpio_request(beagle_dvi_device.reset_gpio, "DVI reset");
 	if (r < 0) {
 		printk(KERN_ERR "Unable to get DVI reset GPIO\n");
@@ -319,12 +325,6 @@ static int beagle_twl_gpio_setup(struct device *dev,
 		gpio_direction_output(gpio + TWL4030_GPIO_MAX, 1);
 	else
 		gpio_direction_output(gpio + TWL4030_GPIO_MAX, 0);
-
-	/* DVI reset GPIO is different between beagle revisions */
-	if (omap3_beagle_get_rev() == OMAP3BEAGLE_BOARD_XM)
-		beagle_dvi_device.reset_gpio = 129;
-	else
-		beagle_dvi_device.reset_gpio = 170;
 
 	/* TWL4030_GPIO_MAX + 1 == ledB, PMU_STAT (out, active low LED) */
 	gpio_leds[2].gpio = gpio + TWL4030_GPIO_MAX + 1;
@@ -664,11 +664,6 @@ static void __init omap3_beagle_init(void)
 			ARRAY_SIZE(omap3_beagle_devices));
 	omap_display_init(&beagle_dss_data);
 	omap_serial_init();
-
-	omap_mux_init_gpio(170, OMAP_PIN_INPUT);
-	gpio_request(170, "DVI_nPD");
-	/* REVISIT leave DVI powered down until it's needed ... */
-	gpio_direction_output(170, true);
 
 	usb_musb_init(&musb_board_data);
 	usbhs_init(&usbhs_bdata);
