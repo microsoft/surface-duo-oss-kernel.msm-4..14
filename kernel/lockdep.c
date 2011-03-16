aa619d07067ee4fe4891f166fe25adb0d3dfdb8d
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2011-03-16 19:04:38 -0400

    lttng-instrumentation/lttng-instrumentation-lockdep
    LTTng instrumentation lockdep tracepoints
    
    lockdep_hardirqs_off
    lockdep_hardirqs_on
    lockdep_lock_acquire
    lockdep_lock_release
    lockdep_softirqs_off
    lockdep_softirqs_on
    
    Update for 2.6.37 to remove dependency on immediate values and play nicely with
    trace event lockdep instrumentation.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>

--- kernel/lockdep.c
+++ kernel/lockdep.c
@@ -66,6 +66,13 @@ module_param(lock_stat, int, 0644);
 #define lock_stat 0
 #endif
 
+DEFINE_TRACE(lockdep_hardirqs_on);
+DEFINE_TRACE(lockdep_hardirqs_off);
+DEFINE_TRACE(lockdep_softirqs_on);
+DEFINE_TRACE(lockdep_softirqs_off);
+DEFINE_TRACE(lockdep_lock_acquire);
+DEFINE_TRACE(lockdep_lock_release);
+
 /*
  * lockdep_lock: protects the lockdep graph, the hashes and the
  *               class/list/hash allocators.
@@ -2300,6 +2307,8 @@ void trace_hardirqs_on_caller(unsigned long ip)
 
 	time_hardirqs_on(CALLER_ADDR0, ip);
 
+	trace_lockdep_hardirqs_on(ip);
+
 	if (unlikely(!debug_locks || current->lockdep_recursion))
 		return;
 
@@ -2358,6 +2367,8 @@ void trace_hardirqs_off_caller(unsigned long ip)
 
 	time_hardirqs_off(CALLER_ADDR0, ip);
 
+	trace_lockdep_hardirqs_off(ip);
+
 	if (unlikely(!debug_locks || current->lockdep_recursion))
 		return;
 
@@ -2389,6 +2400,8 @@ EXPORT_SYMBOL(trace_hardirqs_off);
 void trace_softirqs_on(unsigned long ip)
 {
 	struct task_struct *curr = current;
+
+	trace_lockdep_softirqs_on(ip);
 
 	if (unlikely(!debug_locks))
 		return;
@@ -2446,6 +2459,8 @@ void trace_softirqs_off(unsigned long ip)
 static void __lockdep_trace_alloc(gfp_t gfp_mask, unsigned long flags)
 {
 	struct task_struct *curr = current;
+
+	trace_lockdep_softirqs_off(ip);
 
 	if (unlikely(!debug_locks))
 		return;
@@ -2729,6 +2744,9 @@ static int __lock_acquire(struct lockdep_map *lock, unsigned int subclass,
 	int chain_head = 0;
 	int class_idx;
 	u64 chain_key;
+
+	trace_lockdep_lock_acquire(ip, subclass, lock, trylock, read,
+		hardirqs_off);
 
 	if (!prove_locking)
 		check = 1;
@@ -3107,6 +3125,8 @@ static void
 __lock_release(struct lockdep_map *lock, int nested, unsigned long ip)
 {
 	struct task_struct *curr = current;
+
+	trace_lockdep_lock_release(ip, lock, nested);
 
 	if (!check_unlock(curr, lock, ip))
 		return;
