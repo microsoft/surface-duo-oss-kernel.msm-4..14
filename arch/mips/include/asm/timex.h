231e852aa20b7f5a98c5df3ddf752f8ea69f8138
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2011-03-16 19:04:14 -0400

    trace-clock/get-cycles-mips-have-get-cycles
    get_cycles() : MIPS HAVE_GET_CYCLES_32
    
    partly reverts commit efb9ca08b5a2374b29938cdcab417ce4feb14b54. Selects
    HAVE_GET_CYCLES_32 only on CPUs where it is safe to use it.
    
    Currently consider the "_WORKAROUND" cases for 4000 and 4400 to be unsafe, but
    should probably add other sub-architecture to the blacklist.
    
    Do not define HAVE_GET_CYCLES because MIPS does not provide 64-bit tsc (only
    32-bits).
    
    get_cycles_rate() now uses mips_hpt_frequency.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>
    CC: Ralf Baechle <ralf@linux-mips.org>
    CC: David Miller <davem@davemloft.net>
    CC: Linus Torvalds <torvalds@linux-foundation.org>
    CC: Andrew Morton <akpm@linux-foundation.org>
    CC: Ingo Molnar <mingo@redhat.com>
    CC: Peter Zijlstra <a.p.zijlstra@chello.nl>
    CC: Thomas Gleixner <tglx@linutronix.de>
    CC: Steven Rostedt <rostedt@goodmis.org>
    CC: linux-arch@vger.kernel.org

--- arch/mips/include/asm/timex.h
+++ arch/mips/include/asm/timex.h
@@ -31,14 +31,39 @@ extern unsigned int mips_hpt_frequency;
  * which isn't an evil thing.
  *
  * We know that all SMP capable CPUs have cycle counters.
+ *
+ * Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>
+ * HAVE_GET_CYCLES makes sure that this case is handled properly :
+ *
+ * Ralf Baechle <ralf@linux-mips.org> :
+ * This avoids us executing an mfc0 c0_count instruction on processors which
+ * don't have but also on certain R4000 and R4400 versions where reading from
+ * the count register just in the very moment when its value equals c0_compare
+ * will result in the timer interrupt getting lost.
  */
 
 typedef unsigned int cycles_t;
 
+#ifdef HAVE_GET_CYCLES_32
+static inline cycles_t get_cycles(void)
+{
+	return read_c0_count();
+}
+
+static inline void get_cycles_barrier(void)
+{
+}
+
+static inline cycles_t get_cycles_rate(void)
+{
+	return mips_hpt_frequency;
+}
+#else
 static inline cycles_t get_cycles(void)
 {
 	return 0;
 }
+#endif
 
 #endif /* __KERNEL__ */
 
