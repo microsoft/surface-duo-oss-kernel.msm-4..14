f199490b85172bd24933b75adf86598b6e858ab5
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2011-03-16 19:04:23 -0400

    splice-support-modules
    Splice and pipe : export pipe buf operations for GPL modules
    
    LTTng splice transport use the generic pipe and splice operations from a GPL
    module.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>
    CC: Jens Axboe <axboe@kernel.dk>
    CC: Linus Torvalds <torvalds@osdl.org>
    CC: Ingo Molnar <mingo@elte.hu>

--- fs/splice.c
+++ fs/splice.c
@@ -259,6 +259,7 @@ ssize_t splice_to_pipe(struct pipe_inode_info *pipe,
 
 	return ret;
 }
+EXPORT_SYMBOL_GPL(splice_to_pipe);
 
 static void spd_release_page(struct splice_pipe_desc *spd, unsigned int i)
 {
