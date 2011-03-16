710fff2054cc7838d7e98714e4a78610a52547a2
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2011-03-16 19:04:33 -0400

    lttng-instrumentation/lttng-instrumentation-filemap
    LTTng instrumentation - filemap
    
    Instrumentation of waits caused by memory accesses on mmap regions.
    
    Those tracepoints are used by LTTng.
    
    About the performance impact of tracepoints (which is comparable to markers),
    even without immediate values optimizations, tests done by Hideo Aoki on ia64
    show no regression. His test case was using hackbench on a kernel where
    scheduler instrumentation (about 5 events in code scheduler code) was added.
    See the "Tracepoints" patch header for performance result detail.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>
    CC: linux-mm@kvack.org
    CC: Dave Hansen <haveblue@us.ibm.com>
    CC: Masami Hiramatsu <mhiramat@redhat.com>
    CC: 'Peter Zijlstra' <peterz@infradead.org>
    CC: "Frank Ch. Eigler" <fche@redhat.com>
    CC: 'Ingo Molnar' <mingo@elte.hu>
    CC: Frederic Weisbecker <fweisbec@gmail.com>
    CC: 'Hideo AOKI' <haoki@redhat.com>
    CC: Takashi Nishiie <t-nishiie@np.css.fujitsu.com>
    CC: 'Steven Rostedt' <rostedt@goodmis.org>
    CC: Eduard - Gabriel Munteanu <eduard.munteanu@linux360.ro>

--- mm/filemap.c
+++ mm/filemap.c
@@ -34,6 +34,7 @@
 #include <linux/hardirq.h> /* for BUG_ON(!in_atomic()) only */
 #include <linux/memcontrol.h>
 #include <linux/mm_inline.h> /* for page_is_file_cache() */
+#include <trace/filemap.h>
 #include "internal.h"
 
 /*
@@ -42,6 +43,9 @@
 #include <linux/buffer_head.h> /* for try_to_free_buffers */
 
 #include <asm/mman.h>
+
+DEFINE_TRACE(wait_on_page_start);
+DEFINE_TRACE(wait_on_page_end);
 
 /*
  * Shared mappings implemented 30.11.1994. It's not fully working yet,
@@ -511,9 +515,11 @@ void wait_on_page_bit(struct page *page, int bit_nr)
 {
 	DEFINE_WAIT_BIT(wait, &page->flags, bit_nr);
 
+	trace_wait_on_page_start(page, bit_nr);
 	if (test_bit(bit_nr, &page->flags))
 		__wait_on_bit(page_waitqueue(page), &wait, sync_page,
 							TASK_UNINTERRUPTIBLE);
+	trace_wait_on_page_end(page, bit_nr);
 }
 EXPORT_SYMBOL(wait_on_page_bit);
 
