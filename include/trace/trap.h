a1f04a4097be932c04d3a2d57698f92242136c18
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2011-03-16 19:04:56 -0400

    lttng-instrumentation/lttng-instrumentation-traps
    LTTng instrumentation trap
    
    Trap and fault instrumentation tracepoints.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>

---
+++ include/trace/trap.h
@@ -0,0 +1,11 @@
+#ifndef _TRACE_TRAP_H
+#define _TRACE_TRAP_H
+
+#include <linux/tracepoint.h>
+
+DECLARE_TRACE(trap_entry,
+	TP_PROTO(struct pt_regs *regs, long id),
+	TP_ARGS(regs, id));
+DECLARE_TRACE_NOARGS(trap_exit);
+
+#endif
