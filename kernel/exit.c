cf847b095c9d76cf6ae79f1f381add61b64a7a1d
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2011-03-16 19:05:03 -0400

    lttng-statedump/export-get-files-struct-symbol
    Export get_files_struct symbol
    
    Export put_files_struct and get_files_struct, used by ltt-statedump module.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>

--- kernel/exit.c
+++ kernel/exit.c
@@ -514,6 +514,8 @@ struct files_struct *get_files_struct(struct task_struct *task)
 	return files;
 }
 
+EXPORT_SYMBOL(get_files_struct);
+
 void put_files_struct(struct files_struct *files)
 {
 	struct fdtable *fdt;
@@ -534,6 +536,8 @@ void put_files_struct(struct files_struct *files)
 		rcu_read_unlock();
 	}
 }
+
+EXPORT_SYMBOL(put_files_struct);
 
 void reset_files_struct(struct files_struct *files)
 {
