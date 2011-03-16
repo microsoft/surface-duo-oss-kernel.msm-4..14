4c4e6c48a23b904a5b6dddded2a03bd68f03f4cd
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2011-03-16 19:05:35 -0400

    mips-assume-synchronized-tsc
    MIPS assume synchronized TSC
    
    Code for non-synchronized TSC support was broken (at least for MIPS32). I
    suspect the spinlock did not do well with NMIs or something like this. Revert to
    assuming synchronized TSCs across cores until someone send me a MIPS board to
    play with.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>

--- arch/mips/include/asm/trace-clock.h
+++ arch/mips/include/asm/trace-clock.h
@@ -32,28 +32,15 @@ extern u64 trace_clock_read_synthetic_tsc(void);
  * tracing needs to detect delays longer than 8 seconds, we need a full 64-bits
  * TSC, whic is provided by trace-clock-32-to-64.
 */
-extern u64 trace_clock_async_tsc_read(void);
 
 static inline u32 trace_clock_read32(void)
 {
-	u32 cycles;
-
-	if (likely(tsc_is_sync()))
-		cycles = (u32)get_cycles(); /* only need the 32 LSB */
-	else
-		cycles = (u32)trace_clock_async_tsc_read();
-	return cycles;
+	return (u32)get_cycles(); /* only need the 32 LSB */
 }
 
 static inline u64 trace_clock_read64(void)
 {
-	u64 cycles;
-
-	if (likely(tsc_is_sync()))
-		cycles = trace_clock_read_synthetic_tsc();
-	else
-		cycles = trace_clock_async_tsc_read();
-	return cycles;
+	return trace_clock_read_synthetic_tsc();
 }
 
 static inline u64 trace_clock_frequency(void)
@@ -66,10 +53,18 @@ static inline u32 trace_clock_freq_scale(void)
 	return 1;
 }
 
-extern void get_trace_clock(void);
-extern void put_trace_clock(void);
 extern void get_synthetic_tsc(void);
 extern void put_synthetic_tsc(void);
+
+static inline void get_trace_clock(void)
+{
+	get_synthetic_tsc();
+}
+
+static inline void put_trace_clock(void)
+{
+	put_synthetic_tsc();
+}
 
 static inline void set_trace_clock_is_sync(int state)
 {
