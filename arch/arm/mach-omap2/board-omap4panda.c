64dcf798ccba42e3fcc49431918411eedf5c86f0
Ricardo Neri <ricardo.neri@ti.com>
2011-05-03 12:19:04 +0800

    OMAP4: HDMI: Add device for HDMI audio interface to OMAP4 PANDA
    Add platform device for the HHDMI digital audio interface to OMAP4 Panda
    board. This is for use of the ASoC HDMI CPU DAI driver.
    
    Signed-off-by: Ricardo Neri <ricardo.neri@ti.com>

--- arch/arm/mach-omap2/board-omap4panda.c
+++ arch/arm/mach-omap2/board-omap4panda.c
@@ -83,8 +83,14 @@ static struct platform_device leds_gpio = {
 	},
 };
 
+static struct platform_device omap4panda_hdmi_audio_device = {
+	.name		= "hdmi-audio-dai",
+	.id		= -1,
+};
+
 static struct platform_device *panda_devices[] __initdata = {
 	&leds_gpio,
+	&omap4panda_hdmi_audio_device,
 };
 
 /* Display DVI */
