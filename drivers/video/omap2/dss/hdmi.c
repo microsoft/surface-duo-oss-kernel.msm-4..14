f32697546c88f34ec4e490ce1b93fac0c902746a
Sebastien Jan <s-jan@ti.com>
2011-05-03 12:18:18 +0800

    OMAP4: DSS: add generic notifier mechanism
    A callback can be registered by the dssdev client in order to be
    notified of resolution changes, for example an external monitor
    that is hot-plugged.
    
    Multiple clients can now register for notification from one
    dssdev, and the notification mechanism can be extended in the
    future to add other events.
    
    This is a port of Rob Clark's <rob@ti.com> original patch.
    
    Signed-off-by: Sebastien Jan <s-jan@ti.com>

--- drivers/video/omap2/dss/hdmi.c
+++ drivers/video/omap2/dss/hdmi.c
@@ -1104,6 +1104,7 @@ static void hdmi_enable_clocks(int enable)
 static int hdmi_power_on(struct omap_dss_device *dssdev)
 {
 	int r, code = 0;
+	int dirty = true;
 	struct hdmi_pll_info pll_data;
 	struct omap_video_timings *p;
 	int clkin, n, phy;
@@ -1119,8 +1120,10 @@ static int hdmi_power_on(struct omap_dss_device *dssdev)
 		dssdev->panel.timings.y_res);
 
 	if (!hdmi.custom_set) {
+		code = get_timings_index();
 		DSSDBG("Read EDID as no EDID is not set on poweron\n");
 		hdmi_read_edid(p);
+		dirty = get_timings_index() != code;
 	}
 	code = get_timings_index();
 	dssdev->panel.timings = cea_vesa_timings[code].timings;
@@ -1134,7 +1137,11 @@ static int hdmi_power_on(struct omap_dss_device *dssdev)
 
 	hdmi_wp_video_start(0);
 
-	/* config the PLL and PHY first */
+ 	if (dirty) {
+		omap_dss_notify(dssdev, OMAP_DSS_SIZE_CHANGE);
+ 	}
+ 
+ 	/* config the PLL and PHY first */
 	r = hdmi_pll_program(&pll_data);
 	if (r) {
 		DSSDBG("Failed to lock PLL\n");
