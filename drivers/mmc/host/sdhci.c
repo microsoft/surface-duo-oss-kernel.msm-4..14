fc676e65c7eab02fa3936038559b951352bff710
Shawn Guo <shawn.guo@linaro.org>
2011-06-15 19:15:19 +0800

    mmc: sdhci: fix interrupt storm from card detection
    The issue was initially found by Eric Benard as below.
    
    http://permalink.gmane.org/gmane.linux.ports.arm.kernel/108031
    
    Not sure about other SDHCI based controller, but on Freescale eSDHC,
    the SDHCI_INT_CARD_INSERT bits will be immediately set again when it
    gets cleared, if a card is inserted. The driver need to mask the irq
    to prevent interrupt storm which will freeze the system.  And the
    SDHCI_INT_CARD_REMOVE gets the same situation.
    
    The patch fixes the problem based on the initial idea from
    Eric Benard.
    
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>
    Cc: Eric Benard <eric@eukrea.com>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -1591,12 +1591,29 @@ static irqreturn_t sdhci_irq(int irq, void *dev_id)
 		mmc_hostname(host->mmc), intmask);
 
 	if (intmask & (SDHCI_INT_CARD_INSERT | SDHCI_INT_CARD_REMOVE)) {
+		u32 present = sdhci_readl(host, SDHCI_PRESENT_STATE) &
+			      SDHCI_CARD_PRESENT;
+
+		/*
+		 * There is a observation on i.mx esdhc.  INSERT bit will be
+		 * immediately set again when it gets cleared, if a card is
+		 * inserted.  We have to mask the irq to prevent interrupt
+		 * storm which will freeze the system.  And the REMOVE gets
+		 * the same situation.
+		 *
+		 * More testing are needed here to ensure it works for other
+		 * platforms though.
+		 */
+		sdhci_mask_irqs(host, present ? SDHCI_INT_CARD_INSERT :
+						SDHCI_INT_CARD_REMOVE);
+		sdhci_unmask_irqs(host, present ? SDHCI_INT_CARD_REMOVE :
+						  SDHCI_INT_CARD_INSERT);
+
 		sdhci_writel(host, intmask & (SDHCI_INT_CARD_INSERT |
-			SDHCI_INT_CARD_REMOVE), SDHCI_INT_STATUS);
+			     SDHCI_INT_CARD_REMOVE), SDHCI_INT_STATUS);
+		intmask &= ~(SDHCI_INT_CARD_INSERT | SDHCI_INT_CARD_REMOVE);
 		tasklet_schedule(&host->card_tasklet);
 	}
-
-	intmask &= ~(SDHCI_INT_CARD_INSERT | SDHCI_INT_CARD_REMOVE);
 
 	if (intmask & SDHCI_INT_CMD_MASK) {
 		sdhci_writel(host, intmask & SDHCI_INT_CMD_MASK,
