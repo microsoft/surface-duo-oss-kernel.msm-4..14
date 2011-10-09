43f17552ef427559e66abe0b092789e09e433478
Gustavo F. Padovan <padovan@profusion.mobi>
2011-10-09 02:28:50 +0100

    Bluetooth: fix set but not used warning
    Change-Id: I6b5be004cdb85fee096f82eb4a599edba3af7e67
    Signed-off-by: Gustavo F. Padovan <padovan@profusion.mobi>

--- net/bluetooth/l2cap_core.c
+++ net/bluetooth/l2cap_core.c
@@ -3793,6 +3793,7 @@ static inline int l2cap_data_channel(struct l2cap_conn *conn, u16 cid, struct sk
 {
 	struct l2cap_chan *chan;
 	struct sock *sk = NULL;
+	struct l2cap_pinfo *pi;
 	u16 control;
 	u8 tx_seq;
 	int len;
@@ -3804,7 +3805,7 @@ static inline int l2cap_data_channel(struct l2cap_conn *conn, u16 cid, struct sk
 	}
 
 	sk = chan->sk;
-
+	pi = l2cap_pi(sk);
 	BT_DBG("chan %p, len %d", chan, skb->len);
 
 	if (chan->state != BT_CONNECTED)
