c94c34cf64aa6ca3e9d11ce745777b4d49bf6a27
Colin Cross <ccross@android.com>
2011-12-02 14:01:37 +0800

    mmc: core: host: only use wakelock for detect work
    There is no need to take a wakelock for delayed lazy disable
    work, it will be cancelled in the suspend handler and force
    disabled.  Only take the wakelock when the detect work is
    queued, and make sure to drop the wakelock if the work is
    cancelled.
    
    Change-Id: I1e507a5f98848954ea21d45e23b6192c3132a349
    Signed-off-by: Colin Cross <ccross@android.com>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -13,6 +13,7 @@
 #include <linux/leds.h>
 #include <linux/sched.h>
 #include <linux/fault-inject.h>
+#include <linux/wakelock.h>
 
 #include <linux/mmc/core.h>
 #include <linux/mmc/pm.h>
@@ -297,6 +298,7 @@ struct mmc_host {
 	int			claim_cnt;	/* "claim" nesting count */
 
 	struct delayed_work	detect;
+	struct wake_lock	detect_wake_lock;
 
 	const struct mmc_bus_ops *bus_ops;	/* current bus driver */
 	unsigned int		bus_refs;	/* reference counter */
