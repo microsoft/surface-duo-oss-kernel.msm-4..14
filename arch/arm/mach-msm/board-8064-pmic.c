629a86c32ec50d66ef75b3f1b86f1e1449c903e7
Suman Kumar <suman@inforcecomputing.com>
2013-05-25 05:03:49 +0530

    wifi: pmic: Enable pmic gpios for QCD623Ax Wifi+BT+Radio
    Change-Id: Ia4692ede69e02e334fd3245f782a900a43f7de6e
    
    Signed-off-by: Suman Kumar <suman@inforcecomputing.com>

--- arch/arm/mach-msm/board-8064-pmic.c
+++ arch/arm/mach-msm/board-8064-pmic.c
@@ -129,6 +129,8 @@ static struct pm8xxx_gpio_init pm8921_gpios[] __initdata = {
 	PM8921_GPIO_OUTPUT(34, 1, HIGH),
 	PM8921_GPIO_OUTPUT(13, 0, HIGH),               /* PCIE_CLK_PWR_EN */
 	PM8921_GPIO_INPUT(6, PM_GPIO_PULL_UP_30),     /* PCIE_WAKE_N */
+	PM8921_GPIO_OUTPUT(43, 1, HIGH),
+	PM8921_GPIO_OUTPUT(44, 1, HIGH),
 };
 
 static struct pm8xxx_gpio_init pm8921_mtp_kp_gpios[] __initdata = {
