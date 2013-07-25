b5ff26dd315431af29d270c338dfabd48ce29073
Jani Nikula <jani.nikula@linux.intel.com>
2013-07-25 12:44:34 +0300

    drm/i915: initialize gt_lock early with other spin locks
    commit 14c5cec5d0cd73e7e9d4fbea2bbfeea8f3ade871 upstream.
    
    commit 181d1b9e31c668259d3798c521672afb8edd355c
    Author: Daniel Vetter <daniel.vetter@ffwll.ch>
    Date:   Sun Jul 21 13:16:24 2013 +0200
    
        drm/i915: fix up gt init sequence fallout
    
    moved dev_priv->gt_lock initialization after use. Do the initialization
    much earlier with other spin lock initializations.
    
    Reported-by: Sedat Dilek <sedat.dilek@gmail.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Tested-by: Sedat Dilek <sedat.dilek@gmail.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    [ kamal: backport to 3.8 (context) ]
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>

--- drivers/gpu/drm/i915/i915_dma.c
+++ drivers/gpu/drm/i915/i915_dma.c
@@ -1469,6 +1469,7 @@ int i915_driver_load(struct drm_device *dev, unsigned long flags)
 	spin_lock_init(&dev_priv->error_lock);
 	spin_lock_init(&dev_priv->rps.lock);
 	spin_lock_init(&dev_priv->dpio_lock);
+	spin_lock_init(&dev_priv->gt_lock);
 	mutex_init(&dev_priv->rps.hw_lock);
 
 	i915_dump_device_info(dev_priv);
