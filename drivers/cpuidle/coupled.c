4ebd887135828389b31e30d957a5584abc4139cf
Colin Cross <ccross@android.com>
2013-08-23 12:45:10 -0700

    cpuidle: coupled: disable interrupts after entering safe state
    commit 59e998561103a93f294c8d3138dd659af772a5da upstream.
    
    Calling cpuidle_enter_state is expected to return with interrupts
    enabled, but interrupts must be disabled before starting the
    ready loop synchronization stage.  Call local_irq_disable after
    each call to cpuidle_enter_state for the safe state.
    
    Tested-by: Stephen Warren <swarren@nvidia.com>
    Signed-off-by: Colin Cross <ccross@android.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>

--- drivers/cpuidle/coupled.c
+++ drivers/cpuidle/coupled.c
@@ -470,6 +470,7 @@ int cpuidle_enter_state_coupled(struct cpuidle_device *dev,
 		}
 		entered_state = cpuidle_enter_state(dev, drv,
 			dev->safe_state_index);
+		local_irq_disable();
 	}
 
 	/* Read barrier ensures online_count is read after prevent is cleared */
@@ -514,6 +515,7 @@ retry:
 
 		entered_state = cpuidle_enter_state(dev, drv,
 			dev->safe_state_index);
+		local_irq_disable();
 	}
 
 	if (cpuidle_coupled_clear_pokes(dev->cpu)) {
