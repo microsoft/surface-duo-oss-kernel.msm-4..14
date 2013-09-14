41bfd7556d05140780adae7b3a7152d277abfed0
Rob Clark <robdclark@gmail.com>
2013-09-14 14:06:43 -0400

    WIP: perf/timechart: support for GPU
    Show GPU as another "CPU" on the timeline, so we can see where it is
    busy and idle.  Also show pending flips (green bar on the GPU), as well
    as places where processes are blocked on a fence (yellow bar on the
    process).
    
    Currently a bit hacky.  And looking for drm/msm specific traces.  We
    should probably make a set of generic traces that can be used by all drm
    drivers, and use these trace events in perf.  (For other GPU's we
    probably want a way to be able to visualize activity on multiple rings.)

--- tools/perf/util/svghelper.h
+++ tools/perf/util/svghelper.h
@@ -8,11 +8,14 @@ extern void svg_box(int Yslot, u64 start, u64 end, const char *type);
 extern void svg_sample(int Yslot, int cpu, u64 start, u64 end);
 extern void svg_waiting(int Yslot, u64 start, u64 end);
 extern void svg_cpu_box(int cpu, u64 max_frequency, u64 turbo_frequency);
+void svg_gpu_box(int cpu);
 
 
 extern void svg_process(int cpu, u64 start, u64 end, const char *type, const char *name);
 extern void svg_cstate(int cpu, u64 start, u64 end, int type);
 extern void svg_pstate(int cpu, u64 start, u64 end, u64 freq);
+void svg_flip(int cpu, u64 start, u64 end, int crtc, const void *obj);
+void svg_fence(int Yslot, u64 start, u64 end, u32 fence, int ret);
 
 
 extern void svg_time_grid(void);
