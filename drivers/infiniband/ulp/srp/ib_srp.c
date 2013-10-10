9a6a141cae6f6f37dc98bf69f3839a4c2f8cb095
Bart Van Assche <bvanassche@acm.org>
2013-10-10 13:52:33 +0200

    IB/srp: Avoid offlining operational SCSI devices
    commit 99b6697a50c2acbe3ca2772d359fc9a28835dc84 upstream.
    
    If SCSI commands are submitted with a SCSI request timeout that is
    lower than the the IB RC timeout, it can happen that the SCSI error
    handler has already started device recovery before transport layer
    error handling starts.  So it can happen that the SCSI error handler
    tries to abort a SCSI command after it has been reset by
    srp_rport_reconnect().
    
    Tell the SCSI error handler that such commands have finished and that
    it is not necessary to continue its recovery strategy for commands
    that have been reset by srp_rport_reconnect().
    
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: Roland Dreier <roland@purestorage.com>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>

--- drivers/infiniband/ulp/srp/ib_srp.c
+++ drivers/infiniband/ulp/srp/ib_srp.c
@@ -1749,7 +1749,7 @@ static int srp_abort(struct scsi_cmnd *scmnd)
 	shost_printk(KERN_ERR, target->scsi_host, "SRP abort called\n");
 
 	if (!req || !srp_claim_req(target, req, scmnd))
-		return FAILED;
+		return SUCCESS;
 	srp_send_tsk_mgmt(target, req->index, scmnd->device->lun,
 			  SRP_TSK_ABORT_TASK);
 	srp_free_req(target, req, scmnd, 0);
