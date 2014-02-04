da208817aa405d6a3dd882b0736f9274ef45152d
Rob Clark <robdclark@gmail.com>
2014-02-04 14:16:04 -0500

    drm/msm: add chip-id param
    Some of the w/a or different behavior of userspace blob driver seem to
    be keyed to gpu patch revision, rather than gpu-id.  So expose the full
    chip-id to userspace so it can DTRT.
    
    Signed-off-by: Rob Clark <robdclark@gmail.com>

--- drivers/gpu/drm/msm/adreno/adreno_gpu.c
+++ drivers/gpu/drm/msm/adreno/adreno_gpu.c
@@ -73,6 +73,9 @@ int adreno_get_param(struct msm_gpu *gpu, uint32_t param, uint64_t *value)
 	case MSM_PARAM_GMEM_SIZE:
 		*value = adreno_gpu->gmem;
 		return 0;
+	case MSM_PARAM_CHIP_ID:
+		*value = *(uint64_t *)&adreno_gpu->rev;
+		return 0;
 	default:
 		DBG("%s: invalid param: %u", gpu->name, param);
 		return -EINVAL;
