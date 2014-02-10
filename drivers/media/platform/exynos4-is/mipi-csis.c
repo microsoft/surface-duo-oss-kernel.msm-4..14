d551dc50e62dcf76037fdd38998ed46a169bfbdf
Philipp Zabel <p.zabel@pengutronix.de>
2014-02-10 22:01:48 +0100

    [media] of: move graph helpers from drivers/media/v4l2-core to drivers/of
    This patch moves the parsing helpers used to parse connected graphs
    in the device tree, like the video interface bindings documented in
    Documentation/devicetree/bindings/media/video-interfaces.txt, from
    drivers/media/v4l2-core/v4l2-of.c into drivers/of/base.c.
    
    This allows to reuse the same parser code from outside the V4L2
    framework, most importantly from display drivers.
    The functions v4l2_of_get_next_endpoint, v4l2_of_get_remote_port,
    and v4l2_of_get_remote_port_parent are moved. They are renamed to
    of_graph_get_next_endpoint, of_graph_get_remote_port, and
    of_graph_get_remote_port_parent, respectively.
    Since there are not that many current users yet, switch all of
    them to the new functions right away.
    
    This is a backport from Mainline 3.15
    commit: fd9fdb78a9bf85b94fb2190c82ff280c8f8375cc
    
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>
    Acked-by: Tomi Valkeinen <tomi.valkeinen@ti.com>
    Acked-by: Mauro Carvalho Chehab <m.chehab@samsung.com>
    Acked-by: Sylwester Nawrocki <s.nawrocki@samsung.com>
    Signed-off-by: Brian Starkey <brian.starkey@arm.com>
    
    Conflicts:
    	drivers/media/i2c/adv7343.c (no DT support in 3.10)
    	drivers/media/i2c/mt9p031.c (no DT support in 3.10)
    	drivers/media/i2c/s5k5baf.c (doesn't exist in 3.10)
    	drivers/media/i2c/tvp514x.c (no DT support in 3.10)
    	drivers/media/i2c/tvp7002.c (no DT support in 3.10)
    	drivers/media/platform/exynos4-is/media-dev.c
    	drivers/media/platform/exynos4-is/mipi-csis.c
    	drivers/media/v4l2-core/v4l2-of.c
    	drivers/of/base.c
    	include/media/v4l2-of.h
    
    Change-Id: I6eb22afe3b1f23e3bebebbe5709efc00684e04bd

--- drivers/media/platform/exynos4-is/mipi-csis.c
+++ drivers/media/platform/exynos4-is/mipi-csis.c
@@ -20,6 +20,7 @@
 #include <linux/memory.h>
 #include <linux/module.h>
 #include <linux/of.h>
+#include <linux/of_graph.h>
 #include <linux/platform_data/mipi-csis.h>
 #include <linux/platform_device.h>
 #include <linux/pm_runtime.h>
@@ -743,7 +744,7 @@ static int s5pcsis_parse_dt(struct platform_device *pdev,
 				 &state->max_num_lanes))
 		return -EINVAL;
 
-	node = v4l2_of_get_next_endpoint(node, NULL);
+	node = of_graph_get_next_endpoint(node, NULL);
 	if (!node) {
 		dev_err(&pdev->dev, "No port node at %s\n",
 				pdev->dev.of_node->full_name);
