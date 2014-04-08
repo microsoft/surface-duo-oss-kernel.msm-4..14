e9b4dbfe5a28b35659043e91edacc5c436f7e295
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2014-04-08 10:43:39 +0100

    clk: qcom: add clocks necessary for apq8064 sdcc
    This patch adds clocks necessary for SD card controller on apq8064 SOC.
    Without this patch the clocks are visible to the sdcc driver.
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>

--- drivers/clk/qcom/gcc-msm8960.c
+++ drivers/clk/qcom/gcc-msm8960.c
@@ -2876,6 +2876,16 @@ static struct clk_regmap *gcc_apq8064_clks[] = {
 	[GSBI7_QUP_SRC] = &gsbi7_qup_src.clkr,
 	[GSBI7_QUP_CLK] = &gsbi7_qup_clk.clkr,
 	[GSBI7_H_CLK] = &gsbi7_h_clk.clkr,
+	[SDC1_SRC] = &sdc1_src.clkr,
+	[SDC1_CLK] = &sdc1_clk.clkr,
+	[SDC2_SRC] = &sdc2_src.clkr,
+	[SDC2_CLK] = &sdc2_clk.clkr,
+	[SDC3_SRC] = &sdc3_src.clkr,
+	[SDC3_CLK] = &sdc3_clk.clkr,
+	[SDC4_SRC] = &sdc4_src.clkr,
+	[SDC4_CLK] = &sdc4_clk.clkr,
+	[SDC5_SRC] = &sdc5_src.clkr,
+	[SDC5_CLK] = &sdc5_clk.clkr,
 };
 
 static const struct regmap_config gcc_msm8960_regmap_config = {
