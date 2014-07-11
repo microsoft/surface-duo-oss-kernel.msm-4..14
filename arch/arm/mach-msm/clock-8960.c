fd76042e7d999cefce5d106776b3313fd5c0c1ce
Nicolas Dechesne <nicolas.dechesne@linaro.org>
2014-07-11 11:40:09 +0200

    mach-msm: fix set_rate_tv when using DRM_MSM
    Signed-off-by: Nicolas Dechesne <nicolas.dechesne@linaro.org>

--- arch/arm/mach-msm/clock-8960.c
+++ arch/arm/mach-msm/clock-8960.c
@@ -4124,7 +4124,7 @@ static unsigned long fmax_tv_src_8064[VDD_DIG_NUM] = {
  */
 void set_rate_tv(struct rcg_clk *rcg, struct clk_freq_tbl *nf)
 {
-#ifdef CONFIG_FB_MSM_HDMI_MSM_PANEL
+#if  defined(CONFIG_FB_MSM_HDMI_MSM_PANEL) || defined(CONFIG_DRM_MSM)
 	unsigned long pll_rate = (unsigned long)nf->extra_freq_data;
 	if (pll_rate) {
 		hdmi_pll_set_rate(pll_rate);
