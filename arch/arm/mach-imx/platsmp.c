ce44a00b84a2f73acb2048b78915081aaeb9461b
Gilles Talis <gilles.talis@freescale.com>
2014-10-02 11:26:52 +0200

    Add SMP support to sac58r SoC
    Signed-off-by: Gilles Talis <gilles.talis@freescale.com>
    Signed-off-by: Stoica Cosmin-Stefan <cosminstefan.stoica@freescale.com>

--- arch/arm/mach-imx/platsmp.c
+++ arch/arm/mach-imx/platsmp.c
@@ -19,12 +19,15 @@
 #include <asm/page.h>
 #include <asm/smp_scu.h>
 #include <asm/mach/map.h>
+#include <asm/cputype.h>
 
 #include "common.h"
 #include "hardware.h"
 
 u32 g_diag_reg;
 static void __iomem *scu_base;
+
+static unsigned long cpu_part_number;
 
 static struct map_desc scu_io_desc __initdata = {
 	/* .virtual and .pfn are run-time assigned */
@@ -50,6 +53,7 @@ static int imx_boot_secondary(unsigned int cpu, struct task_struct *idle)
 {
 	imx_set_cpu_jump(cpu, v7_secondary_startup);
 	imx_enable_cpu(cpu, true);
+
 	return 0;
 }
 
@@ -61,7 +65,23 @@ static void __init imx_smp_init_cpus(void)
 {
 	int i, ncores;
 
-	ncores = scu_get_core_count(scu_base);
+	cpu_part_number = read_cpuid_part_number();
+
+	switch (cpu_part_number)
+	{
+		case ARM_CPU_PART_CORTEX_A7:
+			/* sac58R and other A7 based SoC */
+			/* get number of cores from CP15 L2 controller register(L2CTLR)*/
+			asm volatile ("mrc p15, 1, %0, c9, c0, 2" : "=r" (ncores));
+			ncores = ((ncores >> 24) & 0x3) + 1;
+			break;
+
+		case ARM_CPU_PART_CORTEX_A9:
+		default:
+			/* i.MX6 and other A9 based SoC */
+			ncores = scu_get_core_count(scu_base);
+			break;
+	}
 
 	for (i = ncores; i < NR_CPUS; i++)
 		set_cpu_possible(i, false);
@@ -74,18 +94,22 @@ void imx_smp_prepare(void)
 
 static void __init imx_smp_prepare_cpus(unsigned int max_cpus)
 {
-	imx_smp_prepare();
+	if (ARM_CPU_PART_CORTEX_A9 == cpu_part_number)
+	{
+		imx_smp_prepare();
 
-	/*
-	 * The diagnostic register holds the errata bits.  Mostly bootloader
-	 * does not bring up secondary cores, so that when errata bits are set
-	 * in bootloader, they are set only for boot cpu.  But on a SMP
-	 * configuration, it should be equally done on every single core.
-	 * Read the register from boot cpu here, and will replicate it into
-	 * secondary cores when booting them.
-	 */
-	asm("mrc p15, 0, %0, c15, c0, 1" : "=r" (g_diag_reg) : : "cc");
-	sync_cache_w(&g_diag_reg);
+		/*
+		 * The diagnostic register holds the errata bits.  Mostly bootloader
+		 * does not bring up secondary cores, so that when errata bits are set
+		 * in bootloader, they are set only for boot cpu.  But on a SMP
+		 * configuration, it should be equally done on every single core.
+		 * Read the register from boot cpu here, and will replicate it into
+		 * secondary cores when booting them.
+		 */
+		asm("mrc p15, 0, %0, c15, c0, 1" : "=r" (g_diag_reg) : : "cc");
+		__cpuc_flush_dcache_area(&g_diag_reg, sizeof(g_diag_reg));
+		outer_clean_range(__pa(&g_diag_reg), __pa(&g_diag_reg + 1));
+	}
 }
 
 struct smp_operations  imx_smp_ops __initdata = {
@@ -126,4 +150,10 @@ static void __init ls1021a_smp_prepare_cpus(unsigned int max_cpus)
 struct smp_operations  ls1021a_smp_ops __initdata = {
 	.smp_prepare_cpus	= ls1021a_smp_prepare_cpus,
 	.smp_boot_secondary	= ls1021a_boot_secondary,
+};
+
+struct smp_operations  sac58r_smp_ops __initdata = {
+	.smp_init_cpus		= imx_smp_init_cpus,
+	.smp_prepare_cpus	= imx_smp_prepare_cpus,
+	.smp_boot_secondary	= imx_boot_secondary,
 };
