b1272d8d0856f3d5cccae7cbfac94daf27f33277
Gilles Talis <gilles.talis@freescale.com>
2014-10-02 11:26:08 +0200

    Add IMX SRC driver support to sac58r SoC
    Signed-off-by: Gilles Talis <gilles.talis@freescale.com>

--- arch/arm/mach-imx/src.c
+++ arch/arm/mach-imx/src.c
@@ -21,6 +21,7 @@
 
 #define SRC_SCR				0x000
 #define SRC_GPR1			0x020
+
 #define BP_SRC_SCR_WARM_RESET_ENABLE	0
 #define BP_SRC_SCR_SW_GPU_RST		1
 #define BP_SRC_SCR_SW_VPU_RST		2
@@ -29,6 +30,19 @@
 #define BP_SRC_SCR_SW_IPU2_RST		12
 #define BP_SRC_SCR_CORE1_RST		14
 #define BP_SRC_SCR_CORE1_ENABLE		22
+
+/* SAC58R SRC registers definition */
+#define SAC58R_SRC_GPR2				0x028
+#define SAC58R_SRC_SCR_CORE1_ENABLE	31
+#define SRC_SCR_SW_RESET			12
+
+
+enum {
+	IMX_SRC_DEVTYPE_IMX6,
+	IMX_SRC_DEVTYPE_SAC58R,
+};
+
+static int imx_src_devtype;
 
 static void __iomem *src_base;
 static DEFINE_SPINLOCK(scr_lock);
@@ -87,32 +101,68 @@ void imx_enable_cpu(int cpu, bool enable)
 	u32 mask, val;
 
 	cpu = cpu_logical_map(cpu);
-	mask = 1 << (BP_SRC_SCR_CORE1_ENABLE + cpu - 1);
-	spin_lock(&scr_lock);
-	val = readl_relaxed(src_base + SRC_SCR);
-	val = enable ? val | mask : val & ~mask;
-	val |= 1 << (BP_SRC_SCR_CORE1_RST + cpu - 1);
-	writel_relaxed(val, src_base + SRC_SCR);
-	spin_unlock(&scr_lock);
+
+	if (imx_src_devtype == IMX_SRC_DEVTYPE_SAC58R) {
+		mask = 1 << SAC58R_SRC_SCR_CORE1_ENABLE;
+		spin_lock(&scr_lock);
+		val = readl_relaxed(src_base + SRC_SCR);
+		val = enable ? val | mask : val & ~mask;
+		writel_relaxed(val, src_base + SRC_SCR);
+		spin_unlock(&scr_lock);
+	}
+	else {
+		mask = 1 << (BP_SRC_SCR_CORE1_ENABLE + cpu - 1);
+		spin_lock(&scr_lock);
+		val = readl_relaxed(src_base + SRC_SCR);
+		val = enable ? val | mask : val & ~mask;
+		val |= 1 << (BP_SRC_SCR_CORE1_RST + cpu - 1);
+		writel_relaxed(val, src_base + SRC_SCR);
+		spin_unlock(&scr_lock);
+	}
 }
 
 void imx_set_cpu_jump(int cpu, void *jump_addr)
 {
+	int src_base_address = SRC_GPR1;
+
+	if (imx_src_devtype == IMX_SRC_DEVTYPE_SAC58R) {
+		src_base_address = SAC58R_SRC_GPR2;
+	}
+
+	printk("%s: cpu(%d), jump_addr(%x), base @ = %x\n",
+		__func__, cpu, (int)virt_to_phys(jump_addr), (int)(src_base + src_base_address + cpu * 8));
+
+
 	cpu = cpu_logical_map(cpu);
 	writel_relaxed(virt_to_phys(jump_addr),
-		       src_base + SRC_GPR1 + cpu * 8);
+		       src_base + src_base_address + cpu * 8);
 }
 
 u32 imx_get_cpu_arg(int cpu)
 {
+	int src_base_address = SRC_GPR1;
+
+	if (imx_src_devtype == IMX_SRC_DEVTYPE_SAC58R) {
+		src_base_address = SAC58R_SRC_GPR2;
+	}
+
 	cpu = cpu_logical_map(cpu);
-	return readl_relaxed(src_base + SRC_GPR1 + cpu * 8 + 4);
+	return readl_relaxed(src_base + src_base_address + cpu * 8 + 4);
 }
 
 void imx_set_cpu_arg(int cpu, u32 arg)
 {
+	int src_base_address = SRC_GPR1;
+
+	if (imx_src_devtype == IMX_SRC_DEVTYPE_SAC58R) {
+		src_base_address = SAC58R_SRC_GPR2;
+	}
+
+	pr_debug("%s: cpu(%d), arg(%x), base @ = %x\n",
+		__func__, cpu, arg, (int)(src_base + src_base_address + cpu * 8 + 4));
+
 	cpu = cpu_logical_map(cpu);
-	writel_relaxed(arg, src_base + SRC_GPR1 + cpu * 8 + 4);
+	writel_relaxed(arg, src_base + src_base_address + cpu * 8 + 4);
 }
 
 void __init imx_src_init(void)
@@ -121,22 +171,35 @@ void __init imx_src_init(void)
 	u32 val;
 
 	np = of_find_compatible_node(NULL, NULL, "fsl,imx51-src");
-	if (!np)
-		return;
+	if (np) {
+		imx_src_devtype = IMX_SRC_DEVTYPE_IMX6;
+	}
+	else {
+		np = of_find_compatible_node(NULL, NULL, "fsl,sac58r-src");
+		if (np) {
+			imx_src_devtype = IMX_SRC_DEVTYPE_SAC58R;
+		}
+		else {
+			return;
+		}
+	}
+
 	src_base = of_iomap(np, 0);
 	WARN_ON(!src_base);
 
-	imx_reset_controller.of_node = np;
-	if (IS_ENABLED(CONFIG_RESET_CONTROLLER))
-		reset_controller_register(&imx_reset_controller);
+	if (IMX_SRC_DEVTYPE_IMX6 == imx_src_devtype) {
+		imx_reset_controller.of_node = np;
+		if (IS_ENABLED(CONFIG_RESET_CONTROLLER))
+			reset_controller_register(&imx_reset_controller);
 
-	/*
-	 * force warm reset sources to generate cold reset
-	 * for a more reliable restart
-	 */
-	spin_lock(&scr_lock);
-	val = readl_relaxed(src_base + SRC_SCR);
-	val &= ~(1 << BP_SRC_SCR_WARM_RESET_ENABLE);
-	writel_relaxed(val, src_base + SRC_SCR);
-	spin_unlock(&scr_lock);
+		/*
+		 * force warm reset sources to generate cold reset
+		 * for a more reliable restart
+		 */
+		spin_lock(&scr_lock);
+		val = readl_relaxed(src_base + SRC_SCR);
+		val &= ~(1 << BP_SRC_SCR_WARM_RESET_ENABLE);
+		writel_relaxed(val, src_base + SRC_SCR);
+		spin_unlock(&scr_lock);
+	}
 }
