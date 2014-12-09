cba52c8611e2fb2a89e82eaa03802d3f7c0de603
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2014-12-09 13:23:02 +0000

    WIP:drm/msm/hdmi: Pull 3.4 kernel changes required for hdmi audio
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>

--- drivers/gpu/drm/msm/hdmi/hdmi.c
+++ drivers/gpu/drm/msm/hdmi/hdmi.c
@@ -251,11 +251,17 @@ fail:
 
 #include <linux/of_gpio.h>
 
+// this is still needed on downstream kernel for now just for
+// hdmi_msm_audio_info_setup() / hdmi_msm_audio_sample_rate_reset..
+// TODO sane interface between audio and display..
+struct platform_device *hdmi_pdev_hack;
+
 static void set_hdmi_pdev(struct drm_device *dev,
 		struct platform_device *pdev)
 {
 	struct msm_drm_private *priv = dev->dev_private;
 	priv->hdmi_pdev = pdev;
+	hdmi_pdev_hack = pdev;
 }
 
 #ifdef CONFIG_OF
@@ -394,6 +400,34 @@ static int hdmi_dev_remove(struct platform_device *pdev)
 	component_del(&pdev->dev, &hdmi_ops);
 	return 0;
 }
+
+static struct hdmi *find_hdmi(void)
+{
+	return hdmi_pdev_hack ? platform_get_drvdata(hdmi_pdev_hack) : NULL;
+}
+
+int hdmi_msm_audio_info_setup(bool enabled, u32 num_of_channels,
+	u32 channel_allocation, u32 level_shift, bool down_mix)
+{
+	struct hdmi *hdmi = find_hdmi();
+	printk("DEBUG::: %s \n", __func__);
+	if (!hdmi)
+		return -EINVAL;
+	printk("DEBUG:::1 %s \n", __func__);
+	return hdmi_audio_info_setup(hdmi, enabled, num_of_channels,
+			channel_allocation, level_shift, down_mix);
+}
+EXPORT_SYMBOL(hdmi_msm_audio_info_setup);
+
+void hdmi_msm_audio_sample_rate_reset(int rate)
+{
+	struct hdmi *hdmi = find_hdmi();
+	if (!hdmi)
+		return;
+	printk("DEBUG::: %s \n", __func__);
+	hdmi_audio_set_sample_rate(hdmi, rate);
+}
+EXPORT_SYMBOL(hdmi_msm_audio_sample_rate_reset);
 
 static const struct of_device_id dt_match[] = {
 	{ .compatible = "qcom,hdmi-tx-8074" },
