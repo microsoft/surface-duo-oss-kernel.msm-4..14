4da67629187372b7e0d449cdc235217221cd7556
Fabrice Goucem <fgoucem@freescale.com>
2015-02-05 17:19:32 +0100

    mmc: sdhci-esdhc-imx: Fix a crash caused by reading registers with clock disabled
    Based on commit: 480c9946099d124c1306b60b12794d967b3802f9
    
    If built as module, ESDHC driver crashes when removing the module.
    This is due to accesses to ESDHC registers with clock disabled.
    
    Issue: ALB-2546
    Signed-off-by: Fabrice Goucem <fgoucem@freescale.com>
    Signed-off-by: Dan Nica <dan.nica@nxp.com>

--- drivers/mmc/host/sdhci-esdhc-imx.c
+++ drivers/mmc/host/sdhci-esdhc-imx.c
@@ -1388,7 +1388,13 @@ static int sdhci_esdhc_imx_remove(struct platform_device *pdev)
 	struct sdhci_host *host = platform_get_drvdata(pdev);
 	struct sdhci_pltfm_host *pltfm_host = sdhci_priv(host);
 	struct pltfm_imx_data *imx_data = sdhci_pltfm_priv(pltfm_host);
-	int dead = (readl(host->ioaddr + SDHCI_INT_STATUS) == 0xffffffff);
+	int dead;
+
+	clk_prepare_enable(imx_data->clk_per);
+	clk_prepare_enable(imx_data->clk_ipg);
+	clk_prepare_enable(imx_data->clk_ahb);
+
+	dead = (readl(host->ioaddr + SDHCI_INT_STATUS) == 0xffffffff);
 
 	pm_runtime_get_sync(&pdev->dev);
 	pm_runtime_disable(&pdev->dev);
