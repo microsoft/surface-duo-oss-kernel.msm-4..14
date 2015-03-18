1a7a7ebc23634337c74536524a6dd26f59bafa06
Andy Gross <agross@codeaurora.org>
2015-03-18 15:49:35 -0500

    mm: Don't offset memmap for flatmem]
    ----- Forwarded message from Laura Abbott <lauraa@codeaurora.org> -----
    
    Date: Fri, 13 Mar 2015 17:08:35 -0700
    From: Laura Abbott <lauraa@codeaurora.org>
    To: Vlastimil Babka <vbabka@suse.cz>, Srinivas Kandagatla <srinivas.kandagatla@linaro.org>, linux-arm-kernel@lists.infradead.org, Russell King - ARM Linux
    	<linux@arm.linux.org.uk>, ssantosh@kernel.org, Andrew Morton <akpm@linux-foundation.org>
    Cc: Kevin Hilman <khilman@linaro.org>, Laura Abbott <lauraa@codeaurora.org>, Arnd Bergman <arnd@arndb.de>, Stephen Boyd <sboyd@codeaurora.org>,
    	linux-mm@kvack.org, Mel Gorman <mgorman@suse.de>, Kumar Gala <galak@codeaurora.org>
    Subject: [PATCHv3] mm: Don't offset memmap for flatmem
    X-Mailer: git-send-email 1.8.2.1
    
    Srinivas Kandagatla reported bad page messages when trying to
    remove the bottom 2MB on an ARM based IFC6410 board
    
    BUG: Bad page state in process swapper  pfn:fffa8
    page:ef7fb500 count:0 mapcount:0 mapping:  (null) index:0x0
    flags: 0x96640253(locked|error|dirty|active|arch_1|reclaim|mlocked)
    page dumped because: PAGE_FLAGS_CHECK_AT_FREE flag(s) set
    bad because of flags:
    flags: 0x200041(locked|active|mlocked)
    Modules linked in:
    CPU: 0 PID: 0 Comm: swapper Not tainted 3.19.0-rc3-00007-g412f9ba-dirty #816
    Hardware name: Qualcomm (Flattened Device Tree)
    [<c0218280>] (unwind_backtrace) from [<c0212be8>] (show_stack+0x20/0x24)
    [<c0212be8>] (show_stack) from [<c0af7124>] (dump_stack+0x80/0x9c)
    [<c0af7124>] (dump_stack) from [<c0301570>] (bad_page+0xc8/0x128)
    [<c0301570>] (bad_page) from [<c03018a8>] (free_pages_prepare+0x168/0x1e0)
    [<c03018a8>] (free_pages_prepare) from [<c030369c>] (free_hot_cold_page+0x3c/0x174)
    [<c030369c>] (free_hot_cold_page) from [<c0303828>] (__free_pages+0x54/0x58)
    [<c0303828>] (__free_pages) from [<c030395c>] (free_highmem_page+0x38/0x88)
    [<c030395c>] (free_highmem_page) from [<c0f62d5c>] (mem_init+0x240/0x430)
    [<c0f62d5c>] (mem_init) from [<c0f5db3c>] (start_kernel+0x1e4/0x3c8)
    [<c0f5db3c>] (start_kernel) from [<80208074>] (0x80208074)
    Disabling lock debugging due to kernel taint
    
    Removing the lower 2MB made the start of the lowmem zone to no longer
    be page block aligned. IFC6410 uses CONFIG_FLATMEM where
    alloc_node_mem_map allocates memory for the mem_map. alloc_node_mem_map
    will offset for unaligned nodes with the assumption the pfn/page
    translation functions will account for the offset. The functions for
    CONFIG_FLATMEM do not offset however, resulting in overrunning
    the memmap array. Just use the allocated memmap without any offset
    when running with CONFIG_FLATMEM to avoid the overrun.
    
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>
    Reported-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4996,6 +4996,8 @@ static void __paginginit free_area_init_core(struct pglist_data *pgdat,
 
 static void __init_refok alloc_node_mem_map(struct pglist_data *pgdat)
 {
+	unsigned long __maybe_unused offset = 0;
+
 	/* Skip empty nodes */
 	if (!pgdat->node_spanned_pages)
 		return;
@@ -5012,6 +5014,7 @@ static void __init_refok alloc_node_mem_map(struct pglist_data *pgdat)
 		 * for the buddy allocator to function correctly.
 		 */
 		start = pgdat->node_start_pfn & ~(MAX_ORDER_NR_PAGES - 1);
+		offset = pgdat->node_start_pfn - start;
 		end = pgdat_end_pfn(pgdat);
 		end = ALIGN(end, MAX_ORDER_NR_PAGES);
 		size =  (end - start) * sizeof(struct page);
@@ -5019,7 +5022,7 @@ static void __init_refok alloc_node_mem_map(struct pglist_data *pgdat)
 		if (!map)
 			map = memblock_virt_alloc_node_nopanic(size,
 							       pgdat->node_id);
-		pgdat->node_mem_map = map + (pgdat->node_start_pfn - start);
+		pgdat->node_mem_map = map + offset;
 	}
 #ifndef CONFIG_NEED_MULTIPLE_NODES
 	/*
@@ -5027,10 +5030,12 @@ static void __init_refok alloc_node_mem_map(struct pglist_data *pgdat)
 	 */
 	if (pgdat == NODE_DATA(0)) {
 		mem_map = NODE_DATA(0)->node_mem_map;
-#ifdef CONFIG_HAVE_MEMBLOCK_NODE_MAP
-		if (page_to_pfn(mem_map) != pgdat->node_start_pfn)
-			mem_map -= (pgdat->node_start_pfn - ARCH_PFN_OFFSET);
-#endif /* CONFIG_HAVE_MEMBLOCK_NODE_MAP */
+#if defined(CONFIG_HAVE_MEMBLOCK_NODE_MAP) || defined(CONFIG_FLATMEM)
+		if (page_to_pfn(mem_map) != pgdat->node_start_pfn) {
+			mem_map -= offset;
+			VM_BUG_ON(page_to_pfn(mem_map) != pgdat->node_start_pfn);
+		}
+#endif /* CONFIG_HAVE_MEMBLOCK_NODE_MAP || CONFIG_FLATMEM */
 	}
 #endif
 #endif /* CONFIG_FLAT_NODE_MEM_MAP */
