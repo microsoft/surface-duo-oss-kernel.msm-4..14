fae3725186df9af62bf10629f0d54aa35c32c792
Stephen Boyd <sboyd@codeaurora.org>
2015-03-20 23:45:23 -0700

    clk: Add safe switch hook
    Sometimes clocks can't accept their parent source turning off
    while the source is reprogrammed to a different rate. Most
    notably CPU clocks require a way to switch away from the current
    PLL they're running on, reprogram that PLL to a new rate, and
    then switch back to the PLL with the new rate once they're done.
    Add a hook that drivers can implement allowing them to return a
    'safe parent' that they can switch their parent to while the
    upstream source is reprogrammed to support this.
    
    Signed-off-by: Stephen Boyd <sboyd@codeaurora.org>

--- include/linux/clk-provider.h
+++ include/linux/clk-provider.h
@@ -202,6 +202,7 @@ struct clk_ops {
 					  struct clk_rate_request *req);
 	int		(*set_parent)(struct clk_hw *hw, u8 index);
 	u8		(*get_parent)(struct clk_hw *hw);
+	struct clk_hw	*(*get_safe_parent)(struct clk_hw *hw);
 	int		(*set_rate)(struct clk_hw *hw, unsigned long rate,
 				    unsigned long parent_rate);
 	int		(*set_rate_and_parent)(struct clk_hw *hw,
