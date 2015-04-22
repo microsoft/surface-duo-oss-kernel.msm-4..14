8bd2350993b3b499ac091159e3b509eea54831b5
Andy Gross <agross@codeaurora.org>
2015-04-22 00:14:51 -0500

    soc: qcom: smd: Fix fifo copies
    This patch makes sure we do 32 bit accesses to the FIFO addresses.  The FIFO
    address space cannot handle >32 bit access.
    
    Signed-off-by: Andy Gross <agross@codeaurora.org>

--- drivers/soc/qcom/smd.c
+++ drivers/soc/qcom/smd.c
@@ -367,6 +367,32 @@ static void qcom_smd_channel_set_state(struct qcom_smd_channel *channel,
 }
 
 /*
+ * Copy count bytes of data from memory to device memory using 32bit accesses
+ */
+static void smd_copy_to_fifo(void __iomem *dst, const void *src, size_t count)
+{
+        while (count) {
+                __raw_writel(*(u32 *)src, dst);
+		dst += sizeof(u32);
+		src += sizeof(u32);
+		count -= sizeof(u32);
+	}
+}
+
+/*
+ * Copy count bytes of data from device memory to memory using 32bit accesses
+ */
+static void smd_copy_from_fifo(void *dst, const void __iomem *src, size_t count)
+{
+        while (count) {
+                *(u32 *)dst = __raw_readl(src);
+		src += sizeof(u32);
+		dst += sizeof(u32);
+		count -= sizeof(u32);
+	}
+}
+
+/*
  * Read count bytes of data from the rx fifo into buf, but don't advance the
  * tail.
  */
@@ -380,10 +406,10 @@ static size_t qcom_smd_channel_peek(struct qcom_smd_channel *channel,
 
 	len = min_t(size_t, count, channel->fifo_size - tail);
 	if (len)
-		memcpy(buf, channel->rx_fifo + tail, len);
+		smd_copy_from_fifo(buf, channel->rx_fifo + tail, len);
 
 	if (len != count)
-		memcpy(buf + len, channel->rx_fifo, count - len);
+		smd_copy_from_fifo(buf + len, channel->rx_fifo, count - len);
 
 	return count;
 }
@@ -585,10 +611,10 @@ static int qcom_smd_write_fifo(struct qcom_smd_channel *channel,
 
 	len = min_t(size_t, count, channel->fifo_size - head);
 	if (len)
-		memcpy(channel->tx_fifo + head, data, len);
+		smd_copy_to_fifo(channel->tx_fifo + head, data, len);
 
 	if (len != count)
-		memcpy(channel->tx_fifo, data + len, count - len);
+		smd_copy_to_fifo(channel->tx_fifo, data + len, count - len);
 
 	head += count;
 	head &= (channel->fifo_size - 1);
