6d313411b55e770091b6401622292f373b1e701b
Stanimir Varbanov <stanimir.varbanov@linaro.org>
2015-04-27 15:59:43 +0300

    firmware: qcom: scm: Support PIL SCMs
    Signed-off-by: Stanimir Varbanov <stanimir.varbanov@linaro.org>
    
    Conflicts:
    	drivers/firmware/qcom_scm-32.c
    	drivers/firmware/qcom_scm.c
    	drivers/firmware/qcom_scm.h
    
    Conflicts:
    	drivers/firmware/qcom_scm-32.c
    	drivers/firmware/qcom_scm-64.c
    	drivers/firmware/qcom_scm.c

--- drivers/firmware/qcom_scm.h
+++ drivers/firmware/qcom_scm.h
@@ -79,4 +79,18 @@ static inline int qcom_scm_remap_error(int err)
 	return -EINVAL;
 }
 
+enum scm_cmd {
+	PAS_INIT_IMAGE_CMD = 1,
+	PAS_MEM_SETUP_CMD,
+	PAS_AUTH_AND_RESET_CMD = 5,
+	PAS_SHUTDOWN_CMD,
+};
+
+#define SCM_SVC_PIL	0x2
+
+extern int __qcom_scm_pil_init_image_cmd(u32 proc, u64 image_addr);
+extern int __qcom_scm_pil_mem_setup_cmd(u32 proc, u64 start_addr, u32 len);
+extern int __qcom_scm_pil_auth_and_reset_cmd(u32 proc);
+extern int __qcom_scm_pil_shutdown_cmd(u32 proc);
+
 #endif
