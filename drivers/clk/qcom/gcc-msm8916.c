adc094f7c219766fcfc14091a3e777305992ce7e
Georgi Djakov <georgi.djakov@linaro.org>
2015-04-29 14:52:46 +0300

    clk: qcom: Fix MSM8916 venus divider value
    One of the video codec clock frequencies has incorrect divider value. Fix it.
    
    Fixes: 3966fab8b6ab "clk: qcom: Add MSM8916 Global Clock Controller support"
    Signed-off-by: Georgi Djakov <georgi.djakov@linaro.org>

--- drivers/clk/qcom/gcc-msm8916.c
+++ drivers/clk/qcom/gcc-msm8916.c
@@ -1497,7 +1497,7 @@ static struct clk_branch gcc_ultaudio_pcnoc_sway_clk = {
 static const struct freq_tbl ftbl_gcc_venus0_vcodec0_clk[] = {
 	F(100000000, P_GPLL0, 8, 0, 0),
 	F(160000000, P_GPLL0, 5, 0, 0),
-	F(228570000, P_GPLL0, 5, 0, 0),
+	F(228570000, P_GPLL0, 3.5, 0, 0),
 	{ }
 };
 
