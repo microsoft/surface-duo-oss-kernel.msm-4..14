779f1abd6b655daa7ea6ad01a4b1b0a1dbe4ae1e
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2015-05-15 02:00:06 +0300

    of: dma: Split of_configure_dma() into mask and ops configuration
    The of_configure_dma() function configures both the DMA masks and ops.
    Moving DMA ops configuration to probe time would thus also delay
    configuration of the DMA masks, which might not be safe. To avoid issues
    split the configuration in two to allow keeping masks configuration at
    device add time and move ops configuration to device probe time.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>

--- drivers/of/platform.c
+++ drivers/of/platform.c
@@ -179,7 +179,8 @@ static struct platform_device *of_platform_device_create_pdata(
 
 	dev->dev.bus = &platform_bus_type;
 	dev->dev.platform_data = platform_data;
-	of_dma_configure(&dev->dev, dev->dev.of_node);
+	of_dma_configure_masks(&dev->dev, dev->dev.of_node);
+	of_dma_configure_ops(&dev->dev, dev->dev.of_node);
 	of_msi_configure(&dev->dev, dev->dev.of_node);
 
 	if (of_device_add(dev) != 0) {
@@ -243,7 +244,8 @@ static struct amba_device *of_amba_device_create(struct device_node *node,
 		dev_set_name(&dev->dev, "%s", bus_id);
 	else
 		of_device_make_bus_id(&dev->dev);
-	of_dma_configure(&dev->dev, dev->dev.of_node);
+	of_dma_configure_masks(&dev->dev, dev->dev.of_node);
+	of_dma_configure_ops(&dev->dev, dev->dev.of_node);
 
 	/* Allow the HW Peripheral ID to be overridden */
 	prop = of_get_property(node, "arm,primecell-periphid", NULL);
