2c8819996b84dccc89613ce2d91bdfecc5ac0659
Patrick Bellasi <patrick.bellasi@arm.com>
2015-05-16 23:49:32 +0100

    blmp: migration strategy: FORK
    Goals:
    1. spawn new tasks on big cores, but
    2. keep kernel threads and init task direct childs on the slower domain
    
    System services are generally started by init, whilst kernel threads are
    started by kthreadd. We do not want to give those tasks a head start, as
    this costs power for very little benefit. We do however wish to do that
    for tasks which the user launches.
    
    Further, some tasks allocate per-cpu timers directly after launch which
    can lead to those tasks being always scheduled on a big CPU when there
    is no computational need to do so. Not promoting services to big CPUs on
    launch will prevent that unless a service allocates their per-cpu
    resources after a period of intense computation, which is not a common
    pattern.
    
    This is part of a consolidation of patches originally authored by:
    Chris Redpath <chris.redpath@arm.com>
    Dietmar Eggemann <dietmar.eggemann@arm.com>
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Signed-off-by: Jon Medhurst <tixy@linaro.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1825,11 +1825,18 @@ static void __sched_fork(unsigned long clone_flags, struct task_struct *p)
 	INIT_LIST_HEAD(&p->se.group_node);
 
 #ifdef CONFIG_SCHED_HMP
+	/* keep LOAD_AVG_MAX in sync with fair.c if load avg series is changed */
+#define LOAD_AVG_MAX 47742
 	p->se.avg.hmp_last_up_migration = 0;
 	p->se.avg.hmp_last_down_migration = 0;
+	if (hmp_task_should_forkboost(p)) {
 		p->se.avg.load_avg_ratio = 1023;
 		p->se.avg.load_avg_contrib =
 				(1023 * scale_load_down(p->se.load.weight));
+		p->se.avg.runnable_avg_period = LOAD_AVG_MAX;
+		p->se.avg.runnable_avg_sum = LOAD_AVG_MAX;
+		p->se.avg.usage_avg_sum = LOAD_AVG_MAX;
+	}
 #endif
 #ifdef CONFIG_SCHEDSTATS
 	memset(&p->se.statistics, 0, sizeof(p->se.statistics));
