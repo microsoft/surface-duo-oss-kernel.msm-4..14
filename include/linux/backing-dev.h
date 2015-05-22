283c54c84705970a89d0a1e7c6f1e0a855fcb8e0
Tejun Heo <tj@kernel.org>
2015-05-22 17:13:51 -0400

    writeback: remove bdi_start_writeback()
    bdi_start_writeback() is a thin wrapper on top of
    __wb_start_writeback() which is used only by laptop_mode_timer_fn().
    This patches removes bdi_start_writeback(), renames
    __wb_start_writeback() to wb_start_writeback() and makes
    laptop_mode_timer_fn() use it instead.
    
    This doesn't cause any functional difference and will ease making
    laptop_mode_timer_fn() cgroup writeback aware.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Jan Kara <jack@suse.cz>
    Signed-off-by: Jens Axboe <axboe@fb.com>
    (cherry picked from commit c00ddad39f512b1a81e25b7892217ce10efab0f1)
    Signed-off-by: Alex Shi <alex.shi@linaro.org>

--- include/linux/backing-dev.h
+++ include/linux/backing-dev.h
@@ -24,8 +24,8 @@ int bdi_register(struct backing_dev_info *bdi, struct device *parent,
 		const char *fmt, ...);
 int bdi_register_dev(struct backing_dev_info *bdi, dev_t dev);
 int __must_check bdi_setup_and_register(struct backing_dev_info *, char *);
-void bdi_start_writeback(struct backing_dev_info *bdi, long nr_pages,
-			enum wb_reason reason);
+void wb_start_writeback(struct bdi_writeback *wb, long nr_pages,
+			bool range_cyclic, enum wb_reason reason);
 void bdi_start_background_writeback(struct backing_dev_info *bdi);
 void wb_workfn(struct work_struct *work);
 void wb_wakeup_delayed(struct bdi_writeback *wb);
