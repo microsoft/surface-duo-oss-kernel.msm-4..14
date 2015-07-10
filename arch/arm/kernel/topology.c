0ad0e440f4ade075cfe3ee047a647cd8d17efa7b
Dietmar Eggemann <dietmar.eggemann@arm.com>
2015-07-10 13:57:19 +0100

    ANDROID: arm: Cpu invariant scheduler load-tracking and capacity support
    Provides the scheduler with a cpu scaling correction factor for more
    accurate load-tracking and cpu capacity handling.
    
    The Energy Model (EM) (in fact the capacity value of the last element
    of the capacity states vector of the core (MC) level sched_group_energy
    structure) is used instead of the arm arch specific cpu_efficiency and
    dtb property 'clock-frequency' values as the source for this cpu
    scaling factor.
    
    The cpu capacity value depends on the micro-architecture and the
    maximum frequency of the cpu.
    
    The maximum frequency part should not be confused with the frequency
    invariant scheduler load-tracking support which deals with frequency
    related scaling due to DFVS functionality.
    
    Signed-off-by: Juri Lelli <juri.lelli@arm.com>
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Andres Oportus <andresoportus@google.com>
    [AmitP: Refactored the original changes to align with upstream changes
            06073ee26775 ("ARM: 8621/3: parse cpu capacity-dmips-mhz from DT")]
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- arch/arm/kernel/topology.c
+++ arch/arm/kernel/topology.c
@@ -371,6 +371,8 @@ static void __init parse_dt_topology(void)
 		normalize_cpu_capacity();
 }
 
+static const struct sched_group_energy * const cpu_core_energy(int cpu);
+
 /*
  * Look for a customed capacity of a CPU in the cpu_capacity table during the
  * boot. The update of all CPUs is in O(n^2) for heteregeneous system but the
@@ -378,10 +380,17 @@ static void __init parse_dt_topology(void)
  */
 static void update_cpu_capacity(unsigned int cpu)
 {
-	if (!cpu_capacity(cpu) || cap_from_dt)
+	if (cap_from_dt)
 		return;
 
-	set_capacity_scale(cpu, cpu_capacity(cpu) / middle_capacity);
+	unsigned long capacity = SCHED_CAPACITY_SCALE;
+
+	if (cpu_core_energy(cpu)) {
+		int max_cap_idx = cpu_core_energy(cpu)->nr_cap_states - 1;
+		capacity = cpu_core_energy(cpu)->cap_states[max_cap_idx].cap;
+	}
+
+	set_capacity_scale(cpu, capacity);
 
 	pr_info("CPU%u: update cpu_capacity %lu\n",
 		cpu, arch_scale_cpu_capacity(NULL, cpu));
