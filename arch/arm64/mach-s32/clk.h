26e432b4f2a5cd33066bc91f60a258f39705160d
Stoica Cosmin-Stefan <cosminstefan.stoica@freescale.com>
2015-07-28 18:49:53 +0300

    Clock updates to compute correctly the freq for Linflex
    Signed-off-by: Stoica Cosmin-Stefan <cosminstefan.stoica@freescale.com>

--- arch/arm64/mach-s32/clk.h
+++ arch/arm64/mach-s32/clk.h
@@ -27,14 +27,15 @@ enum s32_plldig_type {
 };
 
 struct clk *s32_clk_plldig(enum s32_plldig_type type, const char *name,
-			       const char *parent_name, void __iomem *base,
-			       u32 plldv_mfd, u32 plldv_mfn,
-				   u32 plldv_rfdphi, u32 plldv_rfdphi1);
+			   const char *parent_name, void __iomem *base,
+			   u32 plldv_mfd, u32 plldv_mfn,
+			   u32 plldv_rfdphi, u32 plldv_rfdphi1);
 
 struct clk *s32_clk_plldig_phi(enum s32_plldig_type type, const char *name,
 			       const char *parent, void __iomem *base,
 			       u32 phi);
-struct clk *s32_clk_dfs(enum s32_plldig_type type, const char *name, const char *parent_name,
+struct clk *s32_clk_dfs(enum s32_plldig_type type, const char *name,
+			const char *parent_name,
 			void __iomem *reg, u8 idx, u32 mfn);
 
 struct clk *clk_register_gate2(struct device *dev, const char *name,
@@ -86,31 +87,13 @@ static inline struct clk *s32_clk_fixed(const char *name, int rate)
 }
 
 static inline struct clk *s32_clk_divider(const char *name, const char *parent,
-		void __iomem *reg, u8 shift, u8 width, u32 enable_val, u32 divider)
+		void __iomem *reg, u8 shift, u8 width)
 {
 	struct clk * tmp_clk = clk_register_divider(NULL, name, parent, CLK_SET_RATE_PARENT,
 			reg, shift, width, 0, &s32_cgm_lock);
-	
-	if( tmp_clk )
-	{
-		unsigned long parent_rate = clk_get_rate(tmp_clk);
-		long divn = divider >> 16;
-		#if 0
-		/* enable the clock divider */
-		if (!mutex_trylock(&s32_cgm_lock)) {
-			mutex_lock(&s32_cgm_lock);
-		}
-		#endif
-		printk("[Divider]  Parent rate %ld - divider value %ld -div rate %ld \n", parent_rate, divn, parent_rate / (divn + 1));
-		clk_set_rate(tmp_clk, parent_rate / (divn + 1));
-		writel_relaxed( readl_relaxed(reg) | enable_val | divider , reg);
-		#if 0
-		mutex_unlock(&s32_cgm_lock);
-		#endif
-	}
 
 	return tmp_clk;
-		
+
 }
 
 static inline struct clk *s32_clk_divider_flags(const char *name,
