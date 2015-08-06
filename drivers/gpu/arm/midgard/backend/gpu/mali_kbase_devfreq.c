47b19ee64d0ebcc3a0b1a0fa83600bc7908f8468
Javi Merino <javi.merino@arm.com>
2015-08-06 11:52:14 +0100

    mali: don't update devfreq's last_status
    This is now done in the simple_ondemand devfreq governor.
    
    Signed-off-by: Javi Merino <javi.merino@arm.com>
    Signed-off-by: Jon Medhurst <tixy@linaro.org>

--- drivers/gpu/arm/midgard/backend/gpu/mali_kbase_devfreq.c
+++ drivers/gpu/arm/midgard/backend/gpu/mali_kbase_devfreq.c
@@ -129,10 +129,6 @@ kbase_devfreq_status(struct device *dev, struct devfreq_dev_status *stat)
 
 	stat->private_data = NULL;
 
-#ifdef CONFIG_DEVFREQ_THERMAL
-	memcpy(&kbdev->devfreq_cooling->last_status, stat, sizeof(*stat));
-#endif
-
 	return 0;
 }
 
