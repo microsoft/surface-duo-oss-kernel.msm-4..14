406f458fdd11b182ba8c0fa295d60bb197465cce
Archit Taneja <architt@codeaurora.org>
2015-09-04 12:37:02 +0530

    HACK: drm/msm/mdp5: Make sure mdp5 clocks aren't disbaled when we need them
    Comment out mdp5_disable() calls in mdp5_hw_init and mdp5_kms_init.
    For now, this makes sure we have clocks all the time. This will be
    removed when runtime pm is added, and we identify all paths where we
    risk clocks being disabled when they shouldn't.
    
    Signed-off-by: Archit Taneja <architt@codeaurora.org>

--- drivers/gpu/drm/msm/mdp/mdp5/mdp5_kms.c
+++ drivers/gpu/drm/msm/mdp/mdp5/mdp5_kms.c
@@ -432,7 +432,7 @@ static void read_hw_revision(struct mdp5_kms *mdp5_kms,
 
 	mdp5_enable(mdp5_kms);
 	version = mdp5_read(mdp5_kms, REG_MDSS_HW_VERSION);
-	mdp5_disable(mdp5_kms);
+	//mdp5_disable(mdp5_kms);
 
 	*major = FIELD(version, MDSS_HW_VERSION_MAJOR);
 	*minor = FIELD(version, MDSS_HW_VERSION_MINOR);
@@ -703,7 +703,8 @@ struct msm_kms *mdp5_kms_init(struct drm_device *dev)
 
 		mdp5_write(mdp5_kms, REG_MDP5_INTF_FRAME_LINE_COUNT_EN(i), 0x3);
 	}
-	mdp5_disable(mdp5_kms);
+	/* TODO: Remove this after runtime pm adaptation */
+	//mdp5_disable(mdp5_kms);
 	mdelay(16);
 
 	if (config->platform.iommu) {
