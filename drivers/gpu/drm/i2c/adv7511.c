f027e2fe115e246ac81023c8e1d0f9643966e51d
Archit Taneja <architt@codeaurora.org>
2015-09-07 11:45:02 +0530

    drm/i2c: adv7511: Move the common data structures to header file
    This patch moves the adv7511 data structure to header file so that the
    audio driver file could use it.
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>

--- drivers/gpu/drm/i2c/adv7511.c
+++ drivers/gpu/drm/i2c/adv7511.c
@@ -24,52 +24,6 @@
 
 #include "adv7511.h"
 
-enum adv7511_type {
-	ADV7511,
-	ADV7533,
-};
-
-struct adv7511 {
-	struct i2c_client *i2c_main;
-	struct i2c_client *i2c_edid;
-	struct i2c_client *i2c_cec;
-
-	struct regmap *regmap;
-	struct regmap *regmap_cec;
-	enum drm_connector_status status;
-	bool powered;
-
-	struct drm_display_mode curr_mode;
-
-	unsigned int f_tmds;
-
-	unsigned int current_edid_segment;
-	uint8_t edid_buf[256];
-	bool edid_read;
-
-	wait_queue_head_t wq;
-	struct drm_encoder *encoder;
-
-	struct drm_connector connector;
-	struct drm_bridge bridge;
-
-	bool embedded_sync;
-	enum adv7511_sync_polarity vsync_polarity;
-	enum adv7511_sync_polarity hsync_polarity;
-	bool rgb;
-
-	struct edid *edid;
-
-	struct gpio_desc *gpio_pd;
-
-	/* ADV7533 DSI RX related params */
-	struct device_node *host_node;
-	struct mipi_dsi_device *dsi;
-	u8 num_dsi_lanes;
-
-	enum adv7511_type type;
-};
-
 static struct adv7511 *encoder_to_adv7511(struct drm_encoder *encoder)
 {
 	return to_encoder_slave(encoder)->slave_priv;
@@ -241,7 +195,7 @@ static void adv7511_set_colormap(struct adv7511 *adv7511, bool enable,
 			   ADV7511_CSC_UPDATE_MODE, 0);
 }
 
-static int adv7511_packet_enable(struct adv7511 *adv7511, unsigned int packet)
+int adv7511_packet_enable(struct adv7511 *adv7511, unsigned int packet)
 {
 	if (packet & 0xff)
 		regmap_update_bits(adv7511->regmap, ADV7511_REG_PACKET_ENABLE0,
@@ -256,7 +210,7 @@ static int adv7511_packet_enable(struct adv7511 *adv7511, unsigned int packet)
 	return 0;
 }
 
-static int adv7511_packet_disable(struct adv7511 *adv7511, unsigned int packet)
+int adv7511_packet_disable(struct adv7511 *adv7511, unsigned int packet)
 {
 	if (packet & 0xff)
 		regmap_update_bits(adv7511->regmap, ADV7511_REG_PACKET_ENABLE0,
