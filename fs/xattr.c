d9bfe7b6bcceb92bcd098bfb0f2634446783d9da
Seth Forshee <seth.forshee@canonical.com>
2016-01-19 13:12:02 -0600

    UBUNTU: SAUCE: overlayfs: Skip permission checking for trusted.overlayfs.* xattrs
    The original mounter had CAP_SYS_ADMIN in the user namespace
    where the mount happened, and the vfs has validated that the user
    has permission to do the requested operation. This is sufficient
    for allowing the kernel to write these specific xattrs, so we can
    bypass the permission checks for these xattrs.
    
    To support this, export __vfs_setxattr_noperm and add an similar
    __vfs_removexattr_noperm which is also exported. Use these when
    setting or removing trusted.overlayfs.* xattrs.
    
    BugLink: http://bugs.launchpad.net/bugs/1531747
    BugLink: http://bugs.launchpad.net/bugs/1534961
    BugLink: http://bugs.launchpad.net/bugs/1535150
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>

--- fs/xattr.c
+++ fs/xattr.c
@@ -203,6 +203,7 @@ int __vfs_setxattr_noperm(struct dentry *dentry, const char *name,
 
 	return error;
 }
+EXPORT_SYMBOL_GPL(__vfs_setxattr_noperm);
 
 /**
  * __vfs_setxattr_locked: set an extended attribute while holding the inode
@@ -416,6 +417,33 @@ __vfs_removexattr(struct dentry *dentry, const char *name)
 EXPORT_SYMBOL(__vfs_removexattr);
 
 /**
+ *  __vfs_removexattr_noperm - perform removexattr operation without
+ *  performing permission checks.
+ *
+ *  @dentry - object to perform setxattr on
+ *  @name - xattr name to set
+ *
+ *  returns the result of the internal setxattr or setsecurity operations.
+ *
+ *  This function requires the caller to lock the inode's i_mutex before it
+ *  is executed. It also assumes that the caller will make the appropriate
+ *  permission checks.
+ */
+int
+__vfs_removexattr_noperm(struct dentry *dentry, const char *name)
+{
+	int error;
+
+	error =__vfs_removexattr(dentry, name);
+	if (!error) {
+		fsnotify_xattr(dentry);
+		evm_inode_post_removexattr(dentry, name);
+	}
+	return error;
+}
+EXPORT_SYMBOL_GPL(__vfs_removexattr_noperm);
+
+/**
  * __vfs_removexattr_locked: set an extended attribute while holding the inode
  * lock
  *
@@ -443,12 +471,7 @@ __vfs_removexattr_locked(struct dentry *dentry, const char *name,
 	if (error)
 		goto out;
 
-	error = __vfs_removexattr(dentry, name);
-
-	if (!error) {
-		fsnotify_xattr(dentry);
-		evm_inode_post_removexattr(dentry, name);
-	}
+	error = __vfs_removexattr_noperm(dentry, name);
 
 out:
 	return error;
