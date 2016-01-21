df5b4b279a287a031bd4e8366d96fdafd87a998c
Xinliang Liu <xinliang.liu@linaro.org>
2016-01-21 15:05:16 +0800

    regulator: hi6220: Revert to 4.1 kernel mtcmos driver
    This is temporary. 4.4 kernel mtcmos driver is not working
    and need to be fixed.
    
    Signed-off-by: Xinliang Liu <xinliang.liu@linaro.org>
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- drivers/regulator/hi6220-mtcmos.c
+++ drivers/regulator/hi6220-mtcmos.c
@@ -1,16 +1,9 @@
 /*
- * Device driver for regulators in hi6220 mtcmos
+ * Device driver for MTCMOS DRIVER	 in HI6220 SOC
  *
- * Copyright (c) 2015 Hisilicon.
+ * Copyright (c) 2011 Hisilicon Co. Ltd
  *
- * Fei Wang <w.f@huawei.com>
- * Chen Feng <puck.chen@hisilicon.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
  */
-
 #include <linux/slab.h>
 #include <linux/device.h>
 #include <linux/module.h>
@@ -24,7 +17,8 @@
 #include <linux/regulator/driver.h>
 #include <linux/regulator/machine.h>
 #include <linux/regulator/of_regulator.h>
-#include <linux/sizes.h>
+#include <linux/delay.h>
+#include <linux/time.h>
 
 enum {
 	HI6220_MTCMOS1,
@@ -52,79 +46,105 @@ struct hi6220_mtcmos_info {
 struct hi6220_mtcmos {
 	struct regulator_dev *rdev[HI6220_RG_MAX];
 	void __iomem *sc_on_regs;
+	int mtcmos_steady_time;
+	spinlock_t mtcmos_spin_lock;
 };
 
 static int hi6220_mtcmos_is_on(struct hi6220_mtcmos *mtcmos,
-			       unsigned int regs, unsigned int mask, int shift)
+				unsigned int regs, unsigned int mask, int shift)
 {
 	unsigned int ret;
+	unsigned long mtcmos_spin_flag = 0;
 
+	spin_lock_irqsave(&mtcmos->mtcmos_spin_lock, mtcmos_spin_flag);
 	ret = readl(mtcmos->sc_on_regs + regs);
+	spin_unlock_irqrestore(&mtcmos->mtcmos_spin_lock, mtcmos_spin_flag);
+
 	ret &= (mask << shift);
-
-	return ret;
+	return !!ret;
 }
 
-static int hi6220_mtcmos_is_enabled(struct regulator_dev *rdev)
+int hi6220_mtcmos_on(struct hi6220_mtcmos *mtcmos,
+				unsigned int regs, unsigned int mask, int shift)
 {
-	int ret;
-	struct hi6220_mtcmos_info *sreg = rdev_get_drvdata(rdev);
-	struct platform_device *pdev =
-		container_of(rdev->dev.parent, struct platform_device, dev);
-	struct hi6220_mtcmos *mtcmos = platform_get_drvdata(pdev);
-	struct hi6220_mtcmos_ctrl_regs *ctrl_regs = &sreg->ctrl_regs;
-	struct hi6220_mtcmos_ctrl_data *ctrl_data = &sreg->ctrl_data;
+	unsigned long mtcmos_spin_flag = 0;
 
-	ret = hi6220_mtcmos_is_on(mtcmos, ctrl_regs->status_reg,
-				  ctrl_data->mask, ctrl_data->shift);
-	return ret;
-}
-
-static int hi6220_mtcmos_op(struct hi6220_mtcmos *mtcmos,
-		      unsigned int regs, unsigned int mask, int shift)
-{
+	spin_lock_irqsave(&mtcmos->mtcmos_spin_lock, mtcmos_spin_flag);
 	writel(mask << shift, mtcmos->sc_on_regs + regs);
+	udelay(mtcmos->mtcmos_steady_time);
+	spin_unlock_irqrestore(&mtcmos->mtcmos_spin_lock, mtcmos_spin_flag);
 
 	return 0;
 }
 
-static int hi6220_mtcmos_enable(struct regulator_dev *rdev)
+int hi6220_mtcmos_off(struct hi6220_mtcmos *mtcmos,
+			unsigned int regs, unsigned int mask, int shift)
+{
+	unsigned long mtcmos_spin_flag = 0;
+
+	spin_lock_irqsave(&mtcmos->mtcmos_spin_lock, mtcmos_spin_flag);
+	writel(mask << shift, mtcmos->sc_on_regs + regs);
+	udelay(mtcmos->mtcmos_steady_time);
+	spin_unlock_irqrestore(&mtcmos->mtcmos_spin_lock,
+		mtcmos_spin_flag);
+
+	return 0;
+}
+
+static int hi6220_regulator_mtcmos_is_enabled(struct regulator_dev *rdev)
 {
 	int ret;
 	struct hi6220_mtcmos_info *sreg = rdev_get_drvdata(rdev);
 	struct platform_device *pdev =
 		container_of(rdev->dev.parent, struct platform_device, dev);
 	struct hi6220_mtcmos *mtcmos = platform_get_drvdata(pdev);
-	struct hi6220_mtcmos_ctrl_regs *ctrl_regs = &sreg->ctrl_regs;
-	struct hi6220_mtcmos_ctrl_data *ctrl_data = &sreg->ctrl_data;
+	struct hi6220_mtcmos_ctrl_regs *ctrl_regs = &(sreg->ctrl_regs);
+	struct hi6220_mtcmos_ctrl_data *ctrl_data = &(sreg->ctrl_data);
 
-	hi6220_mtcmos_op(mtcmos, ctrl_regs->enable_reg,
-			 ctrl_data->mask, ctrl_data->shift);
-	ret =  hi6220_mtcmos_is_on(mtcmos, ctrl_regs->status_reg,
-				   ctrl_data->mask, ctrl_data->shift);
+	ret = hi6220_mtcmos_is_on(mtcmos, ctrl_regs->status_reg,
+					ctrl_data->mask, ctrl_data->shift);
 	return ret;
 }
 
-static int hi6220_mtcmos_disable(struct regulator_dev *rdev)
+static int hi6220_regulator_mtcmos_enabled(struct regulator_dev *rdev)
 {
 	int ret;
 	struct hi6220_mtcmos_info *sreg = rdev_get_drvdata(rdev);
 	struct platform_device *pdev =
 		container_of(rdev->dev.parent, struct platform_device, dev);
 	struct hi6220_mtcmos *mtcmos = platform_get_drvdata(pdev);
-	struct hi6220_mtcmos_ctrl_regs  *ctrl_regs = &sreg->ctrl_regs;
-	struct hi6220_mtcmos_ctrl_data  *ctrl_data = &sreg->ctrl_data;
+	struct hi6220_mtcmos_ctrl_regs *ctrl_regs = &(sreg->ctrl_regs);
+	struct hi6220_mtcmos_ctrl_data *ctrl_data = &(sreg->ctrl_data);
 
-	ret = hi6220_mtcmos_op(mtcmos, ctrl_regs->disable_reg,
-			       ctrl_data->mask, ctrl_data->shift);
+	ret =  hi6220_mtcmos_on(mtcmos, ctrl_regs->enable_reg,
+					ctrl_data->mask, ctrl_data->shift);
+	if (0 == hi6220_mtcmos_is_on(mtcmos, ctrl_regs->status_reg,
+					ctrl_data->mask, ctrl_data->shift)) {
+		return -1;
+	}
+	return ret;
+}
+
+static int hi6220_regulator_mtcmos_disabled(struct regulator_dev *rdev)
+{
+	int ret;
+	struct hi6220_mtcmos_info *sreg = rdev_get_drvdata(rdev);
+	struct platform_device *pdev =
+		container_of(rdev->dev.parent, struct platform_device, dev);
+	struct hi6220_mtcmos *mtcmos = platform_get_drvdata(pdev);
+	struct hi6220_mtcmos_ctrl_regs  *ctrl_regs = &(sreg->ctrl_regs);
+	struct hi6220_mtcmos_ctrl_data  *ctrl_data = &(sreg->ctrl_data);
+
+	ret = hi6220_mtcmos_off(mtcmos, ctrl_regs->disable_reg,
+					ctrl_data->mask, ctrl_data->shift);
 
 	return ret;
 }
 
 static struct regulator_ops hi6220_mtcmos_mtcmos_rops = {
-	.is_enabled = hi6220_mtcmos_is_enabled,
-	.enable = hi6220_mtcmos_enable,
-	.disable = hi6220_mtcmos_disable,
+	.is_enabled = hi6220_regulator_mtcmos_is_enabled,
+	.enable = hi6220_regulator_mtcmos_enabled,
+	.disable = hi6220_regulator_mtcmos_disabled,
 };
 
 #define HI6220_MTCMOS(vreg) \
@@ -157,32 +177,38 @@ static int hi6220_mtcmos_probe(struct platform_device *pdev)
 	void __iomem	*regs;
 	struct device *dev;
 	struct device_node *np, *child;
-	int i;
+	int count, i;
 	struct regulator_config config = { };
 	struct regulator_init_data *init_data;
 	struct hi6220_mtcmos_info *sreg;
-	u32 off_on_delay = 0;
 
 	dev = &pdev->dev;
 	np = dev->of_node;
-	mtcmos = devm_kzalloc(dev, sizeof(struct hi6220_mtcmos), GFP_KERNEL);
-	if (!mtcmos)
+	mtcmos = devm_kzalloc(dev,
+		sizeof(struct hi6220_mtcmos), GFP_KERNEL);
+	if (!mtcmos) {
+		dev_err(dev, "cannot allocate hi6220_mtcmos device info\n");
 		return -ENOMEM;
+	}
 
+	spin_lock_init((spinlock_t *)&mtcmos->mtcmos_spin_lock);
 	sc_on_regs = of_get_property(np, "hisilicon,mtcmos-sc-on-base", NULL);
 	if (sc_on_regs) {
-		regs = ioremap(be32_to_cpu(*sc_on_regs), SZ_4K);
+		regs = ioremap(be32_to_cpu(*sc_on_regs), 0x1000);
 		mtcmos->sc_on_regs = regs;
-	} else
-		return -ENODEV;
-	of_property_read_u32(np, "hisilicon,mtcmos-steady-us", &off_on_delay);
+	}
+	ret = of_property_read_u32(np, "hisilicon,mtcmos-steady-us",
+						&mtcmos->mtcmos_steady_time);
+
+	count = of_regulator_match(&pdev->dev, np,
+				 hi6220_mtcmos_matches,
+				 ARRAY_SIZE(hi6220_mtcmos_matches));
 
 	for (i = 0; i < HI6220_RG_MAX; i++) {
 		init_data = hi6220_mtcmos_matches[i].init_data;
 		if (!init_data)
 			continue;
 		sreg = hi6220_mtcmos_matches[i].driver_data;
-		sreg->rdesc.off_on_delay = off_on_delay;
 		config.dev = &pdev->dev;
 		config.init_data = init_data;
 		config.driver_data = sreg;
@@ -190,17 +216,15 @@ static int hi6220_mtcmos_probe(struct platform_device *pdev)
 		child = config.of_node;
 
 		ret = of_property_read_u32_array(child, "hisilicon,ctrl-regs",
-						 (u32 *)(&sreg->ctrl_regs),
-						 0x3);
+				(unsigned int *)(&sreg->ctrl_regs), 0x3);
 		ret = of_property_read_u32_array(child, "hisilicon,ctrl-data",
-						 (u32 *)(&sreg->ctrl_data),
-						 0x2);
+				(unsigned int *)(&sreg->ctrl_data), 0x2);
 
 		mtcmos->rdev[i] = regulator_register(&sreg->rdesc, &config);
 		if (IS_ERR(mtcmos->rdev[i])) {
 			ret = PTR_ERR(mtcmos->rdev[i]);
 			dev_err(&pdev->dev, "failed to register mtcmos %s\n",
-				sreg->rdesc.name);
+					sreg->rdesc.name);
 			while (--i >= 0)
 				regulator_unregister(mtcmos->rdev[i]);
 
@@ -213,7 +237,7 @@ static int hi6220_mtcmos_probe(struct platform_device *pdev)
 	return 0;
 }
 
-static const struct of_device_id of_hi6220_mtcmos_match_tbl[] = {
+static struct of_device_id of_hi6220_mtcmos_match_tbl[] = {
 	{ .compatible = "hisilicon,hi6220-mtcmos-driver", },
 	{}
 };
@@ -240,6 +264,6 @@ static void __exit hi6220_mtcmos_exit(void)
 fs_initcall(hi6220_mtcmos_init);
 module_exit(hi6220_mtcmos_exit);
 
-MODULE_AUTHOR("Fei Wang <w.f@huawei.com>");
-MODULE_DESCRIPTION("Hi6220 mtcmos interface driver");
-MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR("Baixing Quan<quanbaixing@huawei.com>");
+MODULE_DESCRIPTION("HI6220 MTCMOS interface driver");
+MODULE_LICENSE("GPL V2");
