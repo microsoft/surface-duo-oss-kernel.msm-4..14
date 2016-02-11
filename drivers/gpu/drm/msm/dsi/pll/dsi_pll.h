44821946022c3e5fbc7cf3c423f12760c52a379f
Archit Taneja <architt@codeaurora.org>
2016-02-11 16:10:58 +0530

    drm/msm/dsi: Don't access registers in 'is_enabled' PLL clk_ops
    Accessing registers for the DSI PLL 'is_enabled' op in clk_ops can result
    in problems if the DSI PHY's interface clock is disabled.
    
    We already have PLL state information available through the 'pll_on'
    member in the msm_dsi_pll struct. Use that instead.
    
    Signed-off-by: Archit Taneja <architt@codeaurora.org>

--- drivers/gpu/drm/msm/dsi/pll/dsi_pll.h
+++ drivers/gpu/drm/msm/dsi/pll/dsi_pll.h
@@ -76,6 +76,7 @@ long msm_dsi_pll_helper_clk_round_rate(struct clk_hw *hw,
 		unsigned long rate, unsigned long *parent_rate);
 int msm_dsi_pll_helper_clk_prepare(struct clk_hw *hw);
 void msm_dsi_pll_helper_clk_unprepare(struct clk_hw *hw);
+int msm_dsi_pll_helper_clk_is_enabled(struct clk_hw *hw);
 /* misc */
 void msm_dsi_pll_helper_unregister_clks(struct platform_device *pdev,
 					struct clk **clks, u32 num_clks);
