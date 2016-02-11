6673e777253d92b0ccdcf211ea692e847b0d228b
Archit Taneja <architt@codeaurora.org>
2016-02-11 16:04:13 +0530

    drm/i2c: adv7511: Add Audio support.
    This patch adds support to Audio for both adv7511 and adv7533 bridge
    chips. Content of this patch is has copied from [1].
    
    [1] https://github.com/analogdevicesinc/linux/blob/xcomm_zynq/drivers/gpu/drm/i2c/adv7511_audio.c
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    [Srinivas Kandagatla] Adapted to new adv7511 bridge driver.
    Signed-off-by: Archit Taneja <architt@codeaurora.org>

--- drivers/gpu/drm/i2c/adv7511.c
+++ drivers/gpu/drm/i2c/adv7511.c
@@ -1366,6 +1366,8 @@ static int adv7511_probe(struct i2c_client *i2c, const struct i2c_device_id *id)
 		goto err_i2c_unregister_cec;
 	}
 
+	adv7511_audio_init(dev);
+
 	return 0;
 
 err_i2c_unregister_cec:
@@ -1387,6 +1389,8 @@ static int adv7511_remove(struct i2c_client *i2c)
 	}
 
 	drm_bridge_remove(&adv7511->bridge);
+
+	adv7511_audio_exit(&i2c->dev);
 
 	i2c_unregister_device(adv7511->i2c_cec);
 	i2c_unregister_device(adv7511->i2c_edid);
