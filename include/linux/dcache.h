823042fec32969b3545533e84e43705b50bc6064
Daniel Rosenberg <drosen@google.com>
2016-02-11 16:44:15 -0800

    ANDROID: vfs: add d_canonical_path for stacked filesystem support
    Inotify does not currently know when a filesystem
    is acting as a wrapper around another fs. This means
    that inotify watchers will miss any modifications to
    the base file, as well as any made in a separate
    stacked fs that points to the same file.
    d_canonical_path solves this problem by allowing the fs
    to map a dentry to a path in the lower fs. Inotify
    can use it to find the appropriate place to watch to
    be informed of all changes to a file.
    
    Change-Id: I09563baffad1711a045e45c1bd0bd8713c2cc0b6
    Signed-off-by: Daniel Rosenberg <drosen@google.com>

--- include/linux/dcache.h
+++ include/linux/dcache.h
@@ -149,6 +149,7 @@ struct dentry_operations {
 	int (*d_manage)(const struct path *, bool);
 	struct dentry *(*d_real)(struct dentry *, const struct inode *,
 				 unsigned int, unsigned int);
+	void (*d_canonical_path)(const struct dentry *, struct path *);
 } ____cacheline_aligned;
 
 /*
@@ -267,7 +268,7 @@ extern int path_has_submounts(const struct path *);
  * This adds the entry to the hash queues.
  */
 extern void d_rehash(struct dentry *);
- 
+
 extern void d_add(struct dentry *, struct inode *);
 
 extern void dentry_update_name_case(struct dentry *, const struct qstr *);
@@ -309,7 +310,7 @@ extern char *dentry_path(struct dentry *, char *, int);
  *	@dentry: dentry to get a reference to
  *
  *	Given a dentry or %NULL pointer increment the reference count
- *	if appropriate and return the dentry. A dentry will not be 
+ *	if appropriate and return the dentry. A dentry will not be
  *	destroyed when it has references.
  */
 static inline struct dentry *dget_dlock(struct dentry *dentry)
@@ -334,7 +335,7 @@ extern struct dentry *dget_parent(struct dentry *dentry);
  *
  *	Returns true if the dentry passed is not currently hashed.
  */
- 
+
 static inline int d_unhashed(const struct dentry *dentry)
 {
 	return hlist_bl_unhashed(&dentry->d_hash);
