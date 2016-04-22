6d8a463f8f186112b2b227c10ae0718938b2f483
Sricharan R <sricharan@codeaurora.org>
2016-04-22 14:53:49 +0530

    drivers: arm-smmu: temp: Enable clocks and regulators
    This is a temporary fix, not required for upstream.
    
    Signed-off-by: Sricharan R <sricharan@codeaurora.org>

--- drivers/iommu/arm-smmu.c
+++ drivers/iommu/arm-smmu.c
@@ -377,6 +377,9 @@ struct arm_smmu_device {
 	struct rb_root			masters;
 
 	u32				cavium_id_base; /* Specific to Cavium */
+	int				num_clocks;
+	struct clk			**clocks;
+	struct regulator		*regulator;
 };
 
 enum arm_smmu_context_fmt {
@@ -438,6 +441,32 @@ static struct arm_smmu_domain *to_smmu_domain(struct iommu_domain *dom)
 {
 	return container_of(dom, struct arm_smmu_domain, domain);
 }
+
+static int arm_smmu_enable_clocks(struct arm_smmu_device *smmu)
+{
+       int i, ret = 0;
+
+       for (i = 0; i < smmu->num_clocks; ++i) {
+               ret = clk_prepare_enable(smmu->clocks[i]);
+               if (ret) {
+                       dev_err(smmu->dev, "Couldn't enable clock #%d\n", i);
+                       while (i--)
+                               clk_disable_unprepare(smmu->clocks[i]);
+                       break;
+               }
+       }
+
+       return ret;
+}
+
+static void arm_smmu_disable_clocks(struct arm_smmu_device *smmu)
+{
+       int i;
+
+       for (i = 0; i < smmu->num_clocks; ++i)
+               clk_disable_unprepare(smmu->clocks[i]);
+}
+
 
 static void parse_driver_options(struct arm_smmu_device *smmu)
 {
@@ -502,6 +531,23 @@ static void __arm_smmu_free_bitmap(unsigned long *map, int idx)
 {
 	clear_bit(idx, map);
 }
+
+static int arm_smmu_enable_regulators(struct arm_smmu_device *smmu)
+{
+       if (!smmu->regulator)
+               return 0;
+
+       return regulator_enable(smmu->regulator);
+}
+
+static int arm_smmu_disable_regulators(struct arm_smmu_device *smmu)
+{
+       if (!smmu->regulator)
+               return 0;
+
+       return regulator_disable(smmu->regulator);
+}
+
 
 /* Wait for any pending TLB invalidations to complete */
 static void __arm_smmu_tlb_sync(struct arm_smmu_device *smmu)
@@ -1638,6 +1684,61 @@ static int arm_smmu_id_size_to_bits(int size)
 	}
 }
 
+static int arm_smmu_init_regulators(struct arm_smmu_device *smmu)
+{
+       struct device *dev = smmu->dev;
+
+       if (!of_get_property(dev->of_node, "vdd-supply", NULL))
+               return 0;
+
+       smmu->regulator = devm_regulator_get(dev, "vdd");
+       if (IS_ERR(smmu->regulator))
+               return PTR_ERR(smmu->regulator);
+
+       return 0;
+}
+
+static int arm_smmu_init_clocks(struct arm_smmu_device *smmu)
+{
+       const char *cname;
+       struct property *prop;
+       int i;
+       struct device *dev = smmu->dev;
+
+       smmu->num_clocks =
+               of_property_count_strings(dev->of_node, "clock-names");
+
+       if (smmu->num_clocks < 1)
+               return 0;
+
+       smmu->clocks = devm_kzalloc(
+               dev, sizeof(*smmu->clocks) * smmu->num_clocks,
+               GFP_KERNEL);
+
+       if (!smmu->clocks) {
+               dev_err(dev,
+                       "Failed to allocate memory for clocks\n");
+               return -ENODEV;
+       }
+
+       i = 0;
+       of_property_for_each_string(dev->of_node, "clock-names",
+                                   prop, cname) {
+               struct clk *c = devm_clk_get(dev, cname);
+
+               if (IS_ERR(c)) {
+                       dev_err(dev, "Couldn't get clock: %s",
+                               cname);
+                       return -EPROBE_DEFER;
+               }
+
+               smmu->clocks[i] = c;
+
+               ++i;
+       }
+       return 0;
+}
+
 static int arm_smmu_device_cfg_probe(struct arm_smmu_device *smmu)
 {
 	unsigned long size;
@@ -2039,9 +2140,25 @@ static int arm_smmu_device_dt_probe(struct platform_device *pdev)
 		smmu->irqs[i] = irq;
 	}
 
+       err = arm_smmu_init_regulators(smmu);
+       if (err)
+               goto out;
+
+       err = arm_smmu_init_clocks(smmu);
+       if (err)
+               goto out;
+
+       err = arm_smmu_enable_regulators(smmu);
+       if (err)
+               goto out;
+
+       err = arm_smmu_enable_clocks(smmu);
+       if (err)
+               goto out_disable_regulators;
+
 	err = arm_smmu_device_cfg_probe(smmu);
 	if (err)
-		return err;
+		goto out_disable_clocks;
 
 	parse_driver_options(smmu);
 
@@ -2050,7 +2167,7 @@ static int arm_smmu_device_dt_probe(struct platform_device *pdev)
 		dev_err(dev,
 			"found only %d context interrupt(s) but %d required\n",
 			smmu->num_context_irqs, smmu->num_context_banks);
-		return -ENODEV;
+		goto out_disable_clocks;
 	}
 
 	for (i = 0; i < smmu->num_global_irqs; ++i) {
@@ -2075,8 +2192,16 @@ static int arm_smmu_device_dt_probe(struct platform_device *pdev)
 	return 0;
 
 out_free_irqs:
-	while (i--)
-		free_irq(smmu->irqs[i], smmu);
+        while (i--)
+                free_irq(smmu->irqs[i], smmu);
+
+out_disable_clocks:
+       arm_smmu_disable_clocks(smmu);
+
+out_disable_regulators:
+       arm_smmu_disable_regulators(smmu);
+
+out:
 	return err;
 }
 
@@ -2098,6 +2223,9 @@ static int arm_smmu_device_remove(struct platform_device *pdev)
 
 	/* Turn the thing off */
 	writel(sCR0_CLIENTPD, ARM_SMMU_GR0_NS(smmu) + ARM_SMMU_GR0_sCR0);
+	arm_smmu_disable_clocks(smmu);
+	arm_smmu_disable_regulators(smmu);
+
 	return 0;
 }
 
