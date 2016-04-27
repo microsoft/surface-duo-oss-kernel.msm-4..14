01a475be56bc9fff90db4d95d5154c9e234e8949
James Morse <james.morse@arm.com>
2016-04-27 17:47:07 +0100

    arm64: Change cpu_resume() to enable mmu early then access sleep_sp by va
    By enabling the MMU early in cpu_resume(), the sleep_save_sp and stack can
    be accessed by VA, which avoids the need to convert-addresses and clean to
    PoC on the suspend path.
    
    MMU setup is shared with the boot path, meaning the swapper_pg_dir is
    restored directly: ttbr1_el1 is no longer saved/restored.
    
    struct sleep_save_sp is removed, replacing it with a single array of
    pointers.
    
    cpu_do_{suspend,resume} could be further reduced to not restore: cpacr_el1,
    mdscr_el1, tcr_el1, vbar_el1 and sctlr_el1, all of which are set by
    __cpu_setup(). However these values all contain res0 bits that may be used
    to enable future features.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    Reviewed-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    (cherry picked from commit cabe1c81ea5be983425d117912d7883e252a3b09)
    Signed-off-by: Alex Shi <alex.shi@linaro.org>
    
    Conflicts:
    	skip kaslr in arch/arm64/kernel/head.S
    	arch/arm64/mm/proc.S

--- arch/arm64/kernel/setup.c
+++ arch/arm64/kernel/setup.c
@@ -195,7 +195,6 @@ static void __init smp_build_mpidr_hash(void)
 	 */
 	if (mpidr_hash_size() > 4 * num_possible_cpus())
 		pr_warn("Large number of MPIDR hash buckets detected\n");
-	__flush_dcache_area(&mpidr_hash, sizeof(struct mpidr_hash));
 }
 
 static void __init hyp_mode_check(void)
