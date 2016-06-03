ec2d1835c250ad7f07fa3fe33a4d047816be110c
Alex Shi <alex.shi@linaro.org>
2016-06-03 12:02:18 +0800

    Revert "cgroup, block: implement task_get_css() and use it in bio_associate_current()"
    This reverts commit a41e6e7eee7df20f526dfdee3c5bfb896c7059a1.

--- block/bio.c
+++ block/bio.c
@@ -2019,6 +2019,7 @@ EXPORT_SYMBOL_GPL(bio_associate_blkcg);
 int bio_associate_current(struct bio *bio)
 {
 	struct io_context *ioc;
+	struct cgroup_subsys_state *css;
 
 	if (bio->bi_css)
 		return -EBUSY;
@@ -2027,9 +2028,17 @@ int bio_associate_current(struct bio *bio)
 	if (!ioc)
 		return -ENOENT;
 
+	/* acquire active ref on @ioc and associate */
 	get_io_context_active(ioc);
 	bio->bi_ioc = ioc;
-	bio->bi_css = task_get_css(current, blkio_cgrp_id);
+
+	/* associate blkcg if exists */
+	rcu_read_lock();
+	css = task_css(current, blkio_cgrp_id);
+	if (css && css_tryget_online(css))
+		bio->bi_css = css;
+	rcu_read_unlock();
+
 	return 0;
 }
 EXPORT_SYMBOL_GPL(bio_associate_current);
