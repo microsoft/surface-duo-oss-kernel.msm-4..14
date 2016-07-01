c8d13386b7e9634586a23128f082558e5017474e
Vinod Koul <vinod.koul@intel.com>
2016-07-01 17:34:14 +0530

    dmaengine: fsl-edma: explicitly freeup irq
    dmaengine device should explicitly call devm_free_irq() when using
    devm_request_irq().
    
    The irq is still ON when devices remove is executed and irq should be
    quiesced before remove is completed.
    
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>
    Cc: Jingchang Lu <b35083@freescale.com>
    Cc: Peter Griffin <peter.griffin@linaro.org>
    (cherry picked from commit 476c7c809ebeefb83e017be543922dede1c10584)
    Cherry-pick-repository:
    git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git
    
    Adapt patch according to the changes from commit
    0a3992ae0fc657e0c82b92548074d2e5ddd60f99.
    
    Issue: ALB-2543
    Signed-off-by: Larisa Grigore <larisa.grigore@nxp.com>

--- drivers/dma/fsl-edma.c
+++ drivers/dma/fsl-edma.c
@@ -1012,6 +1012,19 @@ static unsigned vf610_mux_channel_mapping(u32 channel_id)
 	return channel_id;
 }
 
+static void fsl_edma_irq_exit(
+		struct platform_device *pdev, struct fsl_edma_engine *fsl_edma)
+{
+	unsigned int i;
+	const struct fsl_edma_soc_data *socdata = fsl_edma->socdata;
+
+	for (i = 0; i < socdata->n_irqs; i++) {
+		if (socdata->irqs[i].irqno >= 0)
+			devm_free_irq(&pdev->dev,
+					socdata->irqs[i].irqno, fsl_edma);
+	}
+}
+
 static void fsl_edma_enable_arbitration(struct fsl_edma_engine *fsl_edma)
 {
 	void __iomem *addr = fsl_edma->membase;
@@ -1251,6 +1264,7 @@ static int fsl_edma_remove(struct platform_device *pdev)
 	struct fsl_edma_engine *fsl_edma = platform_get_drvdata(pdev);
 	int i;
 
+	fsl_edma_irq_exit(pdev, fsl_edma);
 	of_dma_controller_free(np);
 	dma_async_device_unregister(&fsl_edma->dma_dev);
 
