88ed421a2528f36d0106f79e04eafa376eb50c1e
Larisa Grigore <Larisa.Grigore@nxp.com>
2016-08-05 16:54:52 +0300

    Optimize STM resources allocation
    STM module allocates stm structures for each possible core. This can be
    optimized in order to allocate only as many structures as stm modules
    defined in dts.
    
    Issue: ALB-471
    
    Signed-off-by: Larisa Grigore <Larisa.Grigore@nxp.com>

--- drivers/clocksource/fsl_stm_timer.c
+++ drivers/clocksource/fsl_stm_timer.c
@@ -34,9 +34,8 @@
 #define STM_CCR_CEN	(0x1 << 0)
 #define STM_CIR_CIF	(0x1 << 0)
 
-#define MAX_CPUS_NR	(NR_CPUS)
-#define MASTER_CORE	0
-#define TIMER_NAME	"FSL stm timer"
+#define MASTER_CPU	0
+#define STM_TIMER_NAME	"FSL stm timer"
 
 struct stm_timer {
 	void __iomem *timer_base;
@@ -48,22 +47,30 @@ struct stm_timer {
 	struct clock_event_device clockevent_stm;
 	struct irqaction stm_timer_irq;
 	unsigned long delta;
+	struct list_head list;
 };
 
-static struct stm_timer *stm[MAX_CPUS_NR];
-static int stm_allocated;
+static LIST_HEAD(stms_list);
+static struct stm_timer *clocksource;
+static bool registered;
 
-static inline void stm_timer_enable(int cpu)
+static inline struct stm_timer *stm_timer_from_evt(
+		struct clock_event_device *evt)
+{
+	return container_of(evt, struct stm_timer, clockevent_stm);
+}
+
+static inline void stm_timer_enable(struct stm_timer *stm)
 {
 	/* enable the stm module */
 	__raw_writel(STM_CR_CPS | STM_CR_FRZ | STM_CR_TEN,
-			stm[cpu]->timer_base + STM_CR);
+			stm->timer_base + STM_CR);
 
 	/* enable clockevent channel */
-	__raw_writel(STM_CCR_CEN, stm[cpu]->clkevt_base + STM_CCR);
+	__raw_writel(STM_CCR_CEN, stm->clkevt_base + STM_CCR);
 }
 
-static inline void stm_timer_disable(int cpu)
+static inline void stm_timer_disable(struct stm_timer *stm)
 {
 	/**
 	 * The counter is shared between channels and will continue to
@@ -71,36 +78,38 @@ static inline void stm_timer_disable(int cpu)
 	 * be lost if we don't disable the entire module.
 	 * Disabling the entire module, makes STM not suitable as clocksource.
 	 */
-	__raw_writel(0, stm[cpu]->timer_base + STM_CR);
-	__raw_writel(0, stm[cpu]->clkevt_base + STM_CCR);
+	__raw_writel(0, stm->timer_base + STM_CR);
+	__raw_writel(0, stm->clkevt_base + STM_CCR);
 }
 
-static u32 get_counter(int cpu)
+static u32 get_counter(struct stm_timer *stm)
 {
-	return __raw_readl(stm[cpu]->timer_base + STM_CNT);
+	return __raw_readl(stm->timer_base + STM_CNT);
 }
 
-static inline void stm_irq_acknowledge(int cpu)
+static inline void stm_irq_acknowledge(struct stm_timer *stm)
 {
 	u32 val;
 
 	/* clear the interrupt */
-	__raw_writel(STM_CIR_CIF, stm[cpu]->clkevt_base + STM_CIR);
+	__raw_writel(STM_CIR_CIF, stm->clkevt_base + STM_CIR);
 
 	/* update STM_CMP value using the counter value */
-	val = get_counter(cpu) + stm[cpu]->delta;
-	__raw_writel(val, stm[cpu]->clkevt_base + STM_CMP);
+	val = get_counter(stm) + stm->delta;
+	__raw_writel(val, stm->clkevt_base + STM_CMP);
 }
 
 static u64 stm_read_sched_clock(void)
 {
-	return __raw_readl(stm[MASTER_CORE]->timer_base + STM_CNT);
+	return __raw_readl(clocksource->timer_base + STM_CNT);
 }
 
-static int __init stm_clocksource_init(unsigned long rate)
+static int __init stm_clocksource_init(struct stm_timer *stm,
+						unsigned long rate)
 {
+	clocksource = stm;
 	sched_clock_register(stm_read_sched_clock, 32, rate);
-	clocksource_mmio_init(stm[MASTER_CORE]->timer_base + STM_CNT,
+	clocksource_mmio_init(clocksource->timer_base + STM_CNT,
 			     "fsl-stm", rate,
 			     CONFIG_STM_CLKSRC_RATE, 32,
 			     clocksource_mmio_readl_up);
@@ -109,21 +118,19 @@ static int __init stm_clocksource_init(unsigned long rate)
 }
 
 static int stm_set_next_event(unsigned long delta,
-				struct clock_event_device *unused)
+				struct clock_event_device *evt)
 {
-	int cpu;
 	u32 val;
+	struct stm_timer *stm = stm_timer_from_evt(evt);
 
-	cpu = get_cpu();
-	stm_timer_disable(cpu);
+	stm_timer_disable(stm);
 
-	stm[cpu]->delta = delta;
+	stm->delta = delta;
 
-	val = get_counter(cpu) + delta;
-	__raw_writel(val, stm[cpu]->clkevt_base + STM_CMP);
+	val = get_counter(stm) + delta;
+	__raw_writel(val, stm->clkevt_base + STM_CMP);
 
-	stm_timer_enable(cpu);
-	put_cpu();
+	stm_timer_enable(stm);
 
 	return 0;
 }
@@ -131,18 +138,15 @@ static int stm_set_next_event(unsigned long delta,
 static void stm_set_mode(enum clock_event_mode mode,
 				struct clock_event_device *evt)
 {
-	int cpu;
+	struct stm_timer *stm = stm_timer_from_evt(evt);
 
 	switch (mode) {
 	case CLOCK_EVT_MODE_PERIODIC:
-		cpu = get_cpu();
-		put_cpu(); /* TODO: fix next call for preemption */
-		stm_set_next_event(stm[cpu]->cycle_per_jiffy, evt);
+		stm_set_next_event(stm->cycle_per_jiffy, evt);
 		break;
 	case CLOCK_EVT_MODE_SHUTDOWN:
 	case CLOCK_EVT_MODE_UNUSED:
-		stm_timer_disable(get_cpu());
-		put_cpu();
+		stm_timer_disable(stm);
 		break;
 	default:
 		break;
@@ -152,11 +156,9 @@ static void stm_set_mode(enum clock_event_mode mode,
 static irqreturn_t stm_timer_interrupt(int irq, void *dev_id)
 {
 	struct clock_event_device *evt = dev_id;
-	int cpu = get_cpu();
+	struct stm_timer *stm = stm_timer_from_evt(evt);
 
-	put_cpu();
-
-	stm_irq_acknowledge(cpu);
+	stm_irq_acknowledge(stm);
 
 	/*
 	 * stm hardware doesn't support oneshot, it will generate an interrupt
@@ -164,41 +166,42 @@ static irqreturn_t stm_timer_interrupt(int irq, void *dev_id)
 	 * to stop the counter loop in ONESHOT mode.
 	 */
 	if (likely(evt->mode == CLOCK_EVT_MODE_ONESHOT))
-		stm_timer_disable(cpu);
+		stm_timer_disable(stm);
 
 	evt->event_handler(evt);
 
 	return IRQ_HANDLED;
 }
 
-static int __init stm_clockevent_init(int cpu, unsigned long rate, int irq)
+static int stm_clockevent_init(struct stm_timer *stm, unsigned long rate,
+								int irq)
 {
-	__raw_writel(0, stm[cpu]->clkevt_base + STM_CCR);
+	__raw_writel(0, stm->clkevt_base + STM_CCR);
 
-	stm[cpu]->clockevent_stm.name = TIMER_NAME;
-	stm[cpu]->clockevent_stm.features = CLOCK_EVT_FEAT_PERIODIC |
+	stm->clockevent_stm.name = STM_TIMER_NAME;
+	stm->clockevent_stm.features = CLOCK_EVT_FEAT_PERIODIC |
 					    CLOCK_EVT_FEAT_ONESHOT;
-	stm[cpu]->clockevent_stm.set_mode = stm_set_mode;
-	stm[cpu]->clockevent_stm.set_next_event = stm_set_next_event;
-	stm[cpu]->clockevent_stm.rating = CONFIG_STM_CLKEVT_RATE;
-	stm[cpu]->clockevent_stm.cpumask = cpumask_of(cpu);
-	stm[cpu]->clockevent_stm.irq = irq;
+	stm->clockevent_stm.set_mode = stm_set_mode;
+	stm->clockevent_stm.set_next_event = stm_set_next_event;
+	stm->clockevent_stm.rating = CONFIG_STM_CLKEVT_RATE;
+	stm->clockevent_stm.cpumask = cpumask_of(stm->cpu);
+	stm->clockevent_stm.irq = irq;
 
-	stm[cpu]->stm_timer_irq.name = TIMER_NAME;
-	stm[cpu]->stm_timer_irq.flags = IRQF_TIMER | IRQF_IRQPOLL;
-	stm[cpu]->stm_timer_irq.handler = stm_timer_interrupt;
-	stm[cpu]->stm_timer_irq.dev_id = &stm[cpu]->clockevent_stm;
+	stm->stm_timer_irq.name = STM_TIMER_NAME;
+	stm->stm_timer_irq.flags = IRQF_TIMER | IRQF_IRQPOLL;
+	stm->stm_timer_irq.handler = stm_timer_interrupt;
+	stm->stm_timer_irq.dev_id = &stm->clockevent_stm;
 
-	BUG_ON(setup_irq(irq, &stm[cpu]->stm_timer_irq));
+	BUG_ON(setup_irq(irq, &stm->stm_timer_irq));
 
-	if (cpu == MASTER_CORE)
-		BUG_ON(irq_set_affinity(irq,  cpumask_of(cpu)));
+	if (stm->cpu == MASTER_CPU)
+		BUG_ON(irq_set_affinity(irq,  cpumask_of(stm->cpu)));
 
-	clockevents_config_and_register(&stm[cpu]->clockevent_stm, rate, 1,
+	clockevents_config_and_register(&stm->clockevent_stm, rate, 1,
 					0xffffffff);
 
-	if (cpu == MASTER_CORE)
-		__raw_writel(STM_CIR_CIF, stm[cpu]->clkevt_base + STM_CIR);
+	if (stm->cpu == MASTER_CPU)
+		__raw_writel(STM_CIR_CIF, stm->clkevt_base + STM_CIR);
 
 	return 0;
 }
@@ -206,34 +209,30 @@ static int __init stm_clockevent_init(int cpu, unsigned long rate, int irq)
 static int stm_timer_cpu_notify(struct notifier_block *self,
 	unsigned long action, void *hcpu)
 {
-	int mcpu;
-
-	/*
-	* Grab cpu pointer in each case to avoid
-	* spurious preemptible warnings
-	*/
+	struct stm_timer *stm;
 	int cpu = (long)hcpu;
 
-	if (stm[cpu] == NULL)
+	list_for_each_entry(stm, &stms_list, list)
+		if (stm->cpu == cpu)
+			break;
+
+	if (stm == NULL || stm->cpu != cpu)
 		return NOTIFY_OK;
 
 	switch (action & ~CPU_TASKS_FROZEN) {
 	case CPU_ONLINE:
-		BUG_ON(irq_set_affinity(stm[cpu]->irq,
-			    cpumask_of(cpu)));
+		BUG_ON(irq_set_affinity(stm->irq,
+			    cpumask_of(stm->cpu)));
 		__raw_writel(STM_CIR_CIF,
-			     stm[cpu]->clkevt_base + STM_CIR);
+			     stm->clkevt_base + STM_CIR);
 	break;
 	case CPU_STARTING:
-		mcpu = get_cpu();
-		put_cpu();
-		stm_clockevent_init(mcpu,
-			    stm[mcpu]->cycle_per_jiffy * (HZ),
-			    stm[mcpu]->irq);
+		stm_clockevent_init(stm,
+			    stm->cycle_per_jiffy * (HZ),
+			    stm->irq);
 	break;
 	case CPU_DYING:
-		stm_timer_disable(get_cpu());
-		put_cpu();
+		stm_timer_disable(stm);
 	    break;
 	}
 
@@ -249,48 +248,52 @@ void __init stm_timer_init(struct device_node *np)
 	void __iomem *timer_base;
 	unsigned long clk_rate;
 	int cpu;
+	struct stm_timer *stm;
 
 	of_property_read_u32(np, "cpu", &cpu);
-	if (cpu < 0 || cpu >= MAX_CPUS_NR) {
-		pr_err("fsl-stm: please specify a cpu number between 0 and %d.\n",
-				MAX_CPUS_NR-1);
+	if (cpu < 0 || cpu >= num_possible_cpus()) {
+		pr_err("%s: please specify a cpu number between 0 and %d.\n",
+				STM_TIMER_NAME, num_possible_cpus() - 1);
 		return;
 	}
 
-	stm[cpu] = kzalloc(sizeof(struct stm_timer), GFP_KERNEL);
-	if (stm[cpu] == NULL) {
-		pr_err("fsl-stm: impossible to allocate memory\n");
+	stm = kzalloc(sizeof(struct stm_timer), GFP_KERNEL);
+	if (stm == NULL) {
+		pr_err("%s: impossible to allocate memory\n", STM_TIMER_NAME);
 		return;
 	}
 
-	stm_allocated++;
-	stm[cpu]->cpu = cpu;
+	list_add_tail(&stm->list, &stms_list);
+
+	stm->cpu = cpu;
 
 	timer_base = of_iomap(np, 0);
 	BUG_ON(!timer_base);
 
-	stm[cpu]->timer_base = timer_base;
+	stm->timer_base = timer_base;
 
 	/* use channel 0 as clockevent */
-	stm[cpu]->clkevt_base = timer_base + STM_CH(0);
+	stm->clkevt_base = timer_base + STM_CH(0);
 
-	stm[cpu]->irq = irq_of_parse_and_map(np, 0);
-	BUG_ON(stm[cpu]->irq <= 0);
+	stm->irq = irq_of_parse_and_map(np, 0);
+	BUG_ON(stm->irq <= 0);
 
-	stm[cpu]->stm_clk = of_clk_get(np, 0);
-	BUG_ON(IS_ERR(stm[cpu]->stm_clk));
+	stm->stm_clk = of_clk_get(np, 0);
+	BUG_ON(IS_ERR(stm->stm_clk));
 
-	BUG_ON(clk_prepare_enable(stm[cpu]->stm_clk));
+	BUG_ON(clk_prepare_enable(stm->stm_clk));
 
-	clk_rate = clk_get_rate(stm[cpu]->stm_clk);
-	stm[cpu]->cycle_per_jiffy = clk_rate / (HZ);
+	clk_rate = clk_get_rate(stm->stm_clk);
+	stm->cycle_per_jiffy = clk_rate / (HZ);
 
-	if (stm_allocated == 1)
+	if (registered == false) {
 		BUG_ON(register_cpu_notifier(&stm_timer_cpu_nb));
+		registered = true;
+	}
 
-	if (cpu == MASTER_CORE) {
-		BUG_ON(stm_clocksource_init(clk_rate));
-		stm_clockevent_init(cpu, clk_rate, stm[cpu]->irq);
+	if (cpu == MASTER_CPU) {
+		BUG_ON(stm_clocksource_init(stm, clk_rate));
+		stm_clockevent_init(stm, clk_rate, stm->irq);
 	}
 
 	/* reset counter value */
