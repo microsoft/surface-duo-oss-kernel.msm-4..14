2aeec2781344b2354aed85961b64024f565f06f5
Irina Presa <irina.presa@nxp.com>
2016-09-22 12:27:15 +0300

    cse3: Move HW rng code in a separate module
    Issue: ALB-666
    Signed-off-by: Irina Presa <irina.presa@nxp.com>

--- drivers/crypto/cse3_core.c
+++ drivers/crypto/cse3_core.c
@@ -19,12 +19,12 @@
 #include <linux/ioport.h>
 #include <linux/slab.h>
 #include <linux/of.h>
-#include <linux/hw_random.h>
 #include <linux/io.h>
 
 #include "cse3.h"
 #include "cse3_req.h"
 #include "cse3_capi.h"
+#include "cse3_rng.h"
 #include "cse3_hw.h"
 
 
@@ -375,100 +375,12 @@ irqreturn_t cse_irq_handler(int irq_no, void *dev_id)
 	return IRQ_HANDLED;
 }
 
-#ifdef CONFIG_CRYPTO_DEV_FSL_CSE3_HWRNG
-
-static int cse_rng_copy_output
-(struct cse_device_data *dev, struct cse_request *req)
-{
-	struct cse_rval_request *rval_req = (struct cse_rval_request *)dev->req;
-	cse_desc_t *desc = dev->hw_desc;
-
-	memcpy(rval_req->rval, desc->rval, RND_VAL_SIZE);
-
-	return 0;
-}
-
-static void cse_rng_complete
-(struct cse_device_data *dev, struct cse_request *req)
-{
-	complete(&req->complete);
-}
-
-static void cse_rng_init_ops(struct cse_request *req)
-{
-	req->copy_output = cse_rng_copy_output;
-	req->comp = cse_rng_complete;
-}
-
-static int cse_rng_read(struct hwrng *rng, void *data, size_t max, bool wait)
-{
-	int size = 0;
-	struct cse_rval_request *new_req;
-	cse_ctx_t *ctx;
-
-	if (!wait)
-		return 0;
-
-	if (down_trylock(&cse_dev_ptr->access))
-		return -EBUSY;
-
-	ctx = kzalloc(sizeof(cse_ctx_t), GFP_KERNEL);
-	if (!ctx) {
-		dev_err(cse_dev_ptr->device, "failed to alloc mem for crypto context.\n");
-		up(&cse_dev_ptr->access);
-		return -ENOMEM;
-	}
-	ctx->dev = cse_dev_ptr;
-
-	new_req = kzalloc(sizeof(*new_req), GFP_KERNEL);
-	new_req->base.ctx = ctx;
-	new_req->base.phase = 0;
-	new_req->base.flags = FLAG_RND;
-	cse_rng_init_ops(&new_req->base);
-	init_completion(&new_req->base.complete);
-
-	if (!cse_handle_request(ctx->dev, (cse_req_t *)new_req)) {
-
-		if (wait_for_completion_interruptible(
-					&new_req->base.complete)) {
-			cse_cancel_request((cse_req_t *)new_req);
-			goto out;
-		} else if (!new_req->base.error) {
-			size = max < RND_VAL_SIZE ? max : RND_VAL_SIZE;
-			memcpy(data, new_req->rval, size);
-		}
-
-	}
-
-	cse_finish_req(ctx->dev, (cse_req_t *)new_req);
-out:
-	up(&ctx->dev->access);
-	kfree(ctx);
-
-	return size;
-}
-
-static int cse_rng_data_read(struct hwrng *rng, u32 *data)
-{
-	return cse_rng_read(rng, data, RND_VAL_SIZE, 1);
-}
-#endif
-
 static const struct file_operations cse_fops = {
 	.owner = THIS_MODULE,
 	.open = cse_cdev_open,
 	.release = cse_cdev_release,
 	.unlocked_ioctl = cse_cdev_ioctl,
 };
-
-#ifdef CONFIG_CRYPTO_DEV_FSL_CSE3_HWRNG
-static struct hwrng cse_rng = {
-	.name		= "rng-cse",
-	/* .cleanup	= cse_rng_cleanup, */
-	.data_read	= cse_rng_data_read,
-	.read		= cse_rng_read,
-};
-#endif
 
 static const struct platform_device_id cse3_platform_ids[] = {
 	{ .name = "cse3-s32v234" },
@@ -557,12 +469,7 @@ static int cse_probe(struct platform_device *pdev)
 	cdev_init(&cse_dev->cdev, &cse_fops);
 	cdev_add(&cse_dev->cdev, MKDEV(CSE3_MAJOR, 0), 1);
 
-#ifdef CONFIG_CRYPTO_DEV_FSL_CSE3_HWRNG
-	/** Register HW Random Number Generator API */
-	if (devm_hwrng_register(cse_dev->device, &cse_rng))
-		dev_err(&pdev->dev, "failed to register hwrng.\n");
-#endif
-
+	cse_register_rng();
 	cse_register_crypto_api();
 
 	return 0;
