02c3dbdd28ce67b9a1feea3012b6cbd6561021ef
Ethan Hsieh <ethan.hsieh@canonical.com>
2016-10-18 15:21:17 +0800

    Bluetooth: btusb: Fix atheros firmware download error
    BugLink: http://bugs.launchpad.net/bugs/1634380
    
    Move usb_autopm_get_interface() ahead of setup_on_usb() to prevent
    device from sending usb control message in usb suspend mode.
    
    The error message is as below:
    
    [   83.944103] btusb 1-2:1.1: usb_suspend_interface: status 0
    [   83.944107] btusb 1-2:1.0: usb_suspend_interface: status 0
    [   83.960132] usb 1-2: usb auto-suspend, wakeup 0
    [   83.976156] usb 1-2: usb_suspend_device: status 0
    [   83.976162] usb 1-2: usb_suspend_both: status 0
    [  298.689106] Bluetooth: hci0
    [  298.689399] Bluetooth: hci0: Failed to access otp area (-113)
    
    Signed-off-by: Ethan Hsieh <ethan.hsieh@canonical.com>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    (cherry picked from commit c7e163fe282f34efdf77fb09b333529de97a6a10)
    Signed-off-by: AceLan Kao <acelan.kao@canonical.com>
    Acked-by: Tim Gardner <tim.gardner@canonical.com>
    Acked-by: Seth Forshee <seth.forshee@canonical.com>
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -1036,6 +1036,10 @@ static int btusb_open(struct hci_dev *hdev)
 
 	BT_DBG("%s", hdev->name);
 
+	err = usb_autopm_get_interface(data->intf);
+	if (err < 0)
+		return err;
+
 	/* Patching USB firmware files prior to starting any URBs of HCI path
 	 * It is more safe to use USB bulk channel for downloading USB patch
 	 */
@@ -1044,10 +1048,6 @@ static int btusb_open(struct hci_dev *hdev)
 		if (err < 0)
 			return err;
 	}
-
-	err = usb_autopm_get_interface(data->intf);
-	if (err < 0)
-		return err;
 
 	data->intf->needs_remote_wakeup = 1;
 
