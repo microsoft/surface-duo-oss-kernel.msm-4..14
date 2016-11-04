98702871c465830b1172c4706bd2d7c01aa7d349
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2016-11-04 12:36:08 +0000

    PCI: qcom: add runtime pm support to pcie_port
    This patch is required when the pcie controller sits on a bus with
    its own power domain and clocks which are controlled via a bus driver
    like simple pm bus. As these bus driver have runtime pm enabled, it makes
    sense to update the usage counter so that the runtime pm does not suspend
    the clks or power domain associated with the bus driver.
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>

--- drivers/pci/dwc/pcie-qcom.c
+++ drivers/pci/dwc/pcie-qcom.c
@@ -1104,6 +1104,9 @@ static int qcom_pcie_host_init(struct pcie_port *pp)
 	struct qcom_pcie *pcie = to_qcom_pcie(pci);
 	int ret;
 
+
+	pm_runtime_get_sync(pci->dev);
+
 	qcom_ep_reset_assert(pcie);
 
 	ret = pcie->ops->init(pcie);
@@ -1140,6 +1143,7 @@ err_disable_phy:
 	phy_power_off(pcie->phy);
 err_deinit:
 	pcie->ops->deinit(pcie);
+	pm_runtime_put_sync(pci->dev);
 
 	return ret;
 }
@@ -1228,6 +1232,7 @@ static int qcom_pcie_probe(struct platform_device *pdev)
 	if (!pci)
 		return -ENOMEM;
 
+	pm_runtime_enable(dev);
 	pci->dev = dev;
 	pci->ops = &dw_pcie_ops;
 	pp = &pci->pp;
@@ -1277,6 +1282,7 @@ static int qcom_pcie_probe(struct platform_device *pdev)
 				       "qcom-pcie-msi", pp);
 		if (ret) {
 			dev_err(dev, "cannot request msi irq\n");
+			pm_runtime_disable(&pdev->dev);
 			return ret;
 		}
 	}
@@ -1290,6 +1296,7 @@ static int qcom_pcie_probe(struct platform_device *pdev)
 	ret = dw_pcie_host_init(pp);
 	if (ret) {
 		dev_err(dev, "cannot initialize host\n");
+		pm_runtime_disable(&pdev->dev);
 		return ret;
 	}
 
