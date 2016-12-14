9e3c94406958cb11941934a5a22dce6a85efc541
John Stultz <john.stultz@linaro.org>
2016-12-13 17:28:55 -0800

    HACK: Use CAP_SYS_NICE instead of CAP_CGROUP for now
    Andorid userspace needs changes to be able to use CAP_CGROUP
    so for now, just use CAP_SYS_NICE.
    
    NOT FOR UPSTREAM!
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -2437,7 +2437,7 @@ static int cgroup_procs_write_permission(struct task_struct *task,
 		if (uid_eq(cred->euid, GLOBAL_ROOT_UID) ||
 		    uid_eq(cred->euid, tcred->uid) ||
 		    uid_eq(cred->euid, tcred->suid) ||
-	    	    ns_capable(tcred->user_ns, CAP_CGROUP))
+	    	    ns_capable(tcred->user_ns, CAP_SYS_NICE))
 			ret = 0;
 		else
 			ret = -EACCES;
