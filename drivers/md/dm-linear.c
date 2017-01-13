db246b5429fc0c7bf19683c11d41045fd6d62db0
Badhri Jagan Sridharan <Badhri@google.com>
2017-01-13 11:05:00 -0800

    ANDROID: dm: rebase for 4.9
    Export the direct_access method of dm_linear target for
    dm-android-verity target.
    
    Signed-off-by: Badhri Jagan Sridharan <Badhri@google.com>
    Change-Id: I46556d882305e5194352946264cbc9c06e5038e4

--- drivers/md/dm-linear.c
+++ drivers/md/dm-linear.c
@@ -149,7 +149,7 @@ int dm_linear_iterate_devices(struct dm_target *ti,
 }
 EXPORT_SYMBOL_GPL(dm_linear_iterate_devices);
 
-static long linear_dax_direct_access(struct dm_target *ti, pgoff_t pgoff,
+long dm_linear_direct_access(struct dm_target *ti, pgoff_t pgoff,
 		long nr_pages, void **kaddr, pfn_t *pfn)
 {
 	long ret;
@@ -164,6 +164,7 @@ static long linear_dax_direct_access(struct dm_target *ti, pgoff_t pgoff,
 		return ret;
 	return dax_direct_access(dax_dev, pgoff, nr_pages, kaddr, pfn);
 }
+EXPORT_SYMBOL_GPL(dm_linear_direct_access);
 
 static struct target_type linear_target = {
 	.name   = "linear",
@@ -176,7 +177,7 @@ static struct target_type linear_target = {
 	.status = dm_linear_status,
 	.prepare_ioctl = dm_linear_prepare_ioctl,
 	.iterate_devices = dm_linear_iterate_devices,
-	.direct_access = linear_dax_direct_access,
+	.direct_access = dm_linear_direct_access,
 };
 
 int __init dm_linear_init(void)
