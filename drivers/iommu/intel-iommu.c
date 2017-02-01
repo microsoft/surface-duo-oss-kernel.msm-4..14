ee624f2d44634a6092d07d389eef481b9dc8a090
Joerg Roedel <jroedel@suse.de>
2017-02-01 17:23:22 +0100

    iommu: Make iommu_device_link/unlink take a struct iommu_device
    This makes the interface more consistent with
    iommu_device_sysfs_add/remove.
    
    BugLink: http://bugs.launchpad.net/bugs/1688158
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    (cherry picked from commit e3d10af1128b6bc394f21656ff13753130f3c107)
    Signed-off-by: Manoj Iyer <manoj.iyer@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Thadeu Lima de Souza Cascardo <cascardo@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- drivers/iommu/intel-iommu.c
+++ drivers/iommu/intel-iommu.c
@@ -5197,7 +5197,7 @@ static int intel_iommu_add_device(struct device *dev)
 	if (!iommu)
 		return -ENODEV;
 
-	iommu_device_link(&iommu->iommu.dev, dev);
+	iommu_device_link(&iommu->iommu, dev);
 
 	group = iommu_group_get_for_dev(dev);
 
@@ -5219,7 +5219,7 @@ static void intel_iommu_remove_device(struct device *dev)
 
 	iommu_group_remove_device(dev);
 
-	iommu_device_unlink(&iommu->iommu.dev, dev);
+	iommu_device_unlink(&iommu->iommu, dev);
 }
 
 static void intel_iommu_get_resv_regions(struct device *device,
