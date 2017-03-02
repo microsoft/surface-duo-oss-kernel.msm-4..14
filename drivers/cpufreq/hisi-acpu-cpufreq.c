31ed774fdaee596f9bebfd42e5672e8d639a9b92
Leo Yan <leo.yan@linaro.org>
2017-03-02 10:35:02 +0800

    cpufreq: hisi-acpu: add support for hi3660
    Enable cpufreq-dt driver for hi3660.
    
    Signed-off-by: Leo Yan <leo.yan@linaro.org>

--- drivers/cpufreq/hisi-acpu-cpufreq.c
+++ drivers/cpufreq/hisi-acpu-cpufreq.c
@@ -29,7 +29,8 @@ static int __init hisi_acpu_cpufreq_driver_init(void)
 {
 	struct platform_device *pdev;
 
-	if (!of_machine_is_compatible("hisilicon,hi6220"))
+	if (!of_machine_is_compatible("hisilicon,hi6220") &&
+		!of_machine_is_compatible("hisilicon,hi3660"))
 		return -ENODEV;
 
 	pdev = platform_device_register_simple("cpufreq-dt", -1, NULL, 0);
