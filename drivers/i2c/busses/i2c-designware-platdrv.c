c0fe83eb3a5ed0ecdca9f5dd456406faa6fcbdc1
Jarkko Nikula <jarkko.nikula@linux.intel.com>
2017-03-30 15:04:44 +0300

    i2c: designware: Do nothing in system suspend/resume when RT suspended
    There is possibility to enter dw_i2c_plat_suspend() callback twice
    during system suspend under certain cases which is causing here warnings
    from clk_core_disable() and clk_core_unprepare() as well as accessing the
    registers that can be power gated.
    
    Commit 8503ff166504 ("i2c: designware: Avoid unnecessary resuming during
    system suspend") implemented a prepare callback that checks for runtime
    suspended device which allow PM core to set direct_complete flag and
    skip system suspend and resume callbacks.
    
    However it can still happen if nothing resumes the device prior system
    syspend (e.g. acpi_subsys_suspend()) and there is a slave device which
    unsets the direct_complete flag of the parent in __device_suspend() thus
    causing PM code to not skip the system suspend/resume callbacks.
    
    Avoid this by checking runtime status in suspend and resume callbacks
    and return directly if device is runtime suspended. This affects only
    system suspend/resume since during runtime suspend/resume runtime status
    is suspending (not suspended) or resuming.
    
    Signed-off-by: Jarkko Nikula <jarkko.nikula@linux.intel.com>
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- drivers/i2c/busses/i2c-designware-platdrv.c
+++ drivers/i2c/busses/i2c-designware-platdrv.c
@@ -360,6 +360,9 @@ static int dw_i2c_plat_suspend(struct device *dev)
 	struct platform_device *pdev = to_platform_device(dev);
 	struct dw_i2c_dev *i_dev = platform_get_drvdata(pdev);
 
+	if (pm_runtime_suspended(dev))
+		return 0;
+
 	i2c_dw_disable(i_dev);
 	i2c_dw_plat_prepare_clk(i_dev, false);
 
@@ -370,6 +373,9 @@ static int dw_i2c_plat_resume(struct device *dev)
 {
 	struct platform_device *pdev = to_platform_device(dev);
 	struct dw_i2c_dev *i_dev = platform_get_drvdata(pdev);
+
+	if (pm_runtime_suspended(dev))
+		return 0;
 
 	i2c_dw_plat_prepare_clk(i_dev, true);
 
