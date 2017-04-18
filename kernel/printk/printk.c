CodeLinaro-mirror/la_platform_hardware_bsp_kernel_common_v4.4

ANDROID: ARM: Make low-level printk work
Makes low-level printk work.

Includes subsequent fix:

  Arve Hjønnevåg <arve@android.com>:
  ARM: Fix "Make low-level printk work" to use a separate config option

Signed-off-by: Tony Lindgren <tony@atomide.com>
Signed-off-by: Arve Hjønnevåg <arve@android.com>

Bug: None
Patchset: early-printk-direct

Signed-off-by: Mattias Nissler <mnissler@google.com>
Change-Id: I62c0facd1270b259b68b0f1b00f584d5798e671a

kernel/printk/printk.c
@@ -55,6 +55,10 @@
 #include "console_cmdline.h"
 #include "braille.h"
 
+#ifdef CONFIG_EARLY_PRINTK_DIRECT
+extern void printascii(char *);
+#endif
+
 int console_printk[4] = {
 	CONSOLE_LOGLEVEL_DEFAULT,	/* console_loglevel */
 	MESSAGE_LOGLEVEL_DEFAULT,	/* default_message_loglevel */
@@ -1753,6 +1757,10 @@ asmlinkage int vprintk_emit(int facility, int level,
 			text = (char *)end_of_header;
 		}
 	}
+
+#ifdef CONFIG_EARLY_PRINTK_DIRECT
+	printascii(text);
+#endif
 
 	if (level == LOGLEVEL_DEFAULT)
 		level = default_message_loglevel;

microsoft/surface-duo-oss-kernel.msm-4..14

kernel/printk/printk.c
@@ -56,6 +56,10 @@
 #include "braille.h"
 #include "internal.h"
 
+#ifdef CONFIG_DEBUG_LL
+extern void printascii(char *);
+#endif
+
 int console_printk[4] = {
 	CONSOLE_LOGLEVEL_DEFAULT,	/* console_loglevel */
 	MESSAGE_LOGLEVEL_DEFAULT,	/* default_message_loglevel */
@@ -1742,6 +1746,10 @@ asmlinkage int vprintk_emit(int facility, int level,
 	 * prefix which might be passed-in as a parameter.
 	 */
 	text_len = vscnprintf(text, sizeof(textbuf), fmt, args);
+
+#ifdef	CONFIG_DEBUG_LL
+	printascii(text);
+#endif
 
 	/* mark and strip a trailing newline */
 	if (text_len && text[text_len-1] == '\n') {