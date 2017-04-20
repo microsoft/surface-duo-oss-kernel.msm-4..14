70ab8bdd1b60ed4c69f5e2087c021ed156a74938
Amit Pundir <amit.pundir@linaro.org>
2017-04-20 20:00:38 +0530

    ANDROID: mmc: block: rename rqc to new_req
    Upstream commit acd8dbd64a2a ("mmc: block: rename rqc and req")
    renamed "rqc" to "new_req". No functional changes.
    
    Fixes: Change-Id: I342bfbd8b85f9b790e3f0e1e4e51a900ae07e05d
           ("ANDROID: mmc: Add CONFIG_MMC_SIMULATE_MAX_SPEED")
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- drivers/mmc/core/block.c
+++ drivers/mmc/core/block.c
@@ -1944,7 +1944,7 @@ static void mmc_blk_issue_rw_rq(struct mmc_queue *mq, struct request *new_req)
 			 */
 			mmc_blk_reset_success(md, type);
 
-			mmc_blk_simulate_delay(mq, rqc, waitfor);
+			mmc_blk_simulate_delay(mq, new_req, waitfor);
 
 			req_pending = blk_end_request(old_req, 0,
 						      brq->data.bytes_xfered);
