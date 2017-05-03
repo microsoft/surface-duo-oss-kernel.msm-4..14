6309a1248b99e4e870fd29b0a6e6ffcfb8c29371
Luffy Yuan <luffy.yuan@arm.com>
2017-05-03 12:39:48 -0700

    MALI: utgard ddk support for Hikey with kernel 4.9
    Patch originally by Luffy Yuan, which re-adds the
    mali_internal_sync logic and fixes the timeline
    and sync code to work with the new dmabuf fences.
    
    Change-Id: Ic59a33e96345f7f53f8d92f113efb1616763e724
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- drivers/gpu/arm/utgard/linux/mali_sync.h
+++ drivers/gpu/arm/utgard/linux/mali_sync.h
@@ -17,14 +17,17 @@
 #ifndef _MALI_SYNC_H_
 #define _MALI_SYNC_H_
 
-#if defined(CONFIG_SYNC)
+#if defined(CONFIG_SYNC) || defined(CONFIG_SYNC_FILE)
 
 #include <linux/seq_file.h>
 #include <linux/version.h>
+
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3, 10, 0)
 #include <linux/sync.h>
-#else
+#elif LINUX_VERSION_CODE < KERNEL_VERSION(4, 6, 0)
 #include <sync.h>
+#else
+#include "mali_internal_sync.h"
 #endif
 
 
@@ -33,6 +36,7 @@
 struct mali_sync_flag;
 struct mali_timeline;
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 6, 0)
 /**
  * Create a sync timeline.
  *
@@ -67,6 +71,7 @@ struct sync_fence *mali_sync_fence_merge(struct sync_fence *sync_fence1, struct 
  */
 struct sync_fence *mali_sync_timeline_create_signaled_fence(struct sync_timeline *sync_tl);
 
+
 /**
  * Create a sync flag.
  *
@@ -76,6 +81,67 @@ struct sync_fence *mali_sync_timeline_create_signaled_fence(struct sync_timeline
  */
 struct mali_sync_flag *mali_sync_flag_create(struct sync_timeline *sync_tl, u32 point);
 
+/**
+ * Create a sync fence attached to given sync flag.
+ *
+ * @param flag Sync flag.
+ * @return New sync fence if successful, NULL if not.
+ */
+struct sync_fence *mali_sync_flag_create_fence(struct mali_sync_flag *flag);
+#else
+/**
+ * Create a sync timeline.
+ *
+ * @param name Name of the sync timeline.
+ * @return The new sync timeline if successful, NULL if not.
+ */
+struct mali_internal_sync_timeline *mali_sync_timeline_create(struct mali_timeline *timeline, const char *name);
+
+/**
+ * Creates a file descriptor representing the sync fence.  Will release sync fence if allocation of
+ * file descriptor fails.
+ *
+ * @param sync_fence Sync fence.
+ * @return File descriptor representing sync fence if successful, or -1 if not.
+ */
+s32 mali_sync_fence_fd_alloc(struct mali_internal_sync_fence *sync_fence);
+
+/**
+ * Merges two sync fences.  Both input sync fences will be released.
+ *
+ * @param sync_fence1 First sync fence.
+ * @param sync_fence2 Second sync fence.
+ * @return New sync fence that is the result of the merger if successful, or NULL if not.
+ */
+struct mali_internal_sync_fence *mali_sync_fence_merge(struct mali_internal_sync_fence *sync_fence1, struct mali_internal_sync_fence *sync_fence2);
+
+/**
+ * Create a sync fence that is already signaled.
+ *
+ * @param tl Sync timeline.
+ * @return New signaled sync fence if successful, NULL if not.
+ */
+struct mali_internal_sync_fence *mali_sync_timeline_create_signaled_fence(struct mali_internal_sync_timeline *sync_tl);
+
+
+/**
+ * Create a sync flag.
+ *
+ * @param sync_tl Sync timeline.
+ * @param point Point on Mali timeline.
+ * @return New sync flag if successful, NULL if not.
+ */
+struct mali_sync_flag *mali_sync_flag_create(struct mali_internal_sync_timeline *sync_tl, u32 point);
+
+/**
+ * Create a sync fence attached to given sync flag.
+ *
+ * @param flag Sync flag.
+ * @return New sync fence if successful, NULL if not.
+ */
+struct mali_internal_sync_fence *mali_sync_flag_create_fence(struct mali_sync_flag *flag);
+
+#endif
 /**
  * Grab sync flag reference.
  *
@@ -98,14 +164,6 @@ void mali_sync_flag_put(struct mali_sync_flag *flag);
  */
 void mali_sync_flag_signal(struct mali_sync_flag *flag, int error);
 
-/**
- * Create a sync fence attached to given sync flag.
- *
- * @param flag Sync flag.
- * @return New sync fence if successful, NULL if not.
- */
-struct sync_fence *mali_sync_flag_create_fence(struct mali_sync_flag *flag);
-
-#endif /* defined(CONFIG_SYNC) */
+#endif /* defined(CONFIG_SYNC) || defined(CONFIG_SYNC_FILE) */
 
 #endif /* _MALI_SYNC_H_ */
