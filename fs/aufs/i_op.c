d4659c82a1b95bb488dcd5089aaa98b449898e88
Ondrej Kubik <ondrej.kubik@canonical.com>
2017-05-10 17:58:17 +0100

    Fixing compile error in aufs with enabled PREEMPT_RT_FULL
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>

--- fs/aufs/i_op.c
+++ fs/aufs/i_op.c
@@ -589,7 +589,11 @@ out:
 void au_pin_hdir_set_owner(struct au_pin *p, struct task_struct *task)
 {
 #if defined(CONFIG_DEBUG_MUTEXES) || defined(CONFIG_SMP)
+#ifdef CONFIG_PREEMPT_RT_FULL
+        p->hdir->hi_inode->i_mutex.lock.owner = task;
+#else
 	p->hdir->hi_inode->i_mutex.owner = task;
+#endif
 #endif
 }
 
