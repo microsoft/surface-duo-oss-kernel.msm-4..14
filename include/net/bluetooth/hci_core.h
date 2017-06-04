vishalbhoj/linux

Bluetooth: Add LED triggers for bluetooth packet receive and transmit
Two LED triggers are defined: [hdev->name]tx and [hdev->name]rx. Upon packets
availabe in HCI core layer, they blink.

Signed-off-by: Guodong Xu <guodong.xu@linaro.org>

include/net/bluetooth/hci_core.h
@@ -330,6 +330,11 @@ struct hci_dev {
 
 	struct rfkill		*rfkill;
 
+#ifdef CONFIG_BLUETOOTH_LEDS
+	struct led_trigger	*tx_led, *rx_led;
+	char			tx_led_name[32], rx_led_name[32];
+#endif
+
 	unsigned long		dbg_flags;
 	unsigned long		dev_flags;
 

microsoft/surface-duo-oss-kernel.msm-4..14

include/net/bluetooth/hci_core.h
@@ -370,6 +370,11 @@ struct hci_dev {
 
 	DECLARE_BITMAP(dev_flags, __HCI_NUM_FLAGS);
 
+#ifdef CONFIG_BT_LEDS
+	struct led_trigger	*tx_led, *rx_led;
+	char			tx_led_name[32], rx_led_name[32];
+#endif
+
 	struct delayed_work	le_scan_disable;
 	struct delayed_work	le_scan_restart;
 