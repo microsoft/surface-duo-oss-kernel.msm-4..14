9cb2e4cfe1bd609e58d89203bf1446b6d0f1f575
John Stultz <john.stultz@linaro.org>
2017-07-05 15:31:36 -0700

    HACK: ion: Revert ION to 4.11 era code to avoid ABI break
    The ION abi has been changed to allow it to be upstreamed,
    but this unfortunately breaks userspace gralloc/libion code
    in AOSP. So until that code can be updated to support 4.12+
    kernels, revert the ION code back to 4.11.
    
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- drivers/staging/android/ion/ion_page_pool.c
+++ drivers/staging/android/ion/ion_page_pool.c
@@ -22,8 +22,7 @@
 #include <linux/init.h>
 #include <linux/slab.h>
 #include <linux/swap.h>
-
-#include "ion.h"
+#include "ion_priv.h"
 
 static void *ion_page_pool_alloc_pages(struct ion_page_pool *pool)
 {
@@ -31,6 +30,9 @@ static void *ion_page_pool_alloc_pages(struct ion_page_pool *pool)
 
 	if (!page)
 		return NULL;
+	if (!pool->cached)
+		ion_pages_sync_for_device(NULL, page, PAGE_SIZE << pool->order,
+					  DMA_BIDIRECTIONAL);
 	return page;
 }
 
