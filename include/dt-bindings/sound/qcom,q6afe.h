23ee65d4fad8ccf4190b71eaadc340bdd22918a0
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2017-07-14 16:54:03 +0100

    ASoC: qcom: qdsp6: Add support to Q6AFE
    This patch adds support to Q6AFE (Audio Front End) module on Q6DSP.
    
    AFE module sits right at the other end of cpu where the codec/audio
    devices are connected.
    
    AFE provides abstraced interfaces to both hardware and virtual devices.
    Each AFE tx/rx port can be configured to connect to one of the hardware
    devices like codec, hdmi, slimbus, i2s and so on. AFE services include
    starting, stopping, and if needed, any configurations of the ports.
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>

---
+++ include/dt-bindings/sound/qcom,q6afe.h
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0
+#ifndef __DT_BINDINGS_Q6_AFE_H__
+#define __DT_BINDINGS_Q6_AFE_H__
+
+/* Audio Front End (AFE) Ports */
+#define AFE_PORT_HDMI_RX	8
+
+#endif /* __DT_BINDINGS_Q6_AFE_H__ */
+
