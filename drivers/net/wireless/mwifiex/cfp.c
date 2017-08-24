8a034f97a28dba62026343eef7992766c91273a7
Wen-chien Jesse Sung <jesse.sung@canonical.com>
2017-08-24 10:17:00 +0200

    UBUNTU: SAUCE: mwifiex: do not dereference invalid pointer
    BugLink: https://launchpad.net/bugs/1712746
    
    Check priv->wdev.wiphy before dereference.
    
    Signed-off-by: Wen-chien Jesse Sung <jesse.sung@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-By: AceLan Kao <acelan.kao@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- drivers/net/wireless/mwifiex/cfp.c
+++ drivers/net/wireless/mwifiex/cfp.c
@@ -324,7 +324,9 @@ mwifiex_get_cfp(struct mwifiex_private *priv, u8 band, u16 channel, u32 freq)
 	if (mwifiex_band_to_radio_type(band) == HostCmd_SCAN_RADIO_TYPE_BG)
 		sband = priv->wdev.wiphy->bands[IEEE80211_BAND_2GHZ];
 	else
-		sband = priv->wdev.wiphy->bands[IEEE80211_BAND_5GHZ];
+		sband = priv->wdev.wiphy ?
+				priv->wdev.wiphy->bands[IEEE80211_BAND_5GHZ] :
+				NULL;
 
 	if (!sband) {
 		mwifiex_dbg(priv->adapter, ERROR,
