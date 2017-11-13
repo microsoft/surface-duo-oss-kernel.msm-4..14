d39b68d9ca083f2e99d06d6ba7571c56b1cd1233
Todor Tomov <todor.tomov@linaro.org>
2017-11-13 17:31:07 +0200

    camss: vfe: Use hw specific code via callbacks
    Signed-off-by: Todor Tomov <todor.tomov@linaro.org>

--- drivers/media/platform/qcom/camss/camss-vfe-4-1.c
+++ drivers/media/platform/qcom/camss/camss-vfe-4-1.c
@@ -19,7 +19,7 @@
 #include <linux/iopoll.h>
 
 #include "camss-vfe.h"
-#include "camss-vfe-4-1.h"
+
 
 #define VFE_0_HW_VERSION		0x000
 
@@ -105,7 +105,7 @@
 #define VFE_0_BUS_IMAGE_MASTER_n_WR_PONG_ADDR(n)	(0x074 + 0x24 * (n))
 #define VFE_0_BUS_IMAGE_MASTER_n_WR_ADDR_CFG(n)		(0x078 + 0x24 * (n))
 #define VFE_0_BUS_IMAGE_MASTER_n_WR_ADDR_CFG_FRM_DROP_PER_SHIFT	2
-#define VFE_0_BUS_IMAGE_MASTER_n_WR_ADDR_CFG_FRM_DROP_PER_MASK	(0x1F << 2)
+#define VFE_0_BUS_IMAGE_MASTER_n_WR_ADDR_CFG_FRM_DROP_PER_MASK	(0x1f << 2)
 
 #define VFE_0_BUS_IMAGE_MASTER_n_WR_UB_CFG(n)		(0x07c + 0x24 * (n))
 #define VFE_0_BUS_IMAGE_MASTER_n_WR_UB_CFG_OFFSET_SHIFT	16
@@ -214,14 +214,14 @@
 #define MSM_VFE_VFE0_UB_SIZE 1023
 #define MSM_VFE_VFE0_UB_SIZE_RDI (MSM_VFE_VFE0_UB_SIZE / 3)
 
-void vfe_hw_version_read(struct vfe_device *vfe, struct device *dev)
+static void vfe_hw_version_read(struct vfe_device *vfe, struct device *dev)
 {
 	u32 hw_version = readl_relaxed(vfe->base + VFE_0_HW_VERSION);
 
 	dev_dbg(dev, "VFE HW Version = 0x%08x\n", hw_version);
 }
 
-u16 vfe_get_ub_size(u8 vfe_id)
+static u16 vfe_get_ub_size(u8 vfe_id)
 {
 	if (vfe_id == 0)
 		return MSM_VFE_VFE0_UB_SIZE_RDI;
@@ -243,7 +243,7 @@ static inline void vfe_reg_set(struct vfe_device *vfe, u32 reg, u32 set_bits)
 	writel_relaxed(bits | set_bits, vfe->base + reg);
 }
 
-void vfe_global_reset(struct vfe_device *vfe)
+static void vfe_global_reset(struct vfe_device *vfe)
 {
 	u32 reset_bits = VFE_0_GLOBAL_RESET_CMD_TESTGEN		|
 			 VFE_0_GLOBAL_RESET_CMD_BUS_MISR	|
@@ -258,18 +258,18 @@ void vfe_global_reset(struct vfe_device *vfe)
 	writel_relaxed(reset_bits, vfe->base + VFE_0_GLOBAL_RESET_CMD);
 }
 
-void vfe_halt_request(struct vfe_device *vfe)
+static void vfe_halt_request(struct vfe_device *vfe)
 {
 	writel_relaxed(VFE_0_BUS_BDG_CMD_HALT_REQ,
 		       vfe->base + VFE_0_BUS_BDG_CMD);
 }
 
-void vfe_halt_clear(struct vfe_device *vfe)
+static void vfe_halt_clear(struct vfe_device *vfe)
 {
 	writel_relaxed(0x0, vfe->base + VFE_0_BUS_BDG_CMD);
 }
 
-void vfe_wm_enable(struct vfe_device *vfe, u8 wm, u8 enable)
+static void vfe_wm_enable(struct vfe_device *vfe, u8 wm, u8 enable)
 {
 	if (enable)
 		vfe_reg_set(vfe, VFE_0_BUS_IMAGE_MASTER_n_WR_CFG(wm),
@@ -279,7 +279,7 @@ void vfe_wm_enable(struct vfe_device *vfe, u8 wm, u8 enable)
 			    1 << VFE_0_BUS_IMAGE_MASTER_n_WR_CFG_WR_PATH_SHIFT);
 }
 
-void vfe_wm_frame_based(struct vfe_device *vfe, u8 wm, u8 enable)
+static void vfe_wm_frame_based(struct vfe_device *vfe, u8 wm, u8 enable)
 {
 	if (enable)
 		vfe_reg_set(vfe, VFE_0_BUS_IMAGE_MASTER_n_WR_CFG(wm),
@@ -334,9 +334,9 @@ static void vfe_get_wm_sizes(struct v4l2_pix_format_mplane *pix, u8 plane,
 	}
 }
 
-void vfe_wm_line_based(struct vfe_device *vfe, u32 wm,
-		       struct v4l2_pix_format_mplane *pix,
-		       u8 plane, u32 enable)
+static void vfe_wm_line_based(struct vfe_device *vfe, u32 wm,
+			      struct v4l2_pix_format_mplane *pix,
+			      u8 plane, u32 enable)
 {
 	u32 reg;
 
@@ -369,7 +369,7 @@ void vfe_wm_line_based(struct vfe_device *vfe, u32 wm,
 	}
 }
 
-void vfe_wm_set_framedrop_period(struct vfe_device *vfe, u8 wm, u8 per)
+static void vfe_wm_set_framedrop_period(struct vfe_device *vfe, u8 wm, u8 per)
 {
 	u32 reg;
 
@@ -385,13 +385,15 @@ void vfe_wm_set_framedrop_period(struct vfe_device *vfe, u8 wm, u8 per)
 		       vfe->base + VFE_0_BUS_IMAGE_MASTER_n_WR_ADDR_CFG(wm));
 }
 
-void vfe_wm_set_framedrop_pattern(struct vfe_device *vfe, u8 wm, u32 pattern)
+static void vfe_wm_set_framedrop_pattern(struct vfe_device *vfe, u8 wm,
+					 u32 pattern)
 {
 	writel_relaxed(pattern,
 	       vfe->base + VFE_0_BUS_IMAGE_MASTER_n_WR_FRAMEDROP_PATTERN(wm));
 }
 
-void vfe_wm_set_ub_cfg(struct vfe_device *vfe, u8 wm, u16 offset, u16 depth)
+static void vfe_wm_set_ub_cfg(struct vfe_device *vfe, u8 wm,
+			      u16 offset, u16 depth)
 {
 	u32 reg;
 
@@ -400,26 +402,26 @@ void vfe_wm_set_ub_cfg(struct vfe_device *vfe, u8 wm, u16 offset, u16 depth)
 	writel_relaxed(reg, vfe->base + VFE_0_BUS_IMAGE_MASTER_n_WR_UB_CFG(wm));
 }
 
-void vfe_bus_reload_wm(struct vfe_device *vfe, u8 wm)
+static void vfe_bus_reload_wm(struct vfe_device *vfe, u8 wm)
 {
 	wmb();
 	writel_relaxed(VFE_0_BUS_CMD_Mx_RLD_CMD(wm), vfe->base + VFE_0_BUS_CMD);
 	wmb();
 }
 
-void vfe_wm_set_ping_addr(struct vfe_device *vfe, u8 wm, u32 addr)
+static void vfe_wm_set_ping_addr(struct vfe_device *vfe, u8 wm, u32 addr)
 {
 	writel_relaxed(addr,
 		       vfe->base + VFE_0_BUS_IMAGE_MASTER_n_WR_PING_ADDR(wm));
 }
 
-void vfe_wm_set_pong_addr(struct vfe_device *vfe, u8 wm, u32 addr)
+static void vfe_wm_set_pong_addr(struct vfe_device *vfe, u8 wm, u32 addr)
 {
 	writel_relaxed(addr,
 		       vfe->base + VFE_0_BUS_IMAGE_MASTER_n_WR_PONG_ADDR(wm));
 }
 
-int vfe_wm_get_ping_pong_status(struct vfe_device *vfe, u8 wm)
+static int vfe_wm_get_ping_pong_status(struct vfe_device *vfe, u8 wm)
 {
 	u32 reg;
 
@@ -428,7 +430,7 @@ int vfe_wm_get_ping_pong_status(struct vfe_device *vfe, u8 wm)
 	return (reg >> wm) & 0x1;
 }
 
-void vfe_bus_enable_wr_if(struct vfe_device *vfe, u8 enable)
+static void vfe_bus_enable_wr_if(struct vfe_device *vfe, u8 enable)
 {
 	if (enable)
 		writel_relaxed(0x10000009, vfe->base + VFE_0_BUS_CFG);
@@ -436,8 +438,8 @@ void vfe_bus_enable_wr_if(struct vfe_device *vfe, u8 enable)
 		writel_relaxed(0, vfe->base + VFE_0_BUS_CFG);
 }
 
-void vfe_bus_connect_wm_to_rdi(struct vfe_device *vfe, u8 wm,
-			       enum vfe_line_id id)
+static void vfe_bus_connect_wm_to_rdi(struct vfe_device *vfe, u8 wm,
+				      enum vfe_line_id id)
 {
 	u32 reg;
 
@@ -472,15 +474,15 @@ void vfe_bus_connect_wm_to_rdi(struct vfe_device *vfe, u8 wm,
 	vfe_reg_set(vfe, VFE_0_BUS_XBAR_CFG_x(wm), reg);
 }
 
-void vfe_wm_set_subsample(struct vfe_device *vfe, u8 wm)
+static void vfe_wm_set_subsample(struct vfe_device *vfe, u8 wm)
 {
 	writel_relaxed(VFE_0_BUS_IMAGE_MASTER_n_WR_IRQ_SUBSAMPLE_PATTERN_DEF,
 	       vfe->base +
 	       VFE_0_BUS_IMAGE_MASTER_n_WR_IRQ_SUBSAMPLE_PATTERN(wm));
 }
 
-void vfe_bus_disconnect_wm_from_rdi(struct vfe_device *vfe, u8 wm,
-				    enum vfe_line_id id)
+static void vfe_bus_disconnect_wm_from_rdi(struct vfe_device *vfe, u8 wm,
+					   enum vfe_line_id id)
 {
 	u32 reg;
 
@@ -512,8 +514,8 @@ void vfe_bus_disconnect_wm_from_rdi(struct vfe_device *vfe, u8 wm,
 	vfe_reg_clr(vfe, VFE_0_BUS_XBAR_CFG_x(wm), reg);
 }
 
-void vfe_set_xbar_cfg(struct vfe_device *vfe, struct vfe_output *output,
-		      u8 enable)
+static void vfe_set_xbar_cfg(struct vfe_device *vfe, struct vfe_output *output,
+			     u8 enable)
 {
 	struct vfe_line *line = container_of(output, struct vfe_line, output);
 	u32 p = line->video_out.active_fmt.fmt.pix_mp.pixelformat;
@@ -547,7 +549,7 @@ void vfe_set_xbar_cfg(struct vfe_device *vfe, struct vfe_output *output,
 	}
 }
 
-void vfe_set_rdi_cid(struct vfe_device *vfe, enum vfe_line_id id, u8 cid)
+static void vfe_set_rdi_cid(struct vfe_device *vfe, enum vfe_line_id id, u8 cid)
 {
 	vfe_reg_clr(vfe, VFE_0_RDI_CFG_x(id),
 		    VFE_0_RDI_CFG_x_RDI_M0_SEL_MASK);
@@ -556,7 +558,7 @@ void vfe_set_rdi_cid(struct vfe_device *vfe, enum vfe_line_id id, u8 cid)
 		    cid << VFE_0_RDI_CFG_x_RDI_M0_SEL_SHIFT);
 }
 
-void vfe_reg_update(struct vfe_device *vfe, enum vfe_line_id line_id)
+static void vfe_reg_update(struct vfe_device *vfe, enum vfe_line_id line_id)
 {
 	vfe->reg_update |= VFE_0_REG_UPDATE_line_n(line_id);
 	wmb();
@@ -564,13 +566,14 @@ void vfe_reg_update(struct vfe_device *vfe, enum vfe_line_id line_id)
 	wmb();
 }
 
-inline void vfe_reg_update_clear(struct vfe_device *vfe,
-				 enum vfe_line_id line_id)
+static inline void vfe_reg_update_clear(struct vfe_device *vfe,
+					enum vfe_line_id line_id)
 {
 	vfe->reg_update &= ~VFE_0_REG_UPDATE_line_n(line_id);
 }
-void vfe_enable_irq_wm_line(struct vfe_device *vfe, u8 wm,
-			    enum vfe_line_id line_id, u8 enable)
+
+static void vfe_enable_irq_wm_line(struct vfe_device *vfe, u8 wm,
+				   enum vfe_line_id line_id, u8 enable)
 {
 	u32 irq_en0 = VFE_0_IRQ_MASK_0_IMAGE_MASTER_n_PING_PONG(wm) |
 		      VFE_0_IRQ_MASK_0_line_n_REG_UPDATE(line_id);
@@ -586,8 +589,8 @@ void vfe_enable_irq_wm_line(struct vfe_device *vfe, u8 wm,
 	}
 }
 
-void vfe_enable_irq_pix_line(struct vfe_device *vfe, u8 comp,
-			     enum vfe_line_id line_id, u8 enable)
+static void vfe_enable_irq_pix_line(struct vfe_device *vfe, u8 comp,
+				    enum vfe_line_id line_id, u8 enable)
 {
 	struct vfe_output *output = &vfe->line[line_id].output;
 	unsigned int i;
@@ -617,7 +620,7 @@ void vfe_enable_irq_pix_line(struct vfe_device *vfe, u8 comp,
 	}
 }
 
-void vfe_enable_irq_common(struct vfe_device *vfe)
+static void vfe_enable_irq_common(struct vfe_device *vfe)
 {
 	u32 irq_en0 = VFE_0_IRQ_MASK_0_RESET_ACK;
 	u32 irq_en1 = VFE_0_IRQ_MASK_1_VIOLATION |
@@ -627,7 +630,7 @@ void vfe_enable_irq_common(struct vfe_device *vfe)
 	vfe_reg_set(vfe, VFE_0_IRQ_MASK_1, irq_en1);
 }
 
-void vfe_set_demux_cfg(struct vfe_device *vfe, struct vfe_line *line)
+static void vfe_set_demux_cfg(struct vfe_device *vfe, struct vfe_line *line)
 {
 	u32 val, even_cfg, odd_cfg;
 
@@ -677,7 +680,7 @@ static inline u8 vfe_calc_interp_reso(u16 input, u16 output)
 	return 3;
 }
 
-void vfe_set_scale_cfg(struct vfe_device *vfe, struct vfe_line *line)
+static void vfe_set_scale_cfg(struct vfe_device *vfe, struct vfe_line *line)
 {
 	u32 p = line->video_out.active_fmt.fmt.pix_mp.pixelformat;
 	u32 reg;
@@ -732,7 +735,7 @@ void vfe_set_scale_cfg(struct vfe_device *vfe, struct vfe_line *line)
 	writel_relaxed(reg, vfe->base + VFE_0_SCALE_ENC_CBCR_V_PHASE);
 }
 
-void vfe_set_crop_cfg(struct vfe_device *vfe, struct vfe_line *line)
+static void vfe_set_crop_cfg(struct vfe_device *vfe, struct vfe_line *line)
 {
 	u32 p = line->video_out.active_fmt.fmt.pix_mp.pixelformat;
 	u32 reg;
@@ -763,7 +766,7 @@ void vfe_set_crop_cfg(struct vfe_device *vfe, struct vfe_line *line)
 	writel_relaxed(reg, vfe->base + VFE_0_CROP_ENC_CBCR_HEIGHT);
 }
 
-void vfe_set_clamp_cfg(struct vfe_device *vfe)
+static void vfe_set_clamp_cfg(struct vfe_device *vfe)
 {
 	u32 val = VFE_0_CLAMP_ENC_MAX_CFG_CH0 |
 		VFE_0_CLAMP_ENC_MAX_CFG_CH1 |
@@ -778,7 +781,7 @@ void vfe_set_clamp_cfg(struct vfe_device *vfe)
 	writel_relaxed(val, vfe->base + VFE_0_CLAMP_ENC_MIN_CFG);
 }
 
-void vfe_set_qos(struct vfe_device *vfe)
+static void vfe_set_qos(struct vfe_device *vfe)
 {
 	u32 val = VFE_0_BUS_BDG_QOS_CFG_0_CFG;
 	u32 val7 = VFE_0_BUS_BDG_QOS_CFG_7_CFG;
@@ -793,7 +796,7 @@ void vfe_set_qos(struct vfe_device *vfe)
 	writel_relaxed(val7, vfe->base + VFE_0_BUS_BDG_QOS_CFG_7);
 }
 
-void vfe_set_cgc_override(struct vfe_device *vfe, u8 wm, u8 enable)
+static void vfe_set_cgc_override(struct vfe_device *vfe, u8 wm, u8 enable)
 {
 	u32 val = VFE_0_CGC_OVERRIDE_1_IMAGE_Mx_CGC_OVERRIDE(wm);
 
@@ -805,7 +808,7 @@ void vfe_set_cgc_override(struct vfe_device *vfe, u8 wm, u8 enable)
 	wmb();
 }
 
-void vfe_set_camif_cfg(struct vfe_device *vfe, struct vfe_line *line)
+static void vfe_set_camif_cfg(struct vfe_device *vfe, struct vfe_line *line)
 {
 	u32 val;
 
@@ -850,7 +853,7 @@ void vfe_set_camif_cfg(struct vfe_device *vfe, struct vfe_line *line)
 	writel_relaxed(val, vfe->base + VFE_0_CAMIF_CFG);
 }
 
-void vfe_set_camif_cmd(struct vfe_device *vfe, u8 enable)
+static void vfe_set_camif_cmd(struct vfe_device *vfe, u8 enable)
 {
 	u32 cmd;
 
@@ -865,7 +868,7 @@ void vfe_set_camif_cmd(struct vfe_device *vfe, u8 enable)
 	writel_relaxed(cmd, vfe->base + VFE_0_CAMIF_CMD);
 }
 
-void vfe_set_module_cfg(struct vfe_device *vfe, u8 enable)
+static void vfe_set_module_cfg(struct vfe_device *vfe, u8 enable)
 {
 	u32 val = VFE_0_MODULE_CFG_DEMUX |
 		  VFE_0_MODULE_CFG_CHROMA_UPSAMPLE |
@@ -878,7 +881,7 @@ void vfe_set_module_cfg(struct vfe_device *vfe, u8 enable)
 		writel_relaxed(0x0, vfe->base + VFE_0_MODULE_CFG);
 }
 
-int vfe_camif_wait_for_stop(struct vfe_device *vfe, struct device *dev)
+static int vfe_camif_wait_for_stop(struct vfe_device *vfe, struct device *dev)
 {
 	u32 val;
 	int ret;
@@ -894,7 +897,7 @@ int vfe_camif_wait_for_stop(struct vfe_device *vfe, struct device *dev)
 	return ret;
 }
 
-void vfe_isr_read(struct vfe_device *vfe, u32 *value0, u32 *value1)
+static void vfe_isr_read(struct vfe_device *vfe, u32 *value0, u32 *value1)
 {
 	*value0 = readl_relaxed(vfe->base + VFE_0_IRQ_STATUS_0);
 	*value1 = readl_relaxed(vfe->base + VFE_0_IRQ_STATUS_1);
@@ -906,9 +909,50 @@ void vfe_isr_read(struct vfe_device *vfe, u32 *value0, u32 *value1)
 	writel_relaxed(VFE_0_IRQ_CMD_GLOBAL_CLEAR, vfe->base + VFE_0_IRQ_CMD);
 }
 
-void vfe_violation_read(struct vfe_device *vfe, struct device *dev)
+static void vfe_violation_read(struct vfe_device *vfe, struct device *dev)
 {
 	u32 violation = readl_relaxed(vfe->base + VFE_0_VIOLATION_STATUS);
 
 	dev_err_ratelimited(dev, "VFE: violation = 0x%08x\n", violation);
 }
+
+const struct vfe_hw_ops vfe_ops_4_1 = {
+	.hw_version_read = vfe_hw_version_read,
+	.get_ub_size = vfe_get_ub_size,
+	.global_reset = vfe_global_reset,
+	.halt_request = vfe_halt_request,
+	.halt_clear = vfe_halt_clear,
+	.wm_enable = vfe_wm_enable,
+	.wm_frame_based = vfe_wm_frame_based,
+	.wm_line_based = vfe_wm_line_based,
+	.wm_set_framedrop_period = vfe_wm_set_framedrop_period,
+	.wm_set_framedrop_pattern = vfe_wm_set_framedrop_pattern,
+	.wm_set_ub_cfg = vfe_wm_set_ub_cfg,
+	.bus_reload_wm = vfe_bus_reload_wm,
+	.wm_set_ping_addr = vfe_wm_set_ping_addr,
+	.wm_set_pong_addr = vfe_wm_set_pong_addr,
+	.wm_get_ping_pong_status = vfe_wm_get_ping_pong_status,
+	.bus_enable_wr_if = vfe_bus_enable_wr_if,
+	.bus_connect_wm_to_rdi = vfe_bus_connect_wm_to_rdi,
+	.wm_set_subsample = vfe_wm_set_subsample,
+	.bus_disconnect_wm_from_rdi = vfe_bus_disconnect_wm_from_rdi,
+	.set_xbar_cfg = vfe_set_xbar_cfg,
+	.set_rdi_cid = vfe_set_rdi_cid,
+	.reg_update = vfe_reg_update,
+	.reg_update_clear = vfe_reg_update_clear,
+	.enable_irq_wm_line = vfe_enable_irq_wm_line,
+	.enable_irq_pix_line = vfe_enable_irq_pix_line,
+	.enable_irq_common = vfe_enable_irq_common,
+	.set_demux_cfg = vfe_set_demux_cfg,
+	.set_scale_cfg = vfe_set_scale_cfg,
+	.set_crop_cfg = vfe_set_crop_cfg,
+	.set_clamp_cfg = vfe_set_clamp_cfg,
+	.set_qos = vfe_set_qos,
+	.set_cgc_override = vfe_set_cgc_override,
+	.set_camif_cfg = vfe_set_camif_cfg,
+	.set_camif_cmd = vfe_set_camif_cmd,
+	.set_module_cfg = vfe_set_module_cfg,
+	.camif_wait_for_stop = vfe_camif_wait_for_stop,
+	.isr_read = vfe_isr_read,
+	.violation_read = vfe_violation_read,
+};
