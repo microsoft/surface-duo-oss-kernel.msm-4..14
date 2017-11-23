b541655e47040aaa699f9edc47cb83ad5992fedc
Chris Redpath <chris.redpath@arm.com>
2017-11-23 14:05:05 +0000

    ANDROID: trace/sched: Add tracepoint for sched_overutilized events
    Change-Id: Ieb46736cb21fe58024c434cd678af0abb8b0d249
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5376,6 +5376,8 @@ static inline void hrtick_update(struct rq *rq)
 #ifdef CONFIG_SMP
 static bool cpu_overutilized(int cpu);
 
+static unsigned long cpu_util(int cpu);
+
 static bool sd_overutilized(struct sched_domain *sd)
 {
 	return sd->shared->overutilized;
@@ -5383,11 +5385,13 @@ static bool sd_overutilized(struct sched_domain *sd)
 
 static void set_sd_overutilized(struct sched_domain *sd)
 {
+	trace_sched_overutilized(sd, sd->shared->overutilized, true);
 	sd->shared->overutilized = true;
 }
 
 static void clear_sd_overutilized(struct sched_domain *sd)
 {
+	trace_sched_overutilized(sd, sd->shared->overutilized, false);
 	sd->shared->overutilized = false;
 }
 
