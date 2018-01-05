e1f3564b8cc86501d44e36280de0de3b1d938f1c
Michael Ellerman <mpe@ellerman.id.au>
2018-01-05 22:34:03 +1100

    UBUNTU: SAUCE: rfi-flush: Push the instruction selection down to the patching routine
    CVE-2017-5754
    
    BugLink: http://bugs.launchpad.net/bugs/1742772
    
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Signed-off-by: Marcelo Henrique Cerri <marcelo.cerri@canonical.com>

--- arch/powerpc/include/asm/feature-fixups.h
+++ arch/powerpc/include/asm/feature-fixups.h
@@ -196,7 +196,6 @@ label##3:					       	\
 #include <linux/types.h>
 
 extern long __start___rfi_flush_fixup, __stop___rfi_flush_fixup;
-extern void do_rfi_flush_fixups(bool enable, unsigned int insn);
 
 #endif
 
