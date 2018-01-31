260de686e30c73c135a658e86823d5dcdc76c9ff
Andy Whitcroft <apw@canonical.com>
2018-01-31 15:53:33 +0000

    Revert "x86/mm: Only set IBPB when the new thread cannot ptrace current thread"
    CVE-2017-5753 (revert embargoed)
    CVE-2017-5715 (revert embargoed)
    
    This reverts commit b0c3e8bd040546f4dfedff77e3a171a9b15c6571.
    
    Signed-off-by: Andy Whitcroft <apw@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- arch/x86/mm/tlb.c
+++ arch/x86/mm/tlb.c
@@ -7,7 +7,6 @@
 #include <linux/module.h>
 #include <linux/cpu.h>
 #include <linux/debugfs.h>
-#include <linux/ptrace.h>
 
 #include <asm/tlbflush.h>
 #include <asm/mmu_context.h>
@@ -141,9 +140,7 @@ void switch_mm(struct mm_struct *prev, struct mm_struct *next,
 		/* Stop flush ipis for the previous mm */
 		cpumask_clear_cpu(cpu, mm_cpumask(prev));
 
-		/* Null tsk means switching to kernel, so that's safe */
-		if (boot_cpu_has(X86_FEATURE_SPEC_CTRL) && tsk &&
-			___ptrace_may_access(tsk, current, PTRACE_MODE_IBPB))
+		if (boot_cpu_has(X86_FEATURE_SPEC_CTRL))
 			native_wrmsrl(MSR_IA32_PRED_CMD, FEATURE_SET_IBPB);
 
 		/* Load per-mm CR4 state */
