f6751014dd8a26f3e233b6f464b897a9a0a09740
Larisa Grigore <Larisa.Grigore@nxp.com>
2018-02-09 15:58:51 +0200

    dts: s32gen1: Add clocking support
    Issue: ALB-1553
    Signed-off-by: Larisa Grigore <Larisa.Grigore@nxp.com>

--- drivers/clk/s32/s32-gen1/dfs.h
+++ drivers/clk/s32/s32-gen1/dfs.h
@@ -6,8 +6,8 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- *
  */
+
 #ifndef _DFS_S32GEN1_H
 #define _DFS_S32GEN1_H
 
