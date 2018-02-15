5d33976583e3a27d0d99c5c19738534a282238f8
Stanislav Nijnikov <stanislav.nijnikov@wdc.com>
2018-02-15 14:14:11 +0200

    UPSTREAM: scsi: ufs: sysfs: attributes
    This patch introduces a sysfs group entry for the UFS attributes. The
    group adds "attributes" folder under the UFS driver sysfs entry
    (/sys/bus/platform/drivers/ufshcd/*). The attributes are shown
    as hexadecimal numbers. The full information about the attributes could
    be found at UFS specifications 2.1.
    
    Signed-off-by: Stanislav Nijnikov <stanislav.nijnikov@wdc.com>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    
    BUG=b:74254727
    TEST=Build kernel for Cheza
    
    Change-Id: I0381034332c6584e18066f107f80ec74d4541a2b
    Signed-off-by: Evan Green <evgreen@chromium.org>
    (cherry picked from commit ec92b59cc03c3d5757449c1c9344524e088df2bd)
    Reviewed-on: https://chromium-review.googlesource.com/1056312
    Commit-Ready: Douglas Anderson <dianders@chromium.org>
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>

--- drivers/scsi/ufs/ufshcd.h
+++ drivers/scsi/ufs/ufshcd.h
@@ -854,6 +854,8 @@ int ufshcd_read_desc_param(struct ufs_hba *hba,
 			   u8 param_offset,
 			   u8 *param_read_buf,
 			   u8 param_size);
+int ufshcd_query_attr(struct ufs_hba *hba, enum query_opcode opcode,
+		      enum attr_idn idn, u8 index, u8 selector, u32 *attr_val);
 int ufshcd_query_flag(struct ufs_hba *hba, enum query_opcode opcode,
 	enum flag_idn idn, bool *flag_res);
 int ufshcd_read_string_desc(struct ufs_hba *hba, int desc_index,
