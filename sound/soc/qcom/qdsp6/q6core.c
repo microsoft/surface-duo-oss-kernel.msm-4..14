36c3ec2cf42e64e07e58089b855bdb43831054e5
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2018-02-26 14:15:55 +0000

    ASoC: qcom: q6core: add versioning support
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>

--- sound/soc/qcom/qdsp6/q6core.c
+++ sound/soc/qcom/qdsp6/q6core.c
@@ -21,10 +21,35 @@
 #define AVCS_CMDRSP_ADSP_EVENT_GET_STATE	0x0001290D
 #define AVCS_GET_VERSIONS       0x00012905
 #define AVCS_GET_VERSIONS_RSP   0x00012906
+#define AVCS_CMD_GET_FWK_VERSION	0x001292c
+#define AVCS_CMDRSP_GET_FWK_VERSION	0x001292d
+
 
 struct avcs_svc_info {
 	uint32_t service_id;
 	uint32_t version;
+} __packed;
+
+struct avcs_cmdrsp_get_version {
+	uint32_t build_id;
+	uint32_t num_services;
+	struct avcs_svc_info svc_api_info[];
+} __packed;
+
+/* for ADSP2.8 and above */
+struct avcs_svc_api_info {
+	uint32_t service_id;
+	uint32_t api_version;
+	uint32_t api_branch_version;
+} __packed;
+
+struct avcs_cmdrsp_get_fwk_version {
+	uint32_t build_major_version;
+	uint32_t build_minor_version;
+	uint32_t build_branch_version;
+	uint32_t build_subbranch_version;
+	uint32_t num_services;
+	struct avcs_svc_api_info svc_api_info[];
 } __packed;
 
 struct q6core {
@@ -33,10 +58,14 @@ struct q6core {
 	uint32_t avcs_state;
 	bool resp_received;
 	uint32_t num_services;
-	struct avcs_svc_info *svcs_info;
+	struct avcs_cmdrsp_get_fwk_version *fwk_version;
+	struct avcs_cmdrsp_get_version *svc_version;
+	bool fwk_version_supported;
+	bool get_state_supported;
+	bool get_version_supported;
 };
 
-struct q6core *core;
+struct q6core *g_core;
 
 static int q6core_callback(struct apr_device *adev,
 			 struct apr_client_message *data)
@@ -46,28 +75,69 @@ static int q6core_callback(struct apr_device *adev,
 
 	result = data->payload;
 	switch (data->opcode) {
-	case AVCS_GET_VERSIONS_RSP:
-		core->num_services = result->status;
+	case APR_BASIC_RSP_RESULT:{
+		result = data->payload;
+		switch (result->opcode) {
+		case AVCS_GET_VERSIONS:
+			if (result->status == ADSP_EUNSUPPORTED)
+				core->get_version_supported = false;
+			core->resp_received = true;
+			break;
+		case AVCS_CMD_GET_FWK_VERSION:
+			if (result->status == ADSP_EUNSUPPORTED)
+				core->fwk_version_supported = false;
+			core->resp_received = true;
+			break;
+		case AVCS_CMD_ADSP_EVENT_GET_STATE:
+			if (result->status == ADSP_EUNSUPPORTED)
+				core->get_state_supported = false;
+			core->resp_received = true;
+			break;
+		}
+		break;
+	}
+	case AVCS_CMDRSP_GET_FWK_VERSION: {
+		struct avcs_cmdrsp_get_fwk_version *fwk;
+		int bytes;
+		fwk = data->payload;
 
-		core->svcs_info = kcalloc(core->num_services,
-					  sizeof(*core->svcs_info),
-					  GFP_ATOMIC);
-		if (!core->svcs_info)
+		core->fwk_version_supported = true;
+		bytes = sizeof(*fwk) + fwk->num_services *
+				sizeof(fwk->svc_api_info[0]);
+
+		core->fwk_version = kzalloc(bytes, GFP_ATOMIC);
+		if (!core->fwk_version)
 			return -ENOMEM;
 
-		/* svc info is after apr result */
-		memcpy(core->svcs_info, result + sizeof(*result),
-		       core->num_services * sizeof(*core->svcs_info));
+		memcpy(core->fwk_version, data->payload, bytes);
 
 		core->resp_received = true;
-		wake_up(&core->wait);
 
 		break;
+	}
+	case AVCS_GET_VERSIONS_RSP: {
+		struct avcs_cmdrsp_get_version *v;
+		int len;
+		v = data->payload;
+		core->get_version_supported = true;
+
+		len = sizeof(*v) + v->num_services * sizeof(v->svc_api_info[0]);
+
+		core->svc_version = kzalloc(len, GFP_ATOMIC);
+		if (!core->svc_version)
+			return -ENOMEM;
+
+		memcpy(core->svc_version, data->payload, len);
+
+		core->resp_received = true;
+
+		break;
+	}
 	case AVCS_CMDRSP_ADSP_EVENT_GET_STATE:
+		core->get_state_supported = true;
 		core->avcs_state = result->opcode;
 
 		core->resp_received = true;
-		wake_up(&core->wait);
 		break;
 	default:
 		dev_err(&adev->dev, "Message id from adsp core svc: 0x%x\n",
@@ -75,7 +145,37 @@ static int q6core_callback(struct apr_device *adev,
 		break;
 	}
 
+	if (core->resp_received)
+		wake_up(&core->wait);
+
 	return 0;
+}
+
+static int q6core_get_fwk_versions(struct q6core *core)
+{
+	struct apr_device *adev = core->adev;
+	struct apr_hdr hdr = {0};
+	int rc;
+
+	core->fwk_version_supported = true;
+	hdr.hdr_field = APR_HDR_FIELD(APR_MSG_TYPE_SEQ_CMD,
+				      APR_HDR_LEN(APR_HDR_SIZE), APR_PKT_VER);
+	hdr.pkt_size = APR_HDR_SIZE;
+	hdr.opcode = AVCS_CMD_GET_FWK_VERSION;
+
+	rc = apr_send_pkt(adev, &hdr);
+	if (rc < 0)
+		return rc;
+
+	rc = wait_event_timeout(core->wait, (core->resp_received),
+				msecs_to_jiffies(Q6_READY_TIMEOUT_MS));
+	if (rc > 0 && core->resp_received) {
+		core->resp_received = false;
+		return 0;
+	}
+
+
+	return rc;
 }
 
 static int q6core_get_svc_versions(struct q6core *core)
@@ -84,6 +184,7 @@ static int q6core_get_svc_versions(struct q6core *core)
 	struct apr_hdr hdr = {0};
 	int rc;
 
+	core->get_version_supported = true;
 	hdr.hdr_field = APR_HDR_FIELD(APR_MSG_TYPE_SEQ_CMD,
 				      APR_HDR_LEN(APR_HDR_SIZE), APR_PKT_VER);
 	hdr.pkt_size = APR_HDR_SIZE;
@@ -109,6 +210,8 @@ static bool __q6core_is_adsp_ready(struct q6core *core)
 	struct apr_hdr hdr = {0};
 	int rc;
 
+	core->get_state_supported = false;
+
 	hdr.hdr_field = APR_HDR_FIELD(APR_MSG_TYPE_SEQ_CMD,
 				      APR_HDR_LEN(APR_HDR_SIZE), APR_PKT_VER);
 	hdr.pkt_size = APR_HDR_SIZE;
@@ -122,9 +225,14 @@ static bool __q6core_is_adsp_ready(struct q6core *core)
 				msecs_to_jiffies(Q6_READY_TIMEOUT_MS));
 	if (rc > 0 && core->resp_received) {
 		core->resp_received = false;
+
 		if (core->avcs_state == 0x1)
 			return true;
 	}
+
+	/* assume that the adsp is up if we not support this command */
+	if (!core->get_state_supported)
+		return true;
 
 	return false;
 }
@@ -140,25 +248,31 @@ static bool __q6core_is_adsp_ready(struct q6core *core)
  */
 uint32_t q6core_get_svc_version(int svc_id)
 {
-	struct apr_device *adev;
-	struct avcs_svc_info *svcs_info;
-	int i, ret;
+	int i;
 
-	if (!core)
+	if (!g_core)
 		return 0;
 
-	if (!core->svcs_info) {
-		ret = q6core_get_svc_versions(core);
-		if (ret)
-			return ret;
+	if (g_core->fwk_version_supported) {
+		for (i = 0; i < g_core->fwk_version->num_services; i++) {
+			struct avcs_svc_api_info *info;
+
+			info = &g_core->fwk_version->svc_api_info[i];
+			if (svc_id == info->service_id)
+				return info->api_version;
+		}
+	} else if (g_core->get_version_supported) {
+		for (i = 0; i < g_core->svc_version->num_services; i++) {
+			struct avcs_svc_info *info;
+
+			info = &g_core->svc_version->svc_api_info[i];
+			pr_err("SVC API Info SVC-ID: %x vs %x API-VER: %x\n",
+					info->service_id, svc_id,  info->version);
+
+			if (svc_id == info->service_id)
+				return info->version;
+		}
 	}
-
-	adev  = core->adev;
-	svcs_info = core->svcs_info;
-
-	for (i = 0; i < core->num_services; i++)
-		if (svcs_info[i].service_id == svc_id)
-			return svcs_info[i].version;
 
 	return 0;
 }
@@ -173,12 +287,12 @@ bool q6core_is_adsp_ready(void)
 {
 	unsigned long  timeout;
 
-	if (!core)
+	if (!g_core)
 		return false;
 
 	timeout = jiffies + msecs_to_jiffies(ADSP_STATE_READY_TIMEOUT_MS);
 	for (;;) {
-		if (__q6core_is_adsp_ready(core))
+		if (__q6core_is_adsp_ready(g_core))
 			return true;
 
 		if (!time_after(timeout, jiffies))
@@ -191,25 +305,36 @@ EXPORT_SYMBOL_GPL(q6core_is_adsp_ready);
 
 static int q6core_probe(struct apr_device *adev)
 {
-	core = kzalloc(sizeof(*core), GFP_KERNEL);
-	if (!core)
+	g_core = kzalloc(sizeof(*g_core), GFP_KERNEL);
+	if (!g_core)
 		return -ENOMEM;
 
-	dev_set_drvdata(&adev->dev, core);
+	dev_set_drvdata(&adev->dev, g_core);
 
-	core->adev = adev;
-	init_waitqueue_head(&core->wait);
+	g_core->adev = adev;
+	init_waitqueue_head(&g_core->wait);
+
+	/* get adsp state */
+	if (q6core_is_adsp_ready()) {
+		q6core_get_fwk_versions(g_core);
+		if (!g_core->fwk_version_supported)
+			q6core_get_svc_versions(g_core);
+	}
 
 	return 0;
 }
 
 static int q6core_exit(struct apr_device *adev)
 {
-	if (core->svcs_info)
-		kfree(core->svcs_info);
+	struct q6core *core = dev_get_drvdata(&adev->dev);
+
+	if (core->fwk_version_supported)
+		kfree(core->fwk_version);
+	if (core->get_version_supported)
+		kfree(core->svc_version);
 
 	kfree(core);
-	core = NULL;
+	g_core = NULL;
 
 	return 0;
 }
