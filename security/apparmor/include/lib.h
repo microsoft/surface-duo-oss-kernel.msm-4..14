e9744a55583e5674c1c31d2b67b53da2f086fd20
John Johansen <john.johansen@canonical.com>
2018-03-13 21:48:00 +0100

    UBUNTU: SAUCE: apparmor: fix bad __initdata tagging on, apparmor_initialized
    BugLink: http://bugs.launchpad.net/bugs/1758471
    
    apparmor_initialized is no longer init only data and the __initdata
    tagging was removed from the variable definition in
    security/apparmor/lsm.c but missed being removed from the declaration
    in security/apparmor/include/lib.h
    
    resulting in the following build warning
    
    WARNING: vmlinux.o(.text+0x393a60): Section mismatch in reference from the function param_set_aauint() to the variable .init.data:apparmor_initialized
    
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- security/apparmor/include/lib.h
+++ security/apparmor/include/lib.h
@@ -56,7 +56,7 @@
 	} while (0)
 
 /* Flag indicating whether initialization completed */
-extern int apparmor_initialized __initdata;
+extern int apparmor_initialized;
 
 /* fn's in lib */
 char *aa_split_fqname(char *args, char **ns_name);
