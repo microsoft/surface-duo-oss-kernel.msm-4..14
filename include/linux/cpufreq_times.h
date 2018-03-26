6305cc66a03c84b2dabc9337c55fbfdf7f9f3216
Connor O'Brien <connoro@google.com>
2018-03-26 15:14:25 -0700

    Revert "ANDROID: cpufreq: times: Add missing includes"
    This reverts commit 18edb0c8f3cbadfc4944022be71c1a723cb6d0d5.
    
    Change-Id: I179c2eca680622390a7d65dcbdf7bb4c12697886
    Signed-off-by: Connor O'Brien <connoro@google.com>

--- include/linux/cpufreq_times.h
+++ include/linux/cpufreq_times.h
@@ -17,8 +17,6 @@
 #define _LINUX_CPUFREQ_TIMES_H
 
 #include <linux/cpufreq.h>
-#include <linux/cputime.h>
-#include <linux/pid.h>
 
 #ifdef CONFIG_CPU_FREQ_TIMES
 void cpufreq_task_times_init(struct task_struct *p);
