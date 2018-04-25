b00d4fbeab63b02270fd94fd7c2875e251c117ab
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2018-04-25 11:07:43 +0100

    ASoC: qdsp6: q6adm: fix copp refcount
    This patch fixes copp refcount which was incremented twice, resulting
    in not getting freed ever.
    
    Reported-by: Amit Pundir <amit.pundir@linaro.org>
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>

--- sound/soc/qcom/qdsp6/q6adm.c
+++ sound/soc/qcom/qdsp6/q6adm.c
@@ -319,6 +319,7 @@ static struct copp *adm_find_matching_copp(struct q6adm *adm,
 		    (mode == c->mode) && (rate == c->rate) &&
 		    (bit_width == c->bit_width) && (app_type == c->app_type)) {
 			spin_unlock_irqrestore(&adm->copps_list_lock, flags);
+			kref_get(&c->refcount);
 			return c;
 		}
 	}
@@ -419,8 +420,6 @@ int q6adm_open(struct device *dev, int port_id, int path, int rate,
 		if (ret < 0)
 			return ret;
 	}
-
-	kref_get(&copp->refcount);
 
 	return copp->copp_idx;
 }
