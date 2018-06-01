325d0717712187715d0c0647e0e250f4d24c6cba
Matthew Nunez <matthew.nunez@nxp.com>
2018-06-01 19:14:28 +0700

    pinctrl: add driver and header files for s32-gen1 pinmuxing
    Add core and pinctrl drivers for s32 platforms G, V, R and S32v234.
    Add header files for pinctrl muxing and device tree bindings. Add
    conditional compilation of pin electrical characteristics in
    pinctrl-s32v-core Add SOC flags to and menu items to Kconfig
    platforms. Add flags to pinctrl Kconfig to allow differentiation
    between platforms for compiling the drivers.
    
    Issue: ALB-1549
    
    Signed-off-by: Matthew Nunez <matthew.nunez@nxp.com>

---
+++ drivers/pinctrl/freescale/pinctrl-s32g275.c
@@ -0,0 +1,80 @@
+/*
+ * s32g275 pinctrl driver based on imx pinmux and pinconf core
+ *
+ * Copyright 2015-2016 Freescale Semiconductor, Inc.
+ * Copyright 2017-2018 NXP
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#include <linux/err.h>
+#include <linux/init.h>
+#include <linux/io.h>
+#include <linux/module.h>
+#include <linux/of.h>
+#include <linux/of_device.h>
+#include <linux/pinctrl/pinctrl.h>
+
+#include "pinctrl-s32.h"
+
+enum s32_pins {
+	S32G275_MSCR_PA_00 =  0,
+};
+
+/* Pad names for the pinmux subsystem */
+static const struct pinctrl_pin_desc s32_pinctrl_pads[] = {
+
+};
+
+
+static struct s32_pinctrl_soc_info s32_pinctrl_info = {
+	.pins = s32_pinctrl_pads,
+	.npins = ARRAY_SIZE(s32_pinctrl_pads),
+};
+
+
+static const struct of_device_id s32_pinctrl_of_match[] = {
+	{
+		.compatible = "fsl,s32g275-siul2",
+		.data = (void *) PINCTRL_V1,
+	},
+	{
+		.compatible = "fsl,s32g275-siul2-pinctrl",
+		.data = (void *) PINCTRL_V2,
+	},
+	{ /* sentinel */ }
+};
+MODULE_DEVICE_TABLE(of, s32_pinctrl_of_match);
+
+static int s32g275_pinctrl_probe(struct platform_device *pdev)
+{
+	enum s32_pinctrl_version vers;
+	const struct of_device_id *of_id =
+		of_match_device(s32_pinctrl_of_match, &pdev->dev);
+
+	if (!of_id)
+		return -ENODEV;
+
+	vers = (enum s32_pinctrl_version) of_id->data;
+
+	return s32_pinctrl_probe(pdev, &s32_pinctrl_info, vers);
+}
+
+static struct platform_driver s32g275_pinctrl_driver = {
+	.driver = {
+		.name = "s32g275-siul2-pinctrl",
+		.owner = THIS_MODULE,
+		.of_match_table = s32_pinctrl_of_match,
+	},
+	.probe = s32g275_pinctrl_probe,
+	.remove = s32_pinctrl_remove,
+};
+
+module_platform_driver(s32g275_pinctrl_driver);
+
+MODULE_AUTHOR("Matthew Nunez <matthew.nunez@nxp.com>");
+MODULE_DESCRIPTION("NXP S32G275 pinctrl driver");
+MODULE_LICENSE("GPL v2");
---
+++ include/dt-bindings/pinctrl/s32-gen1-pinctrl.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2018 NXP
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef __DT_BINDINGS_S32_GEN1_PINCTRL_H__
+#define __DT_BINDINGS_S32_GEN1_PINCTRL_H__
+
+/*
+ * Use to set PAD control
+ */
+
+#define PAD_CTL_OBE		(1 << 21)
+#define PAD_CTL_ODE		(1 << 20)
+#define PAD_CTL_IBE		(1 << 19)
+#define PAD_CTL_INV		(1 << 17)
+
+#define PAD_CTL_SRE_OFS		(14)
+#define PAD_CTL_SRE_208MHZ	(0 << PAD_CTL_SRC_OFS)
+#define PAD_CTL_SRE_150MHZ	(4 << PAD_CTL_SRC_OFS)
+#define PAD_CTL_SRE_100MHZ	(5 << PAD_CTL_SRC_OFS)
+#define PAD_CTL_SRE_50MHZ	(6 << PAD_CTL_SRC_OFS)
+#define PAD_CTL_SRE_25MHZ	(7 << PAD_CTL_SRC_OFS)
+
+#define PAD_CTL_PUE		(1 << 13)
+#define PAD_CTL_PUS		(1 << 12)
+#define PAD_CTL_RCVR		(1 << 10)
+#define PAD_CTL_SMC             (1 << 5)
+
+#define PAD_CTL_SRC_SIG_SEL0   (0)
+#define PAD_CTL_SRC_SIG_SEL1   (1)
+#define PAD_CTL_SRC_SIG_SEL2   (2)
+#define PAD_CTL_SRC_SIG_SEL3   (3)
+#define PAD_CTL_SRC_SIG_SEL4   (4)
+#define PAD_CTL_SRC_SIG_SEL5   (5)
+#define PAD_CTL_SRC_SIG_SEL6   (6)
+#define PAD_CTL_SRC_SIG_SEL7   (7)
+#define PAD_CTL_MUX_MODE_MASK   (0xF)
+
+#endif /* __DT_BINDINGS_S32_GEN1_PINCTRL_H__ */
