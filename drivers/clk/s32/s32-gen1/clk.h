162aaba4ac315722092defe24f4e379f721fb4ef
Larisa Grigore <Larisa.Grigore@nxp.com>
2018-06-07 11:38:13 +0300

    clk: s32gen1: Remove clock gating
    According to the latest specifications, rm444911 - S32G275A Reference
    Manual – Draft F (1.1), the clock gating doesn't look as in S32v234
    case. There is no clock gating of individual peripherals as the power
    saving is tiny in comparison to the logic and testing required to
    implement.
    Only the following modules can be clock gated:
    - M7 Cluster 0, 1, 2
    - A53 cores 0, 1, 2, 3
    - DRAM Controller
    - PFE
    - LLCE
    - uSDHC
    
    Issue: ALB-2408
    Signed-off-by: Larisa Grigore <Larisa.Grigore@nxp.com>

--- drivers/clk/s32/s32-gen1/clk.h
+++ drivers/clk/s32/s32-gen1/clk.h
@@ -28,28 +28,4 @@ struct clk *s32gen1_clk_plldig_phi(enum s32gen1_plldig_type type,
 struct clk *s32gen1_clk_dfs(enum s32gen1_plldig_type type, const char *name,
 			const char *parent_name, void __iomem *reg, u8 idx);
 
-struct clk *s32gen1_clk_register_gate2(struct device *dev, const char *name,
-		const char *parent_name, unsigned long flags,
-		void __iomem *reg, u32 pctln, u8 bit_idx, u8 val,
-		u8 clk_gate_flags, spinlock_t *lock,
-		unsigned int *share_count);
-
-static inline struct clk *s32gen1_clk_gate2(const char *name,
-		const char *parent, void __iomem *reg, u32 pctln,
-		u8 shift, u8 val, spinlock_t *lock)
-{
-	return s32gen1_clk_register_gate2(NULL, name, parent,
-		CLK_SET_RATE_PARENT, reg, pctln, shift, val, 0,
-		lock, NULL);
-}
-
-static inline struct clk *s32gen1_clk_gate2_shared(const char *name,
-		const char *parent, void __iomem *reg, u32 pctln, u8 shift,
-		u8 val, unsigned int *share_count, spinlock_t *lock)
-{
-	return s32gen1_clk_register_gate2(NULL, name, parent,
-			CLK_SET_RATE_PARENT, reg, pctln, shift, val, 0,
-			lock, share_count);
-}
-
 #endif
