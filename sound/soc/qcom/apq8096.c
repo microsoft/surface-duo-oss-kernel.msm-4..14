8b91f4e7b05703e6bf5fcc338037e856789b6e1d
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2018-08-20 17:45:58 +0100

    ASoC: qcom: common: move be_hw_fixup to common
    be_hw_fixup for qdsp is common across mutiple qcom machine drivers,
    so move it to common file and remove the redundant code.
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>

--- sound/soc/qcom/apq8096.c
+++ sound/soc/qcom/apq8096.c
@@ -9,32 +9,6 @@
 #include <sound/pcm.h>
 #include "common.h"
 
-static int apq8096_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
-				      struct snd_pcm_hw_params *params)
-{
-	struct snd_interval *rate = hw_param_interval(params,
-					SNDRV_PCM_HW_PARAM_RATE);
-	struct snd_interval *channels = hw_param_interval(params,
-					SNDRV_PCM_HW_PARAM_CHANNELS);
-
-	rate->min = rate->max = 48000;
-	channels->min = channels->max = 2;
-
-	return 0;
-}
-
-static void apq8096_add_be_ops(struct snd_soc_card *card)
-{
-	struct snd_soc_dai_link *link = card->dai_link;
-	int i, num_links = card->num_links;
-
-	for (i = 0; i < num_links; i++) {
-		if (link->no_pcm == 1)
-			link->be_hw_params_fixup = apq8096_be_hw_params_fixup;
-		link++;
-	}
-}
-
 static int apq8096_platform_probe(struct platform_device *pdev)
 {
 	struct snd_soc_card *card;
@@ -53,7 +27,6 @@ static int apq8096_platform_probe(struct platform_device *pdev)
 		goto err;
 	}
 
-	apq8096_add_be_ops(card);
 	ret = snd_soc_register_card(card);
 	if (ret)
 		goto err_card_register;
--- sound/soc/qcom/sdm845.c
+++ sound/soc/qcom/sdm845.c
@@ -11,7 +11,6 @@
 #include "common.h"
 #include "qdsp6/q6afe.h"
 
-#define DEFAULT_SAMPLE_RATE_48K		48000
 #define DEFAULT_MCLK_RATE		24576000
 #define DEFAULT_BCLK_RATE		12288000
 
@@ -177,32 +176,15 @@ static struct snd_soc_ops sdm845_be_ops = {
 	.shutdown = sdm845_snd_shutdown,
 };
 
-static int sdm845_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
-				struct snd_pcm_hw_params *params)
-{
-	struct snd_interval *rate = hw_param_interval(params,
-					SNDRV_PCM_HW_PARAM_RATE);
-	struct snd_interval *channels = hw_param_interval(params,
-					SNDRV_PCM_HW_PARAM_CHANNELS);
-	struct snd_mask *fmt = hw_param_mask(params, SNDRV_PCM_HW_PARAM_FORMAT);
-
-	rate->min = rate->max = DEFAULT_SAMPLE_RATE_48K;
-	channels->min = channels->max = 2;
-	snd_mask_set_format(fmt, SNDRV_PCM_FORMAT_S16_LE);
-
-	return 0;
-}
-
 static void sdm845_add_be_ops(struct snd_soc_card *card)
 {
 	struct snd_soc_dai_link *link = card->dai_link;
 	int i, num_links = card->num_links;
 
 	for (i = 0; i < num_links; i++) {
-		if (link->no_pcm == 1) {
+		if (link->no_pcm == 1)
 			link->ops = &sdm845_be_ops;
-			link->be_hw_params_fixup = sdm845_be_hw_params_fixup;
-		}
+
 		link++;
 	}
 }
