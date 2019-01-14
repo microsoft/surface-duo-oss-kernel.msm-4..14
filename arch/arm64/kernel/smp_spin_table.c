0eb8cc168e2794e266d159778f311634c957f92e
Vicovan Ionut-Valentin <Ionut.Vicovan@nxp.com>
2019-01-14 09:39:48 +0200

    s32gen1: Add workaround for A53 GPR model event generation
    This is a workaround for the cross-cluster event generation issue
    which came with VDK 7.x.
    The issue manifests by the fact that SEV instruction used by
    the kernel SMP code to wake up all ARM cores only works on
    the cores in the same cluster as the boot core.
    Cores in second cluster are therefore left in WFE state.
    The workaround sets bit WFE_EVT_CA53_CLUSTER1 in A53_GPR
    GPR01 register to also send EVENTO to second cluster.
    
    Issue: ALB-3375
    Signed-off-by: Vicovan Ionut-Valentin <Ionut.Vicovan@nxp.com>

--- arch/arm64/kernel/smp_spin_table.c
+++ arch/arm64/kernel/smp_spin_table.c
@@ -29,6 +29,15 @@
 #include <asm/io.h>
 #include <asm/smp_plat.h>
 
+#ifdef CONFIG_S32GEN1_A53GPR_WORKAROUND
+#include <linux/of_device.h>
+#include <linux/of_address.h>
+
+static phys_addr_t gpr_phys_base_addr = 0;
+static u64 gpr_size = 0;
+
+#endif
+
 extern void secondary_holding_pen(void);
 volatile unsigned long __section(".mmuoff.data.read")
 secondary_holding_pen_release = INVALID_HWID;
@@ -74,6 +83,62 @@ static int smp_spin_table_cpu_init(unsigned int cpu)
 	return ret;
 }
 
+#ifdef CONFIG_S32GEN1_A53GPR_WORKAROUND
+
+#define S32GEN1_GPR01_OFFSET 0x4
+#define S32GEN1_GPR01_WFE_EVT_CA53_CLUSTER1 0x2
+
+static void smp_spin_table_sev_secondary_cluster(unsigned int cpu)
+{
+	volatile void __iomem *gpr_base_addr = NULL;
+	u32 a53_gpr01_val = 0;
+
+	if (!gpr_phys_base_addr) {
+		struct device_node *node = NULL;
+		const __be32 *reg_addr;
+
+		node = of_find_compatible_node(NULL, NULL,
+					"fsl,s32gen1-a53gpr");
+		if (!node)
+			goto error;
+
+		reg_addr = of_get_address(node, 0, &gpr_size, NULL);
+		gpr_phys_base_addr = of_translate_address(node, reg_addr);
+
+		of_node_put(node);
+
+		if (!gpr_phys_base_addr || !gpr_size)
+			goto error;
+
+		pr_info("Found A53 GPR at address 0x%llx\n", gpr_phys_base_addr);
+	}
+
+	gpr_base_addr = ioremap_nocache(gpr_phys_base_addr, gpr_size);
+	if (gpr_base_addr && (cpu > 1)) {
+		gpr_base_addr += S32GEN1_GPR01_OFFSET;
+		u32 a53_gpr01_val_orig = ioread32(gpr_base_addr);
+		a53_gpr01_val = a53_gpr01_val_orig | S32GEN1_GPR01_WFE_EVT_CA53_CLUSTER1;
+		pr_info("CPU%u: Signal secondary cluster\n", cpu);
+		iowrite32(a53_gpr01_val, gpr_base_addr);
+		wmb();
+		iowrite32(a53_gpr01_val_orig, gpr_base_addr);
+		iounmap(gpr_base_addr - S32GEN1_GPR01_OFFSET);
+	}
+
+	return;
+
+error:
+	pr_err("A53 GPR Model Workaround not applied\n");
+}
+
+#define SEV_CPU(cpu) \
+	smp_spin_table_sev_secondary_cluster(cpu); \
+	sev()
+
+#else
+#define SEV_CPU(cpu) sev()
+#endif
+
 static int smp_spin_table_cpu_prepare(unsigned int cpu)
 {
 	__le64 __iomem *release_addr;
@@ -106,7 +171,7 @@ static int smp_spin_table_cpu_prepare(unsigned int cpu)
 	/*
 	 * Send an event to wake up the secondary CPU.
 	 */
-	sev();
+	SEV_CPU(cpu);
 
 	iounmap(release_addr);
 
@@ -123,7 +188,7 @@ static int smp_spin_table_cpu_boot(unsigned int cpu)
 	/*
 	 * Send an event, causing the secondaries to read pen_release.
 	 */
-	sev();
+	SEV_CPU(cpu);
 
 	return 0;
 }
