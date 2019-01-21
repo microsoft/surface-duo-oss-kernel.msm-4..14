10d91620420487450bf8d7d140abaaa1690d21cc
Teodor Marina <teodor.marina@nxp.com>
2019-01-21 14:26:48 +0200

    rtc: Add driver for S32GEN1
    Issue: ALB-3428
    
    Signed-off-by: Teodor Marina <teodor.marina@nxp.com>

---
+++ drivers/rtc/rtc-s32gen1.c
@@ -0,0 +1,177 @@
+/*
+ * Copyright 2019 NXP
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ */
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <asm/current.h>
+#include <linux/sched.h>
+#include <linux/io.h>
+#include <linux/interrupt.h>
+#include <linux/of_irq.h>
+#include <linux/err.h>
+#include <linux/platform_device.h>
+#include <linux/of.h>
+
+#include "rtc-s32gen1.h"
+
+#define DRIVER_NAME			"rtc_s32gen1"
+#define DRIVER_VERSION		"0.1"
+
+/**
+ * struct rtc_s32gen1_priv - RTC driver private data
+ * @rtc_base: rtc base address
+ * @dt_irq_id: rtc interrupt id
+ * @res: rtc resource
+ */
+struct rtc_s32gen1_priv {
+	void __iomem *rtc_base;
+	unsigned int dt_irq_id;
+	struct resource *res;
+};
+
+static void print_rtc(struct platform_device *pdev)
+{
+	struct rtc_s32gen1_priv *priv = platform_get_drvdata(pdev);
+
+	dev_dbg(&pdev->dev, "[notify] RTCSUPV = 0x%08x\n",
+		ioread32((u8 *)priv->rtc_base + RTCSUPV_OFFSET));
+	dev_dbg(&pdev->dev, "[notify] RTCC = 0x%08x\n",
+		ioread32((u8 *)priv->rtc_base + RTCC_OFFSET));
+	dev_dbg(&pdev->dev, "[notify] RTCS = 0x%08x\n",
+		ioread32((u8 *)priv->rtc_base + RTCS_OFFSET));
+	dev_dbg(&pdev->dev, "[notify] RTCCNT = 0x%08x\n",
+		ioread32((u8 *)priv->rtc_base + RTCCNT_OFFSET));
+	dev_dbg(&pdev->dev, "[notify] APIVAL = 0x%08x\n",
+		ioread32((u8 *)priv->rtc_base + APIVAL_OFFSET));
+	dev_dbg(&pdev->dev, "[notify] RTCVAL = 0x%08x\n",
+		ioread32((u8 *)priv->rtc_base + RTCVAL_OFFSET));
+}
+
+static irqreturn_t rtc_handler(int irq, void *dev)
+{
+	struct rtc_s32gen1_priv *priv = platform_get_drvdata(dev);
+
+	/* Clear the IRQ */
+	iowrite32(RTCF, (u8 *)priv->rtc_base + RTCS_OFFSET);
+
+	return IRQ_HANDLED;
+}
+
+static int s32gen1_rtc_probe(struct platform_device *pdev)
+{
+	u32 rtcc_val;
+	struct rtc_s32gen1_priv *priv = NULL;
+	struct device_node *rtc_node;
+	int err = 0;
+
+	dev_dbg(&pdev->dev, "Probing platform device: %s\n", pdev->name);
+
+	/* alloc private data struct */
+	priv = devm_kzalloc(&pdev->dev, sizeof(struct rtc_s32gen1_priv),
+		GFP_KERNEL);
+	if (!priv)
+		return -ENOMEM;
+
+	priv->res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	if (!priv->res) {
+		dev_err(&pdev->dev, "Failed to get resource");
+		err = -ENOMEM;
+		goto err_free_priv_data;
+	}
+
+	priv->rtc_base = (u8 *)devm_ioremap_nocache(&pdev->dev,
+		priv->res->start, (priv->res->end - priv->res->start));
+	if (!priv->rtc_base) {
+		dev_err(&pdev->dev, "Failed to map IO address 0x%016llx\n",
+			priv->res->start);
+		err = -ENOMEM;
+		goto err_release_res;
+	}
+	dev_dbg(&pdev->dev, "RTC successfully mapped to 0x%p\n",
+		priv->rtc_base);
+
+	rtc_node = of_find_compatible_node(NULL, NULL, "fsl,s32gen1-rtc");
+	if (!rtc_node) {
+		dev_err(&pdev->dev, "Unable to find RTC node\n");
+		err = -ENXIO;
+		goto err_release_res;
+	}
+
+	priv->dt_irq_id = of_irq_get(rtc_node, 0);
+	of_node_put(rtc_node);
+
+	if (priv->dt_irq_id <= 0) {
+		err = -ENXIO;
+		goto err_release_res;
+	}
+
+	platform_set_drvdata(pdev, priv);
+
+	err = devm_request_irq(&pdev->dev, priv->dt_irq_id, rtc_handler, 0,
+		"rtc", pdev);
+
+	if (err) {
+		dev_err(&pdev->dev, "Request interrupt %d failed\n",
+			priv->dt_irq_id);
+		err = -ENXIO;
+		goto err_release_res;
+	}
+
+	iowrite32(RTCVAL_VALUE, (u8 *)priv->rtc_base + RTCVAL_OFFSET);
+	rtcc_val = ioread32((u8 *)priv->rtc_base + RTCC_OFFSET);
+	iowrite32(rtcc_val | CNTEN | RTCIE, (u8 *)priv->rtc_base + RTCC_OFFSET);
+
+	print_rtc(pdev);
+
+	return 0;
+
+err_release_res:
+	release_resource(priv->res);
+err_free_priv_data:
+	kfree(priv);
+	return err;
+}
+
+static int s32gen1_rtc_remove(struct platform_device *pdev)
+{
+	u32 rtcc_val;
+	struct rtc_s32gen1_priv *priv = platform_get_drvdata(pdev);
+
+	rtcc_val = ioread32((u8 *)priv->rtc_base + RTCC_OFFSET);
+	iowrite32(rtcc_val & (~CNTEN), (u8 *)priv->rtc_base + RTCC_OFFSET);
+
+	release_resource(priv->res);
+	kfree(priv);
+
+	dev_info(&pdev->dev, "Removed successfully\n");
+	return 0;
+}
+
+static const struct of_device_id s32gen1_rtc_of_match[] = {
+	{.compatible = "fsl,s32gen1-rtc" },
+};
+
+static struct platform_driver s32gen1_rtc_driver = {
+	.probe		= s32gen1_rtc_probe,
+	.remove		= s32gen1_rtc_remove,
+	.driver		= {
+		.name	= "s32gen1-rtc",
+		.of_match_table	= of_match_ptr(s32gen1_rtc_of_match),
+	},
+};
+
+module_platform_driver(s32gen1_rtc_driver);
+
+MODULE_AUTHOR("NXP");
+MODULE_LICENSE("GPL");
+MODULE_ALIAS(DRIVER_NAME);
+MODULE_DESCRIPTION("RTC driver for S32GEN1");
+MODULE_VERSION(DRIVER_VERSION);
---
+++ drivers/rtc/rtc-s32gen1.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2019 NXP
+ *
+ * RTC register definition for the S32GEN1
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ */
+
+#ifndef RTC_S32GEN1_H_
+#define RTC_S32GEN1_H_
+
+#define RTCSUPV_OFFSET	0x0ul
+#define RTCC_OFFSET		0x4ul
+#define RTCS_OFFSET		0x8ul
+#define RTCCNT_OFFSET	0xCul
+#define APIVAL_OFFSET	0x10ul
+#define RTCVAL_OFFSET	0x14ul
+
+#define SUPV			BIT(31)
+#define CNTEN			BIT(31)
+#define RTCIE			BIT(30)
+#define ROVREN			BIT(28)
+#define APIEN			BIT(15)
+#define APIIE			BIT(14)
+#define CLKSEL_MASK		(BIT(12) | BIT(13))
+#define CLKSEL(n)		((n << 12) & CLKSEL_MASK)
+#define DIV512EN		BIT(11)
+#define DIV32EN			BIT(10)
+#define RTCF			BIT(29)
+#define APIF			BIT(13)
+#define ROVRF			BIT(10)
+
+#define RTCVAL_VALUE	0xfff00ul
+
+#endif /* RTC_S32GEN1_H_ */
