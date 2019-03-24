8b5b3f80d656b5633a123b5e95b3fb58da76eb93
Bogdan Hamciuc <bogdan.hamciuc@nxp.com>
2019-03-25 00:40:42 +0200

    rtc: s32gen1: Finer-grained clock initializations
    Select input clock source (FIRC) and divider for the RTC.
    Integrate with rtcwake tool by converting the wall-clock seconds into
    RTC cycles.
    While at it, do some macro refactoring.
    
    Issue: ALB-3760
    
    Signed-off-by: Bogdan Hamciuc <bogdan.hamciuc@nxp.com>
    (cherry picked from commit 945e90fddc7de34227109d570b087116c0b70456)

--- drivers/rtc/rtc-s32gen1.c
+++ drivers/rtc/rtc-s32gen1.c
@@ -11,7 +11,7 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
-#include <asm/current.h>
+#include <linux/delay.h>
 #include <linux/sched.h>
 #include <linux/io.h>
 #include <linux/interrupt.h>
@@ -35,6 +35,10 @@
  * @rtc_s32gen1_kobj: sysfs kernel object
  * @rtc_s32gen1_attr: sysfs command attributes
  * @pdev: platform device structure
+ * @div512: enable DIV512 frequency divider
+ * @div32: enable DIV32 frequency divider
+ * @clk_source: one of S32GEN1_RTC_SOURCE_* input clocks
+ * @rtc_hz: current frequency of the timer
  */
 struct rtc_s32gen1_priv {
 	void __iomem *rtc_base;
@@ -44,32 +48,72 @@ struct rtc_s32gen1_priv {
 	struct kobj_attribute rtc_s32gen1_attr;
 	struct platform_device *pdev;
 	struct rtc_device *rdev;
+	bool div512;
+	bool div32;
+	u8 clk_source;
+	unsigned long rtc_hz;
 };
 
 static void print_rtc(struct platform_device *pdev)
 {
 	struct rtc_s32gen1_priv *priv = platform_get_drvdata(pdev);
 
-	dev_dbg(&pdev->dev, "[notify] RTCSUPV = 0x%08x\n",
+	dev_dbg(&pdev->dev, "RTCSUPV = 0x%08x\n",
 		ioread32((u8 *)priv->rtc_base + RTCSUPV_OFFSET));
-	dev_dbg(&pdev->dev, "[notify] RTCC = 0x%08x\n",
+	dev_dbg(&pdev->dev, "RTCC = 0x%08x\n",
 		ioread32((u8 *)priv->rtc_base + RTCC_OFFSET));
-	dev_dbg(&pdev->dev, "[notify] RTCS = 0x%08x\n",
+	dev_dbg(&pdev->dev, "RTCS = 0x%08x\n",
 		ioread32((u8 *)priv->rtc_base + RTCS_OFFSET));
-	dev_dbg(&pdev->dev, "[notify] RTCCNT = 0x%08x\n",
+	dev_dbg(&pdev->dev, "RTCCNT = 0x%08x\n",
 		ioread32((u8 *)priv->rtc_base + RTCCNT_OFFSET));
-	dev_dbg(&pdev->dev, "[notify] APIVAL = 0x%08x\n",
+	dev_dbg(&pdev->dev, "APIVAL = 0x%08x\n",
 		ioread32((u8 *)priv->rtc_base + APIVAL_OFFSET));
-	dev_dbg(&pdev->dev, "[notify] RTCVAL = 0x%08x\n",
+	dev_dbg(&pdev->dev, "RTCVAL = 0x%08x\n",
 		ioread32((u8 *)priv->rtc_base + RTCVAL_OFFSET));
 }
 
-static irqreturn_t rtc_handler(int irq, void *dev)
+/* Convert a number of seconds to a value suitable for RTCVAL in our clock's
+ * current configuration.
+ * @rtcval: The value to go into RTCVAL[RTCVAL]
+ * Returns: 0 for success, -EINVAL if @seconds push the counter at least
+ *          twice the rollover interval
+ */
+static int s32gen1_sec_to_rtcval(const struct rtc_s32gen1_priv *priv,
+				 unsigned long seconds, unsigned long *rtcval)
+{
+	u32 rtccnt, delta_cnt;
+	unsigned long target_cnt = 0;
+
+	/* For now, support at most one roll-over of the counter */
+	if (!seconds || seconds > ULONG_MAX / priv->rtc_hz)
+		return -EINVAL;
+
+	/* RTCCNT is read-only; we must return a value relative to the
+	 * current value of the counter (and hope we don't linger around
+	 * too much before we get to enable the interrupt)
+	 */
+	delta_cnt = seconds * priv->rtc_hz;
+	rtccnt = ioread32((u8 *)priv->rtc_base + RTCCNT_OFFSET);
+	/* ~rtccnt just stands for (ULONG_MAX - rtccnt) */
+	if (~rtccnt < delta_cnt)
+		target_cnt = (delta_cnt - ~rtccnt);
+	else
+		target_cnt = rtccnt + delta_cnt;
+
+	/* RTCVAL must be at least random() :-) */
+	if (unlikely(target_cnt < 4))
+		target_cnt = 4;
+
+	*rtcval = target_cnt;
+	return 0;
+}
+
+static irqreturn_t s32gen1_rtc_handler(int irq, void *dev)
 {
 	struct rtc_s32gen1_priv *priv = platform_get_drvdata(dev);
 
 	/* Clear the IRQ */
-	iowrite32(RTCF, (u8 *)priv->rtc_base + RTCS_OFFSET);
+	iowrite32(RTCS_RTCF, (u8 *)priv->rtc_base + RTCS_OFFSET);
 
 	return IRQ_HANDLED;
 }
@@ -84,6 +128,7 @@ static int s32gen1_rtc_read_time(struct device *dev, struct rtc_time *tm)
 	 */
 	if (!tm)
 		return -EINVAL;
+	memset(tm, 0, sizeof(*tm));
 	tm->tm_year = 118;	/* 2018 */
 	tm->tm_mon = 7;		/* August */
 	tm->tm_mday = 10;
@@ -92,21 +137,7 @@ static int s32gen1_rtc_read_time(struct device *dev, struct rtc_time *tm)
 	return 0;
 }
 
-static int s32gen1_rtc_set_time(struct device *dev, struct rtc_time *t)
-{
-	struct rtc_s32gen1_priv *priv = dev_get_drvdata(dev);
-
-	return 0;
-}
-
 static int s32gen1_rtc_read_alarm(struct device *dev, struct rtc_wkalrm *t)
-{
-	struct rtc_s32gen1_priv *priv = dev_get_drvdata(dev);
-
-	return 0;
-}
-
-static int s32gen1_rtc_set_alarm(struct device *dev, struct rtc_wkalrm *t)
 {
 	struct rtc_s32gen1_priv *priv = dev_get_drvdata(dev);
 
@@ -122,20 +153,120 @@ static int s32gen1_alarm_irq_enable(struct device *dev, unsigned int enabled)
 		return -EIO;
 
 	rtcc_val = ioread32((u8 *)priv->rtc_base + RTCC_OFFSET);
-	iowrite32(rtcc_val | CNTEN | RTCIE, (u8 *)priv->rtc_base + RTCC_OFFSET);
+	if (enabled)
+		rtcc_val |= RTCC_RTCIE;
+	else
+		rtcc_val &= ~RTCC_RTCIE;
+	iowrite32(rtcc_val, (u8 *)priv->rtc_base + RTCC_OFFSET);
 
-	rtcc_val = ioread32((u8 *)priv->rtc_base + RTCC_OFFSET);
+	return 0;
+}
 
-	return (rtcc_val & RTCIE) ? 0 : -EFAULT;
+static int s32gen1_rtc_set_alarm(struct device *dev, struct rtc_wkalrm *alrm)
+{
+	unsigned long t_crt, t_alrm;
+	struct rtc_time time_crt;
+	unsigned long rtcval;
+	int err = 0, ret;
+	struct rtc_s32gen1_priv *priv = dev_get_drvdata(dev);
+
+	/* Assuming the alarm is being set relative to the same time
+	 * returned by our .rtc_read_time callback
+	 */
+	err = s32gen1_rtc_read_time(dev, &time_crt);
+	if (err)
+		return err;
+	rtc_tm_to_time(&time_crt, &t_crt);
+	rtc_tm_to_time(&alrm->time, &t_alrm);
+	if (t_alrm <= t_crt) {
+		dev_warn(dev, "Alarm is set in the past\n");
+		return -EINVAL;
+	}
+
+	/* RTCVAL can only be written either: a) after reset, or
+	 * b) when CNTEN is set. If the latter, we'd want to know interrupts
+	 * are disabled first.
+	 */
+	if (s32gen1_alarm_irq_enable(dev, 0)) {
+		dev_warn(dev, "Error disabling IRQ\n");
+		return -EIO;
+	}
+	err = s32gen1_sec_to_rtcval(priv, t_alrm - t_crt, &rtcval);
+	if (err) {
+		dev_warn(dev, "Alarm too far in the future\n");
+		goto err_sec_to_rtcval;
+	}
+	iowrite32(rtcval, (u8 *)priv->rtc_base + RTCVAL_OFFSET);
+
+err_sec_to_rtcval:
+	ret = s32gen1_alarm_irq_enable(dev, !!alrm->enabled);
+	return err ? err : ret;
 }
 
 static const struct rtc_class_ops s32gen1_rtc_ops = {
 	.read_time = s32gen1_rtc_read_time,
-	.set_time = s32gen1_rtc_set_time,
 	.read_alarm = s32gen1_rtc_read_alarm,
 	.set_alarm = s32gen1_rtc_set_alarm,
 	.alarm_irq_enable = s32gen1_alarm_irq_enable,
 };
+
+static void s32gen1_rtc_disable(struct rtc_s32gen1_priv *priv)
+{
+	u32 rtcc = ioread32((u8 *)priv->rtc_base + RTCC_OFFSET);
+
+	rtcc &= ~RTCC_CNTEN;
+	iowrite32(rtcc, (u8 *)priv->rtc_base + RTCC_OFFSET);
+}
+
+static void s32gen1_rtc_enable(struct rtc_s32gen1_priv *priv)
+{
+	u32 rtcc = ioread32((u8 *)priv->rtc_base + RTCC_OFFSET);
+
+	rtcc |= RTCC_CNTEN;
+	iowrite32(rtcc, (u8 *)priv->rtc_base + RTCC_OFFSET);
+}
+
+/* RTC specific initializations
+ * Note: This function will leave the clock disabled. This means APIVAL and
+ *       RTCVAL will need to be configured (again) *after* this call.
+ */
+static int s32gen1_priv_setup(struct rtc_s32gen1_priv *priv)
+{
+	u32 rtcc = 0;
+	u32 clksel;
+
+	/* Make sure the clock is disabled before we configure dividers */
+	s32gen1_rtc_disable(priv);
+
+	clksel = RTCC_CLKSEL(priv->clk_source);
+	rtcc |= clksel;
+
+	/* Precompute the base frequency of the clock */
+	switch (clksel) {
+	case RTCC_CLKSEL(S32GEN1_RTC_SOURCE_SIRC):
+		priv->rtc_hz = S32GEN1_SIRC_HZ;
+		break;
+	case RTCC_CLKSEL(S32GEN1_RTC_SOURCE_FIRC):
+		priv->rtc_hz = S32GEN1_FIRC_HZ;
+		break;
+	default:
+		return -EINVAL;
+	}
+	/* Adjust frequency if dividers are enabled */
+	if (priv->div512) {
+		rtcc |= RTCC_DIV512EN;
+		priv->rtc_hz /= 512;
+	}
+	if (priv->div32) {
+		rtcc |= RTCC_DIV32EN;
+		priv->rtc_hz /= 32;
+	}
+
+	rtcc |= RTCC_RTCIE;
+	iowrite32(rtcc, (u8 *)priv->rtc_base + RTCC_OFFSET);
+
+	return 0;
+}
 
 static int s32gen1_rtc_probe(struct platform_device *pdev)
 {
@@ -145,7 +276,7 @@ static int s32gen1_rtc_probe(struct platform_device *pdev)
 
 	dev_dbg(&pdev->dev, "Probing platform device: %s\n", pdev->name);
 
-	/* alloc private data struct */
+	/* alloc and initialize private data struct */
 	priv = devm_kzalloc(&pdev->dev, sizeof(struct rtc_s32gen1_priv),
 		GFP_KERNEL);
 	if (!priv)
@@ -199,10 +330,23 @@ static int s32gen1_rtc_probe(struct platform_device *pdev)
 		goto err_of_irq_get;
 	}
 
-	platform_set_drvdata(pdev, priv);
+	/* Input clock selection: use the 48MHz FIRC with DIV512,
+	 * for a roll-over time of just under 13 hours.
+	 * TODO: clock selection and dividers should be configured in the dts
+	 */
+	priv->div512 = true;
+	priv->clk_source = S32GEN1_RTC_SOURCE_FIRC;
+	if (s32gen1_priv_setup(priv)) {
+		err = -EINVAL;
+		goto err_priv_setup;
+	}
 
-	err = devm_request_irq(&pdev->dev, priv->dt_irq_id, rtc_handler, 0,
-		"rtc", pdev);
+	priv->pdev = pdev;
+	platform_set_drvdata(pdev, priv);
+	s32gen1_rtc_enable(priv);
+
+	err = devm_request_irq(&pdev->dev, priv->dt_irq_id,
+			       s32gen1_rtc_handler, 0, "rtc", pdev);
 	if (err) {
 		dev_err(&pdev->dev, "Request interrupt %d failed\n",
 			priv->dt_irq_id);
@@ -210,12 +354,12 @@ static int s32gen1_rtc_probe(struct platform_device *pdev)
 		goto err_devm_request_irq;
 	}
 
-	priv->pdev = pdev;
 	print_rtc(pdev);
 
 	return 0;
 
 err_devm_request_irq:
+err_priv_setup:
 err_of_irq_get:
 err_of_find_compatible:
 err_device_init_wakeup:
@@ -232,7 +376,7 @@ static int s32gen1_rtc_remove(struct platform_device *pdev)
 	struct rtc_s32gen1_priv *priv = platform_get_drvdata(pdev);
 
 	rtcc_val = ioread32((u8 *)priv->rtc_base + RTCC_OFFSET);
-	iowrite32(rtcc_val & (~CNTEN), (u8 *)priv->rtc_base + RTCC_OFFSET);
+	iowrite32(rtcc_val & (~RTCC_CNTEN), (u8 *)priv->rtc_base + RTCC_OFFSET);
 
 	release_resource(priv->res);
 
@@ -247,10 +391,9 @@ static const struct of_device_id s32gen1_rtc_of_match[] = {
 static struct platform_driver s32gen1_rtc_driver = {
 	.probe		= s32gen1_rtc_probe,
 	.remove		= s32gen1_rtc_remove,
-	.driver		= {
-		.name	= "s32gen1-rtc",
-		.of_match_table	= of_match_ptr(s32gen1_rtc_of_match),
-	},
+	.driver		= {.name = "s32gen1-rtc",
+			   .of_match_table = of_match_ptr(s32gen1_rtc_of_match),
+			  },
 };
 module_platform_driver(s32gen1_rtc_driver);
 
