945e90fddc7de34227109d570b087116c0b70456
Bogdan Hamciuc <bogdan.hamciuc@nxp.com>
2019-03-25 00:40:42 +0200

    rtc: s32gen1: Finer-grained clock initializations
    Select input clock source (FIRC) and divider for the RTC.
    Integrate with rtcwake tool by converting the wall-clock seconds into
    RTC cycles.
    While at it, do some macro refactoring.
    
    Issue: ALB-3506
    
    Signed-off-by: Bogdan Hamciuc <bogdan.hamciuc@nxp.com>

--- drivers/rtc/rtc-s32gen1.h
+++ drivers/rtc/rtc-s32gen1.h
@@ -14,24 +14,36 @@
 #define RTC_S32GEN1_H_
 
 #define RTCSUPV_OFFSET	0x0ul
-#define RTCC_OFFSET		0x4ul
-#define RTCS_OFFSET		0x8ul
+#define RTCC_OFFSET	0x4ul
+#define RTCS_OFFSET	0x8ul
 #define RTCCNT_OFFSET	0xCul
 #define APIVAL_OFFSET	0x10ul
 #define RTCVAL_OFFSET	0x14ul
 
-#define SUPV			BIT(31)
-#define CNTEN			BIT(31)
-#define RTCIE			BIT(30)
-#define ROVREN			BIT(28)
-#define APIEN			BIT(15)
-#define APIIE			BIT(14)
-#define CLKSEL_MASK		(BIT(12) | BIT(13))
-#define CLKSEL(n)		((n << 12) & CLKSEL_MASK)
-#define DIV512EN		BIT(11)
-#define DIV32EN			BIT(10)
-#define RTCF			BIT(29)
-#define APIF			BIT(13)
-#define ROVRF			BIT(10)
+/* RTCSUPV fields */
+#define RTCSUPV_SUPV		BIT(31)
+/* RTCC fields */
+#define RTCC_CNTEN		BIT(31)
+#define RTCC_RTCIE_SHIFT	30
+#define RTCC_RTCIE		BIT(RTCC_RTCIE_SHIFT)
+#define RTCC_ROVREN		BIT(28)
+#define RTCC_APIEN		BIT(15)
+#define RTCC_APIIE		BIT(14)
+#define RTCC_CLKSEL_MASK	(BIT(12) | BIT(13))
+#define RTCC_CLKSEL(n)		((n << 12) & RTCC_CLKSEL_MASK)
+#define RTCC_DIV512EN		BIT(11)
+#define RTCC_DIV32EN		BIT(10)
+/* RTCS fields */
+#define RTCS_RTCF		BIT(29)
+#define RTCS_APIF		BIT(13)
+#define RTCS_ROVRF		BIT(10)
+
+/* Clock sources - usable with RTCC_CLKSEL */
+#define S32GEN1_RTC_SOURCE_FIRC	0x2
+#define S32GEN1_RTC_SOURCE_SIRC	0x0
+
+/* FIXME take clock frequencies from the device-tree! */
+#define S32GEN1_FIRC_HZ		(48 * 1000 * 1000)
+#define S32GEN1_SIRC_HZ		(32 * 1000)
 
 #endif /* RTC_S32GEN1_H_ */
