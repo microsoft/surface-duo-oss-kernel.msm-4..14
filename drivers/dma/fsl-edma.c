9e46b4154f59b2d528c39a57599d84e8aa4244e1
Teodor Marina <teodor.marina@nxp.com>
2019-04-22 18:13:31 +0300

    edma: Fix the edma channel initialisation in resume early function
    Issue: ALB-3760
    Signed-off-by: Teodor Marina <teodor.marina@nxp.com>
    (cherry picked from commit ef9119770720177467fab075d623868554451c66)

--- drivers/dma/fsl-edma.c
+++ drivers/dma/fsl-edma.c
@@ -1354,18 +1354,22 @@ static int fsl_edma_resume_early(struct device *dev)
 {
 	struct fsl_edma_engine *fsl_edma = dev_get_drvdata(dev);
 	struct fsl_edma_chan *fsl_chan;
+	struct fsl_edma_hw_tcd *hw_tcd;
 	int i;
 
 	for (i = 0; i < fsl_edma->n_chans; i++) {
 		fsl_chan = &fsl_edma->chans[i];
 		fsl_chan->pm_state = RUNNING;
-		edma_writew(fsl_edma, 0x0, fsl_edma->membase + EDMA_TCD_CSR(i));
+
+		hw_tcd = (struct fsl_edma_hw_tcd *)
+			fsl_edma->socdata->ops->edma_get_tcd_addr(fsl_chan);
+
+		edma_writew(fsl_edma, 0x0, &hw_tcd->csr);
 		if (fsl_chan->slave_id != 0)
 			fsl_edma_chan_mux(fsl_chan, fsl_chan->slave_id, true);
 	}
 
-	edma_writel(fsl_edma, EDMA_CR_ERGA | EDMA_CR_ERCA,
-			fsl_edma->membase + EDMA_CR);
+	fsl_edma_enable_arbitration(fsl_edma);
 
 	return 0;
 }
