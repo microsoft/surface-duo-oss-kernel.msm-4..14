a3a295cc4b82b49ec712dc6ff4c2f579d9004d4f
Bogdan Folea <bogdan.folea@nxp.com>
2019-05-13 17:03:36 +0300

    hse: Hash algorithms streaming mode support
    Issue: ALB-3672
    Signed-off-by: Bogdan Folea <bogdan.folea@nxp.com>

--- drivers/crypto/hse/hse-hash.c
+++ drivers/crypto/hse/hse-hash.c
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: BSD-3-Clause
 /*
- * NXP HSE Driver - HASH Component
+ * NXP HSE Driver - Hash Support
+ *
+ * This file contains the kernel crypto API implementation for the hash
+ * algorithms and message authentication codes supported by HSE.
  *
  * Copyright 2019 NXP
  */
@@ -22,14 +25,18 @@
 /**
  * struct hse_hash_state - crypto request context
  * @srv_desc: HSE service descriptor
- * @buf: linearized input buffer
+ * @crt_buf: linearized input buffer
+ * @large_buf: buffer larger than block size
  * @len: digest size
- * @channel: MU channel
+ * @crt_idx: current byte index in buffer
+ * @channel: MU channel/stream
  */
 struct hse_hash_state {
 	struct hse_srv_desc *srv_desc;
-	void *buf;
-	u64 *len;
+	void *crt_buf;
+	void *large_buf;
+	u32 *len;
+	u32 crt_idx;
 	u8 channel;
 };
 
@@ -41,6 +48,14 @@ struct hse_hash_ctx {
 	u8 partial_hash[HSE_MAX_DIGEST_SIZE];
 };
 
+/**
+ * hse_hash_template - hash template
+ * @name: algorithm name
+ * @drvname: driver name
+ * @blocksize: block size
+ * @ahash_alg: hash algorithm template
+ * @alg_type: HSE algorithm type (hash/mac)
+ */
 struct hse_hash_template {
 	char name[CRYPTO_MAX_ALG_NAME];
 	char drvname[CRYPTO_MAX_ALG_NAME];
@@ -49,6 +64,15 @@ struct hse_hash_template {
 	u8 alg_type;
 };
 
+/**
+ * hse_halg - hash algorithm data
+ * @entry: list of suported algorithms
+ * @alg_type: HSE algorithm type (hash/mac)
+ * @srv_id: HSE service ID
+ * @ahash_alg: hash algorithm
+ * @dev: HSE device
+ * @mu_inst: MU instance
+ */
 struct hse_halg {
 	struct list_head entry;
 	u8 alg_type;
@@ -58,6 +82,12 @@ struct hse_halg {
 	void *mu_inst;
 };
 
+/**
+ * hse_get_halg - get hash algorithm data from crypto request
+ * @req: asynchronous hash request
+ *
+ * Return: pointer to hash algorithm data
+ */
 static struct hse_halg *hse_get_halg(struct ahash_request *req)
 {
 	struct crypto_ahash *ahash = crypto_ahash_reqtfm(req);
@@ -71,42 +101,187 @@ static struct hse_halg *hse_get_halg(struct ahash_request *req)
 	return t_alg;
 }
 
+/**
+ * ahash_done - asynchronous hash rx callback
+ * @mu_inst: MU instance
+ * @channel: service channel index
+ * @req: asynchronous hash request
+ *
+ * Common rx callback for hash-related sevice requests.
+ */
 static void ahash_done(void *mu_inst, u8 channel, void *req)
 {
-	struct hse_halg *t_alg = hse_get_halg(req);
 	struct hse_hash_state *state = ahash_request_ctx(req);
+	struct crypto_ahash *ahash = crypto_ahash_reqtfm(req);
+	unsigned int blocksize = crypto_ahash_blocksize(ahash);
+	unsigned int access_mode = state->srv_desc->hash_req.access_mode;
+	struct hse_halg *t_alg = hse_get_halg(req);
 	int err;
 	u32 reply;
 
-	err = hse_mu_recv_response(t_alg->mu_inst, channel, &reply);
-	if (!err)
-		err = hse_err_decode(reply);
+	err = hse_mu_recv_response(mu_inst, channel, &reply);
+	err = err ? err : hse_err_decode(reply);
+	if (err)
+		dev_dbg(t_alg->dev, "service response 0x%08x on channel %d\n",
+			reply, channel);
+
+	switch (access_mode) {
+	case HSE_ACCESS_MODE_UPDATE:
+		if (state->srv_desc->hash_req.input_len > blocksize)
+			kfree(state->large_buf);
+		break;
+	case HSE_ACCESS_MODE_FINISH:
+		err = hse_mu_release_channel(mu_inst, channel);
+	case HSE_ACCESS_MODE_ONE_PASS:
+		kfree(state->len);
+		kfree(state->srv_desc);
+		break;
+	default:
+		break;
+	}
 
 	ahash_request_complete(req, err);
-
-	kfree(state->len);
-	kfree(state->srv_desc);
 }
 
+/**
+ * ahash_init - asynchronous hash request init
+ * @req: asynchronous hash request
+ */
 static int ahash_init(struct ahash_request *req)
 {
-	return -EOPNOTSUPP;
+	struct hse_hash_state *state = ahash_request_ctx(req);
+	struct hse_halg *t_alg = hse_get_halg(req);
+	struct crypto_ahash *ahash = crypto_ahash_reqtfm(req);
+	unsigned int blocksize = crypto_ahash_blocksize(ahash);
+	int err;
+	u8 channel;
+
+	state->len = kzalloc(sizeof(*state->len) + blocksize, GFP_KERNEL);
+	if (!state->len)
+		return -ENOMEM;
+	*state->len = crypto_ahash_digestsize(ahash);
+	state->crt_buf = state->len + 1;
+	state->crt_idx = 0;
+
+	state->srv_desc = kzalloc(sizeof(*state->srv_desc), GFP_KERNEL);
+	if (!state->srv_desc) {
+		err = -ENOMEM;
+		goto err_free_buf;
+	}
+
+	err = hse_mu_reserve_channel(t_alg->mu_inst, &channel);
+	if (err)
+		goto err_free_desc;
+
+	state->srv_desc->hash_req.stream_id = channel;
+	state->channel = channel;
+
+	state->srv_desc->srv_id = t_alg->srv_id;
+	state->srv_desc->srv_meta_data.prio = HSE_SRV_PRIO_MED;
+	state->srv_desc->hash_req.hash_algo = t_alg->alg_type;
+	state->srv_desc->hash_req.access_mode = HSE_ACCESS_MODE_START;
+	state->srv_desc->hash_req.input_len = 0;
+
+	err = hse_mu_send_request(t_alg->mu_inst, channel,
+				  hse_addr(state->srv_desc), req, ahash_done);
+	if (err)
+		goto err_free_desc;
+
+	return -EINPROGRESS;
+
+err_free_desc:
+	kfree(state->srv_desc);
+err_free_buf:
+	kfree(state->len);
+	return err;
 }
 
+/**
+ * ahash_update - asynchronous hash request update
+ * @req: asynchronous hash request
+ */
 static int ahash_update(struct ahash_request *req)
 {
-	return -EOPNOTSUPP;
+	struct hse_hash_state *state = ahash_request_ctx(req);
+	struct hse_halg *t_alg = hse_get_halg(req);
+	struct crypto_ahash *ahash = crypto_ahash_reqtfm(req);
+	unsigned int blocksize = crypto_ahash_blocksize(ahash);
+	int err;
+	unsigned int num_blocks, bytes_left = state->crt_idx + req->nbytes;
+
+	if (req->nbytes == 0) {
+		state->srv_desc->hash_req.input_len = state->crt_idx;
+		return 0;
+	}
+
+	if (bytes_left <= blocksize) {
+		scatterwalk_map_and_copy(state->crt_buf + state->crt_idx,
+					 req->src, 0, req->nbytes, 0);
+		state->crt_idx += req->nbytes;
+		return 0;
+	}
+
+	num_blocks = bytes_left / blocksize;
+
+	state->large_buf = kzalloc(num_blocks * blocksize, GFP_KERNEL);
+	if (IS_ERR_OR_NULL(state->large_buf))
+		return -ENOMEM;
+
+	/* copy full blocks */
+	memcpy(state->large_buf, state->crt_buf, state->crt_idx);
+	scatterwalk_map_and_copy(state->large_buf + state->crt_idx,
+				 req->src, 0, num_blocks * blocksize, 0);
+	bytes_left -= num_blocks * blocksize;
+
+	/* copy residue */
+	scatterwalk_map_and_copy(state->crt_buf + state->crt_idx, req->src,
+				 num_blocks * blocksize, bytes_left, 0);
+	state->crt_idx = bytes_left;
+
+	state->srv_desc->hash_req.access_mode = HSE_ACCESS_MODE_UPDATE;
+	state->srv_desc->hash_req.input_len = num_blocks * blocksize;
+	state->srv_desc->hash_req.p_input = hse_addr(state->large_buf);
+
+	err = hse_mu_send_request(t_alg->mu_inst, state->channel,
+				  hse_addr(state->srv_desc), req, ahash_done);
+	if (err)
+		goto err_free_buf;
+
+	return -EINPROGRESS;
+err_free_buf:
+	kfree(state->large_buf);
+	return err;
 }
 
+/**
+ * ahash_final - asynchronous hash request final
+ * @req: asynchronous hash request
+ */
 static int ahash_final(struct ahash_request *req)
 {
-	return -EOPNOTSUPP;
+	struct hse_hash_state *state = ahash_request_ctx(req);
+	struct hse_halg *t_alg = hse_get_halg(req);
+	int err;
+
+	state->srv_desc->hash_req.access_mode = HSE_ACCESS_MODE_FINISH;
+	state->srv_desc->hash_req.input_len = state->crt_idx;
+	state->srv_desc->hash_req.p_input = hse_addr(state->crt_buf);
+	state->srv_desc->hash_req.p_hash_len = hse_addr(state->len);
+	state->srv_desc->hash_req.p_hash = hse_addr(req->result);
+
+	err = hse_mu_send_request(t_alg->mu_inst, state->channel,
+				  hse_addr(state->srv_desc), req, ahash_done);
+
+	return !err ? -EINPROGRESS : err;
 }
 
+/**
+ * ahash_digest - asynchronous hash request digest
+ * @req: asynchronous hash request
+ */
 static int ahash_digest(struct ahash_request *req)
 {
 	struct crypto_ahash *ahash = crypto_ahash_reqtfm(req);
-	int digestsize = crypto_ahash_digestsize(ahash);
 	struct hse_hash_state *state = ahash_request_ctx(req);
 	struct hse_halg *t_alg = hse_get_halg(req);
 	int err;
@@ -114,8 +289,8 @@ static int ahash_digest(struct ahash_request *req)
 	state->len = kzalloc(sizeof(*state->len) + req->nbytes, GFP_KERNEL);
 	if (!state->len)
 		return -ENOMEM;
-	*state->len = digestsize;
-	state->buf = state->len + 1;
+	*state->len = crypto_ahash_digestsize(ahash);
+	state->crt_buf = state->len + 1;
 
 	state->srv_desc = kzalloc(sizeof(*state->srv_desc), GFP_KERNEL);
 	if (!state->srv_desc) {
@@ -124,16 +299,16 @@ static int ahash_digest(struct ahash_request *req)
 	}
 
 	state->srv_desc->srv_id = t_alg->srv_id;
-	state->srv_desc->srv_meta_data.prio = HSE_SRV_PRIO_MED;
+	state->srv_desc->srv_meta_data.prio = HSE_SRV_PRIO_LOW;
 	state->srv_desc->hash_req.hash_algo = t_alg->alg_type;
 
 	state->srv_desc->hash_req.access_mode = HSE_ACCESS_MODE_ONE_PASS;
 	state->srv_desc->hash_req.input_len = req->nbytes;
-	state->srv_desc->hash_req.p_input = hse_addr(state->buf);
+	state->srv_desc->hash_req.p_input = hse_addr(state->crt_buf);
 	state->srv_desc->hash_req.p_hash_len = hse_addr(state->len);
 	state->srv_desc->hash_req.p_hash = hse_addr(req->result);
 
-	scatterwalk_map_and_copy(state->buf, req->src, 0, req->nbytes, 0);
+	scatterwalk_map_and_copy(state->crt_buf, req->src, 0, req->nbytes, 0);
 
 	err = hse_mu_send_request(t_alg->mu_inst, HSE_ANY_CHANNEL,
 				  hse_addr(state->srv_desc), req, ahash_done);
@@ -148,6 +323,10 @@ err_free_buf:
 	return err;
 }
 
+/**
+ * hse_hash_cra_init - cryto algorithm init
+ * @tfm: crypto transformation
+ */
 static int hse_hash_cra_init(struct crypto_tfm *tfm)
 {
 	struct crypto_ahash *ahash = __crypto_ahash_cast(tfm);
@@ -157,6 +336,10 @@ static int hse_hash_cra_init(struct crypto_tfm *tfm)
 	return 0;
 }
 
+/**
+ * hse_hash_cra_exit - cryto algorithm exit
+ * @tfm: crypto transformation
+ */
 static void hse_hash_cra_exit(struct crypto_tfm *tfm)
 {
 }
@@ -195,6 +378,12 @@ static const struct hse_hash_template driver_hash[] = {
 	},
 };
 
+/**
+ * hse_hash_alloc - allocate hash algorithm
+ * @dev: HSE device
+ * @keyed: unkeyed/hash or keyed/mac
+ * @tpl: hash algorithm template
+ */
 static struct hse_halg *hse_hash_alloc(struct device *dev, bool keyed,
 				       const struct hse_hash_template *tpl)
 {
@@ -233,8 +422,8 @@ static struct hse_halg *hse_hash_alloc(struct device *dev, bool keyed,
 }
 
 /**
- * hse_hash_init - HASH component initial setup
- * @dev: parent device
+ * hse_hash_init - hash component initial setup
+ * @dev: HSE device
  *
  * Return: 0 on success, error code otherwise
  */
@@ -274,8 +463,8 @@ int hse_hash_init(struct device *dev)
 }
 
 /**
- * hse_hash_free - HASH component final cleanup
- * @dev: parent device
+ * hse_hash_free - hash component final cleanup
+ * @dev: HSE device
  */
 void hse_hash_free(struct device *dev)
 {
