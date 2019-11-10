54b648b43f3dddec71e5cdd22c2f9e6fe1448f6a
Jan Petrous <jan.petrous@nxp.com>
2019-11-10 16:33:12 +0100

    net: driver: stmmac: dwmac-s32cc: add phy interface mode
    To set correct phy interface mode, the S32CC
    GMAC_0_CTRL_STS register is used
    
    Issue: ALB-4159 AAVB-2124
    Signed-off-by: Jan Petrous <jan.petrous@nxp.com>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-s32cc.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-s32cc.c
@@ -9,21 +9,104 @@
 #include <linux/device.h>
 #include <linux/ethtool.h>
 #include <linux/module.h>
+#include <linux/io.h>
+#include <linux/phy.h>
 #include <linux/platform_device.h>
 #include <linux/of.h>
+#include <linux/of_net.h>
 #include <linux/stmmac.h>
 
 #include "stmmac_platform.h"
+
+/* S32 SRC register for phyif selection */
+#define PHY_INTF_SEL_SGMII      0x01
+#define PHY_INTF_SEL_RGMII      0x02
+#define PHY_INTF_SEL_RMII       0x08
+#define PHY_INTF_SEL_MII        0x00
+#define S32CC_GMAC_0_CTRL_STS		0x4007C004
+#define EQOS_DMA_MODE_SWR		0x01
+#define GMAC_DMA_MODE_CONFIG		0x1000
+
+struct s32cc_priv_data {
+	void __iomem *syscon;
+	int intf_mode;
+};
+
+static int s32cc_gmac_init(struct platform_device *pdev, void *priv)
+{
+	struct s32cc_priv_data *gmac = priv;
+	u32 intf_sel;
+
+	gmac->syscon = ioremap_nocache(S32CC_GMAC_0_CTRL_STS, 4);
+	if (!gmac->syscon) {
+		dev_err(&pdev->dev, "cannot map GMAC_0_CTRL_STS\n");
+		return -EIO;
+	}
+
+	switch (gmac->intf_mode) {
+	case PHY_INTERFACE_MODE_SGMII:
+		dev_info(&pdev->dev, "phy mode set to SGMII\n");
+		intf_sel = PHY_INTF_SEL_SGMII;
+		break;
+	case PHY_INTERFACE_MODE_RGMII:
+		dev_info(&pdev->dev, "phy mode set to RGMII\n");
+		intf_sel = PHY_INTF_SEL_RGMII;
+		break;
+	case PHY_INTERFACE_MODE_RMII:
+		dev_info(&pdev->dev, "phy mode set to RMII\n");
+		intf_sel = PHY_INTF_SEL_RMII;
+		break;
+	case PHY_INTERFACE_MODE_MII:
+		dev_info(&pdev->dev, "phy mode set to MII\n");
+		intf_sel = PHY_INTF_SEL_MII;
+		break;
+	default:
+		dev_info(&pdev->dev, "unsupported mode %d, set phy mode to RGMII\n",
+			 gmac->intf_mode);
+		intf_sel = PHY_INTF_SEL_RGMII;
+		break;
+	}
+
+	/* set interface mode */
+	writel(intf_sel, gmac->syscon);
+
+	return 0;
+}
+
+static void s32cc_gmac_exit(struct platform_device *pdev, void *priv)
+{
+	struct s32cc_priv_data *gmac = priv;
+
+	iounmap(gmac->syscon);
+}
 
 static int s32cc_dwmac_probe(struct platform_device *pdev)
 {
 	struct plat_stmmacenet_data *plat_dat;
 	struct stmmac_resources stmmac_res;
+	struct s32cc_priv_data *gmac;
 	int ret;
 
 	ret = stmmac_get_platform_resources(pdev, &stmmac_res);
 	if (ret)
 		return ret;
+
+	gmac = devm_kzalloc(&pdev->dev, sizeof(*gmac), GFP_KERNEL);
+	if (!gmac) {
+		ret = PTR_ERR(gmac);
+		goto err_exit;
+	}
+
+	/* phy mode */
+	gmac->intf_mode = of_get_phy_mode(pdev->dev.of_node);
+	if (gmac->intf_mode != PHY_INTERFACE_MODE_SGMII &&
+	    gmac->intf_mode != PHY_INTERFACE_MODE_RGMII &&
+	    gmac->intf_mode != PHY_INTERFACE_MODE_RMII &&
+	    gmac->intf_mode != PHY_INTERFACE_MODE_MII) {
+		dev_err(&pdev->dev, "Not supported phy interface mode: [%s]\n",
+			phy_modes(gmac->intf_mode));
+		return -EINVAL;
+	}
 
 	plat_dat = stmmac_probe_config_dt(pdev, &stmmac_res.mac);
 	if (IS_ERR(plat_dat))
@@ -39,18 +122,28 @@ static int s32cc_dwmac_probe(struct platform_device *pdev)
 	plat_dat->pmt = 1;
 	plat_dat->tso_en = of_property_read_bool(pdev->dev.of_node, "snps,tso");
 
+	plat_dat->init = s32cc_gmac_init;
+	plat_dat->exit = s32cc_gmac_exit;
+	plat_dat->bsp_priv = gmac;
+
 	/* configure bitfield for quirks */
 	plat_dat->quirk_mask_id = QUIRK_MASK_ERRATA_E50082;
 
-	ret = stmmac_dvr_probe(&pdev->dev, plat_dat, &stmmac_res);
+	ret = s32cc_gmac_init(pdev, plat_dat->bsp_priv);
 	if (ret)
 		goto err_remove_config_dt;
 
+	ret = stmmac_dvr_probe(&pdev->dev, plat_dat, &stmmac_res);
+	if (ret)
+		goto err_gmac_exit;
+
 	return 0;
 
+err_gmac_exit:
+	s32cc_gmac_exit(pdev, plat_dat->bsp_priv);
 err_remove_config_dt:
 	stmmac_remove_config_dt(pdev, plat_dat);
-
+err_exit:
 	return ret;
 }
 
