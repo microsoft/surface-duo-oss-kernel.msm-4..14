a83caca883d00c18203992c0dfdd54412607f0ae
Ionut Vicovan <Ionut.Vicovan@nxp.com>
2020-01-19 22:55:32 +0200

    s32gen1: pcie: Fix EP setup (iATU, BARs)
    Linux kernel has a bug when setting iATU windows in EP mode.
    On RC mode, it handles both legacy (as for S32V2) and unrolled
    (as for S32gen1) iATU register layouts, while in EP mode only
    legacy mode is supported. Fix this here.
    
    Issue: ALB-3444
    Signed-off-by: Ionut Vicovan <Ionut.Vicovan@nxp.com>

--- drivers/pci/controller/dwc/pci-s32gen1.c
+++ drivers/pci/controller/dwc/pci-s32gen1.c
@@ -455,6 +455,17 @@ static const struct file_operations s32v_pcie_ep_dbgfs_fops = {
 #endif
 };
 
+static u8 dw_pcie_iatu_unroll_enabled(struct dw_pcie *pci)
+{
+	u32 val;
+
+	val = dw_pcie_readl_dbi(pci, PCIE_ATU_VIEWPORT);
+	if (val == 0xffffffff)
+		return 1;
+
+	return 0;
+}
+
 static void s32gen1_pcie_ep_init(struct dw_pcie_ep *ep)
 {
 	struct dw_pcie *pcie = to_dw_pcie_from_ep(ep);
@@ -478,11 +489,20 @@ static void s32gen1_pcie_ep_init(struct dw_pcie_ep *ep)
 		return;
 	}
 
-	dw_pcie_dbi_ro_wr_en(pcie);
+	/* The DW code sets iATU unroll enabled for RC, but not the EP.
+	 * We set it here, since we can't setup BARs otherwise.
+	 * TODO: check if this is fixed for later kernels (5.x.y)
+	 */
+	pcie->iatu_unroll_enabled = dw_pcie_iatu_unroll_enabled(pcie);
+	dev_dbg(pcie->dev, "iATU unroll: %s\n",
+		pcie->iatu_unroll_enabled ? "enabled" : "disabled");
+
+	dw_pcie_setup(pcie);
 	
 	/*
 	 * Configure the class_rev(emulate one memory EP device)
 	 */
+	dw_pcie_dbi_ro_wr_en(pcie);
 	BSET32(pcie, dbi, PCI_CLASS_REVISION,
 		((PCI_BASE_CLASS_PROCESSOR << 24) |
 			     (0x80 /* other */ << 16)));
@@ -490,15 +510,16 @@ static void s32gen1_pcie_ep_init(struct dw_pcie_ep *ep)
 #ifdef CONFIG_PCI_S32GEN1_INIT_EP_BARS
 	
 	/* Setup BARs and inbound regions */
-	for (bar = BAR_0; (bar < PCIE_NUM_BARS) && !ret; bar++) {
-		if (s32gen1_ep_bars_en[bar])
+	for (bar = BAR_0; (bar < PCIE_NUM_BARS); bar++) {
+		if (s32gen1_ep_bars_en[bar]) {
 			ret = epc->ops->set_bar(epc, 0, 
 					&s32gen1_ep_bars[bar]);
+			if (ret) {
+				pr_err("%s: Unable to init BAR%d\n", __func__, bar);
+			}
+		}
 	}
-	if (ret) {
-		pr_err("%s: Unable to init BAR%d\n", __func__, bar);
-	}
-	
+
 #else
 	for (bar = BAR_0; bar <= BAR_5; bar++)
 		dw_pcie_ep_reset_bar(pcie, bar);
