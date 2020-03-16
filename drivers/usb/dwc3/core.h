d569923c6628836ff8a7d05defd7b1df9fbeeaa2
Michal Simek <michal.simek@xilinx.com>
2020-03-16 13:34:39 +0100

    usb: dwc3: Add support for clock disabling during suspend
    In dwc3 usb controller, when remote wakeup feature is enabled and
    the controller is in U3 state (suspended), it requires suspend_clk
    to be active to detect wakeup signalling driven on the link.
    For all other cases usb clocks can be disabled once the controller
    enters U3 state.
    
    This patch adds the logic for disabling usb clocks when remote
    wakeup feature is not enabled.
    
    Signed-off-by: Piyush Mehta <piyush.mehta@xilinx.com>
    Signed-off-by: Anurag Kumar Vulisha <anurag.kumar.vulisha@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>
    State: pending
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>

--- drivers/usb/dwc3/core.h
+++ drivers/usb/dwc3/core.h
@@ -1045,6 +1045,7 @@ struct dwc3_scratchpad_array {
  * @dis_metastability_quirk: set to disable metastability quirk.
  * @imod_interval: set the interrupt moderation interval in 250ns
  *                 increments or 0 to disable.
+ * @is_d3: set if the controller is in d3 state
  */
 struct dwc3 {
 	struct work_struct	drd_work;
@@ -1236,6 +1237,7 @@ struct dwc3 {
 	unsigned		dis_metastability_quirk:1;
 
 	u16			imod_interval;
+	bool			is_d3;
 };
 
 #define INCRX_BURST_MODE 0
@@ -1411,6 +1413,13 @@ static inline bool dwc3_is_usb31(struct dwc3 *dwc)
 {
 	return !!(dwc->revision & DWC3_REVISION_IS_DWC31);
 }
+
+#if IS_ENABLED(CONFIG_USB_DWC3_OF_SIMPLE)
+void dwc3_simple_wakeup_capable(struct device *dev, bool wakeup);
+#else
+void dwc3_simple_wakeup_capable(struct device *dev, bool wakeup)
+{ ; }
+#endif
 
 bool dwc3_has_imod(struct dwc3 *dwc);
 
