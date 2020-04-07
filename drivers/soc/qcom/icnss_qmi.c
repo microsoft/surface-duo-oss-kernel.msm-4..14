b22eabce331029b9749c6b261d2947158b4dc1d1
Naman Padhiar <npadhiar@codeaurora.org>
2020-04-07 15:41:24 +0530

    icnss: Check for modem shutdown on server arrive
    If MODEM_SHUTDOWN flag is set on server arrive return
    without performing any operation.
    
    Change-Id: Ie27f4f3807546c5a36c710e4973d13c808932054
    Signed-off-by: Naman Padhiar <npadhiar@codeaurora.org>

--- drivers/soc/qcom/icnss_qmi.c
+++ drivers/soc/qcom/icnss_qmi.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2017-2019, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2017-2020, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -1200,10 +1200,16 @@ int icnss_clear_server(struct icnss_priv *priv)
 static int wlfw_new_server(struct qmi_handle *qmi,
 			   struct qmi_service *service)
 {
+	struct icnss_priv *priv = container_of(qmi, struct icnss_priv, qmi);
 	struct icnss_event_server_arrive_data *event_data;
 
 	icnss_pr_dbg("WLFW server arrive: node %u port %u\n",
 		     service->node, service->port);
+
+	if (test_bit(ICNSS_MODEM_SHUTDOWN, &priv->state)) {
+		icnss_pr_dbg("WLFW server arrive: Modem is down");
+		return -EINVAL;
+	}
 
 	event_data = kzalloc(sizeof(*event_data), GFP_KERNEL);
 	if (event_data == NULL)
