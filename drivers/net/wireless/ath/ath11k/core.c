3e1cd593e284dcfbbe474233793972391f6f651c
Carl Huang <cjhuang@codeaurora.org>
2020-04-23 16:46:55 +0800

    ath11k: enable internal sleep clock
    On x86 and non-qcom platform, host need explicitly tell firmware to use
    internal sleep clock. Some QCA6390 modules have OTP burnt with external
    sleep clock selected, and these moduesl can't work expectedly unless
    firmware selects internal sleep clock.
    
    Add a field to hw_params to support this difference.
    
    Tested QCA6390 on X86 platform.
    Tested firmware WLAN.HST.1.0.1.c1-00440-QCAHSTSWPLZ_V2_TO_X86-1.
    
    Signed-off-by: Carl Huang <cjhuang@codeaurora.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/ath/ath11k/core.c
+++ drivers/net/wireless/ath/ath11k/core.c
@@ -27,10 +27,12 @@ static const struct ath11k_hw_params ath11k_hw_params_list[] = {
 			.board_size = IPQ8074_MAX_BOARD_DATA_SZ,
 			.cal_size =  IPQ8074_MAX_CAL_DATA_SZ,
 		},
+		.internal_sleep_clock = false,
 	},
 	{
 		.name = "qca6390",
 		.dev_id = ATH11K_HW_QCA6390,
+		.internal_sleep_clock = true,
 	}
 };
 
