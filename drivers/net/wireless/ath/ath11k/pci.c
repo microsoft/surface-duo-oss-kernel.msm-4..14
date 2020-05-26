8ec33245137f317a8454637f71447a7e4f3d64a8
Kalle Valo <kvalo@codeaurora.org>
2020-05-26 11:41:09 +0300

    ath11k: disable double msi enable calls
    Most likely merge error by Govind in "ath11k: Register mhi controller device for qca6x90"
    
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/ath/ath11k/pci.c
+++ drivers/net/wireless/ath/ath11k/pci.c
@@ -1082,7 +1082,6 @@ static int ath11k_pci_probe(struct pci_dev *pdev,
 		goto err_free_core;
 	}
 
-	ret = ath11k_pci_enable_msi(ab_pci);
 	ab->mem = ab_pci->mem;
 	ab->mem_len = ab_pci->mem_len;
 	ret = ath11k_pci_enable_msi(ab_pci);
