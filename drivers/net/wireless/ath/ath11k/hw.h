e39d1a80d2b51d74caa6bfa8fb5672b1e7f5815f
Carl Huang <cjhuang@codeaurora.org>
2020-06-05 17:51:29 +0300

    ath11k: use TCL_DATA_RING_0 for QCA6390
    For QCA6390, wbm2sw1 is used for other purpose rather than
    tx completion ring. So use TCL_DATA_RING 0 only for QCA6390.
    
    Add MISC_CAPS_TCL_0_ONLY to control it.
    
    Tested QCA6390 on X86 platform.
    Tested firmware WLAN.HST.1.0.1.c1-00440-QCAHSTSWPLZ_V2_TO_X86-1.
    
    Signed-off-by: Carl Huang <cjhuang@codeaurora.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/ath/ath11k/hw.h
+++ drivers/net/wireless/ath/ath11k/hw.h
@@ -80,6 +80,7 @@
 #define MISC_CAPS_BAND_TO_MAC           BIT(0)
 #define MISC_CAPS_HOST2FW_RXBUF_RING    BIT(1)
 #define MISC_CAPS_V2_MAP                BIT(2)
+#define MISC_CAPS_TCL_0_ONLY            BIT(3)
 
 enum ath11k_hw_rate_cck {
 	ATH11K_HW_RATE_CCK_LP_11M = 0,
