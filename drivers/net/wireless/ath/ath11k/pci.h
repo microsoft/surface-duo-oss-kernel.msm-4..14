605dd57ed68045e1c35b15f8071a44028cd1b380
Carl Huang <cjhuang@codeaurora.org>
2020-06-16 20:39:37 +0300

    ath11k: reset MHI during power down and power up
    For QCA6390, normal power up an dpower down can't bring MHI
    to a workable state. This happens especially in warm reboot
    and rmmod and insmod. Host needs to write a few registers to
    bring MHI to normal state.
    
    Tested-on: QCA6390 hw2.0 PCI WLAN.HST.1.0.1-01230-QCAHSTSWPLZ_V2_TO_X86-1
    
    Signed-off-by: Carl Huang <cjhuang@codeauroro.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/ath/ath11k/pci.h
+++ drivers/net/wireless/ath/ath11k/pci.h
@@ -24,6 +24,21 @@
 /* FIXME_KVALO: this should be in hw_params */
 /* #define ATH11K_IRQ_CE0_OFFSET 3 */
 
+#define PCIE_SOC_GLOBAL_RESET			0x3008
+#define PCIE_SOC_GLOBAL_RESET_V			1
+
+#define WLAON_WARM_SW_ENTRY			0x1f80504
+#define WLAON_SOC_RESET_CAUSE_REG		0x01f8060c
+
+#define PCIE_Q6_COOKIE_ADDR			0x01F80500
+#define PCIE_Q6_COOKIE_DATA			0xC0000000
+
+/* register to wake the UMAC from power collapse */
+#define PCIE_SCRATCH_0_SOC_PCIE_REG		0x4040
+
+/* register used for handshake mechanism to validate UMAC is awake */
+#define PCIE_SOC_WAKE_PCIE_LOCAL_REG		0x3004
+
 struct ath11k_msi_user {
 	char *name;
 	int num_vectors;
@@ -63,5 +78,7 @@ int ath11k_pci_get_user_msi_assignment(struct ath11k_pci *ar_pci, char *user_nam
 				       int *num_vectors, u32 *user_base_data,
 				       u32 *base_vector);
 int ath11k_pci_get_msi_irq(struct device *dev, unsigned int vector);
+void ath11k_pci_write32(struct ath11k_base *ab, u32 offset, u32 value);
+u32 ath11k_pci_read32(struct ath11k_base *ab, u32 offset);
 
 #endif
