2d2ddcd624f1e29b50785e42fb669c8525b98b3f
Govind Singh <govinds@codeaurora.org>
2020-06-23 14:54:04 +0300

    ath11k: setup pci resource for QCA6x90 target
    Add support for setting up pci region and dma mask
    for QCA6x90 target.
    
    Signed-off-by: Govind Singh <govinds@codeaurora.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

---
+++ drivers/net/wireless/ath/ath11k/pci.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: BSD-3-Clause-Clear */
+/*
+ * Copyright (c) 2019-2020 The Linux Foundation. All rights reserved.
+ */
+
+#define PCI_BAR_NUM			0
+#define PCI_DMA_MASK_64_BIT		64
+#define PCI_DMA_MASK_32_BIT		32
+
+struct ath11k_pci {
+	struct pci_dev *pdev;
+	struct device *dev;
+	struct ath11k_base *ab;
+	void __iomem *mem;
+	size_t mem_len;
+	u16 dev_id;
+	u32 chip_id;
+};
