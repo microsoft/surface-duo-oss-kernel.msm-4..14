83f00977b028aece40416d753c82a8a67f39fe93
Govind Singh <govinds@codeaurora.org>
2020-06-23 14:54:05 +0300

    ath11k: setup resource initialization for QCA6390
    Define CE pipe/qmi config and setup pci irq for the
    same.
    
    Signed-off-by: Govind Singh <govinds@codeaurora.org>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/ath/ath11k/ahb.c
+++ drivers/net/wireless/ath/ath11k/ahb.c
@@ -264,63 +264,6 @@ static const struct service_to_pipe target_service_to_ce_map_wlan[] = {
 	{ /* terminator entry */ }
 };
 
-#define ATH11K_IRQ_CE0_OFFSET 4
-
-static const char *irq_name[ATH11K_IRQ_NUM_MAX] = {
-	"misc-pulse1",
-	"misc-latch",
-	"sw-exception",
-	"watchdog",
-	"ce0",
-	"ce1",
-	"ce2",
-	"ce3",
-	"ce4",
-	"ce5",
-	"ce6",
-	"ce7",
-	"ce8",
-	"ce9",
-	"ce10",
-	"ce11",
-	"host2wbm-desc-feed",
-	"host2reo-re-injection",
-	"host2reo-command",
-	"host2rxdma-monitor-ring3",
-	"host2rxdma-monitor-ring2",
-	"host2rxdma-monitor-ring1",
-	"reo2ost-exception",
-	"wbm2host-rx-release",
-	"reo2host-status",
-	"reo2host-destination-ring4",
-	"reo2host-destination-ring3",
-	"reo2host-destination-ring2",
-	"reo2host-destination-ring1",
-	"rxdma2host-monitor-destination-mac3",
-	"rxdma2host-monitor-destination-mac2",
-	"rxdma2host-monitor-destination-mac1",
-	"ppdu-end-interrupts-mac3",
-	"ppdu-end-interrupts-mac2",
-	"ppdu-end-interrupts-mac1",
-	"rxdma2host-monitor-status-ring-mac3",
-	"rxdma2host-monitor-status-ring-mac2",
-	"rxdma2host-monitor-status-ring-mac1",
-	"host2rxdma-host-buf-ring-mac3",
-	"host2rxdma-host-buf-ring-mac2",
-	"host2rxdma-host-buf-ring-mac1",
-	"rxdma2host-destination-ring-mac3",
-	"rxdma2host-destination-ring-mac2",
-	"rxdma2host-destination-ring-mac1",
-	"host2tcl-input-ring4",
-	"host2tcl-input-ring3",
-	"host2tcl-input-ring2",
-	"host2tcl-input-ring1",
-	"wbm2host-tx-completions-ring3",
-	"wbm2host-tx-completions-ring2",
-	"wbm2host-tx-completions-ring1",
-	"tcl2host-status-ring",
-};
-
 /* enum ext_irq_num - irq numbers that can be used by external modules
  * like datapath
  */
@@ -726,12 +669,12 @@ static int ath11k_ahb_ext_irq_config(struct ath11k_base *ab)
 			int irq_idx = irq_grp->irqs[j];
 
 			irq = platform_get_irq_byname(ab->pdev,
-						      irq_name[irq_idx]);
+						      ath11k_irq_name[irq_idx]);
 			ab->irq_num[irq_idx] = irq;
 			irq_set_status_flags(irq, IRQ_NOAUTOEN | IRQ_DISABLE_UNLAZY);
 			ret = request_irq(irq, ath11k_ahb_ext_interrupt_handler,
 					  IRQF_TRIGGER_RISING,
-					  irq_name[irq_idx], irq_grp);
+					  ath11k_irq_name[irq_idx], irq_grp);
 			if (ret) {
 				ath11k_err(ab, "failed request_irq for %d\n",
 					   irq);
@@ -758,9 +701,9 @@ static int ath11k_ahb_config_irq(struct ath11k_base *ab)
 
 		tasklet_init(&ce_pipe->intr_tq, ath11k_ahb_ce_tasklet,
 			     (unsigned long)ce_pipe);
-		irq = platform_get_irq_byname(ab->pdev, irq_name[irq_idx]);
+		irq = platform_get_irq_byname(ab->pdev, ath11k_irq_name[irq_idx]);
 		ret = request_irq(irq, ath11k_ahb_ce_interrupt_handler,
-				  IRQF_TRIGGER_RISING, irq_name[irq_idx],
+				  IRQF_TRIGGER_RISING, ath11k_irq_name[irq_idx],
 				  ce_pipe);
 		if (ret)
 			return ret;
