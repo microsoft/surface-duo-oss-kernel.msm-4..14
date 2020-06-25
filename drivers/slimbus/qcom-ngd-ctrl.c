55e5e8914f65004fa57725a319ead7478c74606e
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2020-06-25 11:04:47 +0100

    slimbus: ngd: add v2.2.0 compatible
    This patch adds compatible for SLIMBus NGD Controller found on SM8250
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>

--- drivers/slimbus/qcom-ngd-ctrl.c
+++ drivers/slimbus/qcom-ngd-ctrl.c
@@ -1323,6 +1323,9 @@ static const struct of_device_id qcom_slim_ngd_dt_match[] = {
 	},{
 		.compatible = "qcom,slim-ngd-v2.1.0",
 		.data = &ngd_v1_5_offset_info,
+	},{
+		.compatible = "qcom,slim-ngd-v2.2.0",
+		.data = &ngd_v1_5_offset_info,
 	},
 	{}
 };
