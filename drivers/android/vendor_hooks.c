feab21f35e9a6d57336aa8ccb05c4f331128f56a
Bae Soukjin <soukjin.bae@samsung.com>
2020-08-19 08:30:51 +0900

    NOUPSTREAM: ANDROID: vendor_hooks: Add vendor hook to the net
      android_vh_ptype_head:
        To add a debugging chain to ptype list
    
      android_vh_kfree_skb
        To sniff the dropped packet at kernel network
    
    [CPNOTE: 04/08/21] Lee: Vendor hooks
    
    Bug: 163716381
    
    Signed-off-by: Bae Soukjin <soukjin.bae@samsung.com>
    Change-Id: Ide80bf0a129da31a1824d4a33026ac42be327361
    (cherry picked from commit d88b2969cfa78608ad4563e5f4fa84497cc0ffa6)
    (cherry picked from commit a8021ba684c584b8a4361d1680f6e466748ab012)
    Signed-off-by: Matthias Maennich <maennich@google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -31,6 +31,7 @@
 #include <trace/hooks/cgroup.h>
 #include <trace/hooks/sys.h>
 #include <trace/hooks/iommu.h>
+#include <trace/hooks/net.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
@@ -133,6 +134,8 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cgroup_set_task);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_syscall_prctl_finished);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cgroup_attach);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_iommu_setup_dma_ops);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ptype_head);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_kfree_skb);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_check_preempt_tick);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_check_preempt_wakeup_ignore);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_replace_next_task_fair);
