bd1d7de76a543fab98e225d0df385b6e38358d8c
Bogdan Hamciuc <bogdan.hamciuc@nxp.com>
2020-08-27 01:21:59 +0300

    dma:fsl_edma: Fix crash on resume from Suspend to RAM
    FSL_EDMA resume_early function was attempting to write to unmapped area,
    causing an exception on the resume path.
    
    Signed-off-by: Bogdan Hamciuc <bogdan.hamciuc@nxp.com>
    Issue: ALB-5441

--- drivers/dma/fsl-edma.c
+++ drivers/dma/fsl-edma.c
@@ -705,13 +705,17 @@ static int fsl_edma_resume_early(struct device *dev)
 {
 	struct fsl_edma_engine *fsl_edma = dev_get_drvdata(dev);
 	struct fsl_edma_chan *fsl_chan;
-	struct edma_regs *regs = &fsl_edma->regs;
 	int i;
 
 	for (i = 0; i < fsl_edma->n_chans; i++) {
+		struct fsl_edma_hw_tcd *hw_tcd;
+
 		fsl_chan = &fsl_edma->chans[i];
+		hw_tcd = (struct fsl_edma_hw_tcd *)
+			fsl_edma->drvdata->ops->edma_get_tcd_addr(fsl_chan);
+
 		fsl_chan->pm_state = RUNNING;
-		edma_writew(fsl_edma, 0x0, &regs->tcd[i].csr);
+		edma_writew(fsl_edma, 0x0, &hw_tcd->csr);
 		if (fsl_chan->slave_id != 0)
 			fsl_edma_chan_mux(fsl_chan, fsl_chan->slave_id, true);
 	}
