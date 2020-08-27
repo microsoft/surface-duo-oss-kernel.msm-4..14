5932f359aeb78ab1a6e984e075d3c3496fc94f59
Bogdan Hamciuc <bogdan.hamciuc@nxp.com>
2020-08-28 02:02:43 +0300

    nxp:s32gen1:tlb: Workaround for ERR050481
    Replace TLBI variants affected by erratum ERR050481 with broader ones,
    thus restoring correct functionality at the price of performance in some
    use-cases.
    
    Signed-off-by: Bogdan Hamciuc <bogdan.hamciuc@nxp.com>
    Issue: ALB-4879

--- arch/arm64/include/asm/tlbflush.h
+++ arch/arm64/include/asm/tlbflush.h
@@ -4,6 +4,7 @@
  *
  * Copyright (C) 1999-2003 Russell King
  * Copyright (C) 2012 ARM Ltd.
+ * Copyright 2020 NXP
  */
 #ifndef __ASM_TLBFLUSH_H
 #define __ASM_TLBFLUSH_H
@@ -58,6 +59,17 @@
 		__ta |= (unsigned long)(asid) << 48;		\
 		__ta;						\
 	})
+
+#if IS_ENABLED(CONFIG_NXP_S32GEN1_ERRATUM_ERR050481)
+/* The most significant 44 VA bits get encoded as VA[55:12] in TLBI instructions
+ * on A53 cores. Of these, the most significant (44 - (VA_BITS - 12)) are either
+ * all 1 or all 0.
+ * Note: The __TLBI_VADDR(addr) macro has already shifted addr by 12 and masked
+ * by GENMASK_ULL(43, 0).
+ */
+#define S32_IS_KERN_ADDR(addr_shr_12)  \
+	((((long)(addr_shr_12)) >> (VA_BITS - 12)) + 1 == (1 << (56 - VA_BITS)))
+#endif
 
 /*
  *	TLB Invalidation
@@ -160,8 +172,15 @@ static inline void flush_tlb_page_nosync(struct vm_area_struct *vma,
 	unsigned long addr = __TLBI_VADDR(uaddr, ASID(vma->vm_mm));
 
 	dsb(ishst);
-	__tlbi(vale1is, addr);
-	__tlbi_user(vale1is, addr);
+#if IS_ENABLED(CONFIG_NXP_S32GEN1_ERRATUM_ERR050481)
+	if (S32_IS_KERN_ADDR(addr)) {
+		__tlbi(vmalle1is);
+	} else
+#endif
+	{
+		__tlbi(vale1is, addr);
+		__tlbi_user(vale1is, addr);
+	}
 }
 
 static inline void flush_tlb_page(struct vm_area_struct *vma,
@@ -200,6 +219,12 @@ static inline void __flush_tlb_range(struct vm_area_struct *vma,
 
 	dsb(ishst);
 	for (addr = start; addr < end; addr += stride) {
+#if IS_ENABLED(CONFIG_NXP_S32GEN1_ERRATUM_ERR050481)
+		if (S32_IS_KERN_ADDR(addr)) {
+			__tlbi(vmalle1is);
+			break;
+		}
+#endif
 		if (last_level) {
 			__tlbi(vale1is, addr);
 			__tlbi_user(vale1is, addr);
@@ -234,8 +259,15 @@ static inline void flush_tlb_kernel_range(unsigned long start, unsigned long end
 	end = __TLBI_VADDR(end, 0);
 
 	dsb(ishst);
-	for (addr = start; addr < end; addr += 1 << (PAGE_SHIFT - 12))
+	for (addr = start; addr < end; addr += 1 << (PAGE_SHIFT - 12)) {
+#if IS_ENABLED(CONFIG_NXP_S32GEN1_ERRATUM_ERR050481)
+		if (S32_IS_KERN_ADDR(addr)) {
+			__tlbi(vmalle1is);
+			break;
+		}
+#endif
 		__tlbi(vaale1is, addr);
+	}
 	dsb(ish);
 	isb();
 }
@@ -249,7 +281,14 @@ static inline void __flush_tlb_kernel_pgtable(unsigned long kaddr)
 	unsigned long addr = __TLBI_VADDR(kaddr, 0);
 
 	dsb(ishst);
-	__tlbi(vaae1is, addr);
+#if IS_ENABLED(CONFIG_NXP_S32GEN1_ERRATUM_ERR050481)
+	if (S32_IS_KERN_ADDR(addr))
+		__tlbi(vmalle1is);
+	else
+#endif
+	{
+		__tlbi(vaae1is, addr);
+	}
 	dsb(ish);
 	isb();
 }
