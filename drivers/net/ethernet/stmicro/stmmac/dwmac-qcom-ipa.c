9be2a056ba6f3445dc39822cf82d80664c667f46
Suraj Jaiswal <jsuraj@codeaurora.org>
2020-10-14 16:44:25 +0530

    net: stmmac: Loopback fix
    Add support to forward the loopback traffic
    to queue1.
    
    Change-Id: I978b3a27ac2e24c2baede79655e903f1eb43c198
    Signed-off-by: Suraj Jaiswal <jsuraj@codeaurora.org>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-ipa.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-ipa.c
@@ -1516,7 +1516,7 @@ static void ntn_ipa_notify_cb_be(
 			skb->protocol = htons(ETH_P_IP);
 			iph = (struct iphdr *)skb->data;
 		} else {
-			if (ethqos->current_loopback > DISABLE_LOOPBACK)
+			if (pdata->current_loopback > DISABLE_LOOPBACK)
 				swap_ip_port(skb, ETH_P_IP);
 			skb->protocol = eth_type_trans(skb, skb->dev);
 			iph = (struct iphdr *)(skb_mac_header(skb) + ETH_HLEN);
@@ -2543,6 +2543,9 @@ static int ethqos_ipa_offload_resume(struct qcom_ethqos *ethqos)
 	int ret = 1;
 	struct ipa_perf_profile profile;
 	int type;
+	struct platform_device *pdev = ethqos->pdev;
+	struct net_device *dev = platform_get_drvdata(pdev);
+	struct stmmac_priv *priv = netdev_priv(dev);
 
 	ETHQOSDBG("Enter\n");
 
@@ -2596,7 +2599,11 @@ static int ethqos_ipa_offload_resume(struct qcom_ethqos *ethqos)
 			}
 		}
 	}
-
+	if (priv->current_loopback > 0) {
+		priv->hw->mac->map_mtl_to_dma(priv->hw, EMAC_QUEUE_0,
+					      EMAC_CHANNEL_1);
+		ETHQOSINFO("Mapped queue 0 to channel 1 again\n");
+	}
 	ETHQOSDBG("Exit\n");
 
 fail:
@@ -2827,6 +2834,9 @@ void ethqos_ipa_offload_event_handler(void *data,
 				      int ev)
 {
 	int type;
+	struct platform_device *pdev;
+	struct net_device *dev;
+	struct stmmac_priv *priv;
 
 	ETHQOSDBG("Enter: event=%d\n", ev);
 
@@ -3013,6 +3023,18 @@ void ethqos_ipa_offload_event_handler(void *data,
 			*(int *)data = false;
 		}
 
+		break;
+	case EV_LOOPBACK_DMA_MAP:
+
+		pdev = (eth_ipa_ctx.ethqos)->pdev;
+		dev = platform_get_drvdata(pdev);
+		priv = netdev_priv(dev);
+		/* Map queue 0 to channel 1 to forward the loopback
+		 * traffic to channel 1 from queue 0
+		 */
+		priv->hw->mac->map_mtl_to_dma(priv->hw, EMAC_QUEUE_0,
+					      EMAC_CHANNEL_1);
+		ETHQOSINFO("Mapped queue 0 to channel 1\n");
 		break;
 	case EV_INVALID:
 	default:
