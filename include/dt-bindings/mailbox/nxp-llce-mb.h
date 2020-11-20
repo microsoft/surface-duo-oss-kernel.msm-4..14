b04442b276182d4b3a8d33f65c854aa56f28550f
Ghennadi Procopciuc <Ghennadi.Procopciuc@nxp.com>
2020-11-20 10:18:46 +0200

    doc: Add documentation for NXP LLCE Mailbox
    Issue: ALB-5829
    Signed-off-by: Ghennadi Procopciuc <Ghennadi.Procopciuc@nxp.com>

---
+++ include/dt-bindings/mailbox/nxp-llce-mb.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright 2020 NXP
+ */
+#ifndef _DT_BINDINGS_MAILBOX_NXP_LLCE_MB_H_
+#define _DT_BINDINGS_MAILBOX_NXP_LLCE_MB_H_
+
+/*
+ * LLCE Mailbox Type
+ */
+#define S32G274_LLCE_HIF_CONF_MB	0x0
+#define S32G274_LLCE_CAN_CONF_MB	0x1
+#define S32G274_LLCE_CAN_RX_MB		0x2
+#define S32G274_LLCE_CAN_TX_MB		0x3
+
+#endif
