1fc34ebcf7f00c5c7e48b193e41c6cc39f9c1ca4
Shrirang Bagul <shrirang.bagul@canonical.com>
2021-02-22 17:23:05 +0800

    UBUNTU: SAUCE: media: platform: msm: fix build errors for ais_isp trace
      CC      drivers/media/platform/msm/ais/ais_isp/utils/ais_isp_trace.o
    In file included from drivers/media/platform/msm/ais/ais_isp/utils/ais_isp_trace.h:229,
                     from drivers/media/platform/msm/ais/ais_isp/utils/ais_isp_trace.c:16:
    ./include/trace/define_trace.h:89:42: fatal error: ./ais_isp_trace.h: No such file or directory
       89 | #include TRACE_INCLUDE(TRACE_INCLUDE_FILE)
          |                                          ^
    compilation terminated.
    
    Signed-off-by: Shrirang Bagul <shrirang.bagul@canonical.com>

--- drivers/media/platform/msm/ais/ais_isp/utils/ais_isp_trace.h
+++ drivers/media/platform/msm/ais/ais_isp/utils/ais_isp_trace.h
@@ -17,7 +17,7 @@
 #undef TRACE_SYSTEM
 #define TRACE_SYSTEM camera
 #undef TRACE_INCLUDE_PATH
-#define TRACE_INCLUDE_PATH .
+#define TRACE_INCLUDE_PATH ../drivers/media/platform/msm/ais/ais_isp/utils/
 #undef TRACE_INCLUDE_FILE
 #define TRACE_INCLUDE_FILE ais_isp_trace
 
