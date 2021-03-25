37a760b99ae56e915ae4a4f5fad0da829365f3fc
Ghennadi Procopciuc <Ghennadi.Procopciuc@nxp.com>
2021-03-25 14:55:55 +0200

    dt-bindings: s32gen1: Add SerDes subsystem modes
    Issue: ALB-6572
    Signed-off-by: Ghennadi Procopciuc <Ghennadi.Procopciuc@nxp.com>

---
+++ include/dt-bindings/phy/phy-s32gen1-serdes.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright 2021 NXP
+ */
+#ifndef __DT_BINDINGS_PHY_S32GEN1_SERDES_H
+#define __DT_BINDINGS_PHY_S32GEN1_SERDES_H
+
+/* S32GEN1 Serdes Subsystem Mode Selection */
+#define PCIE_GEN3X2_MODE	0
+#define PCIE_XPCS0_MODE		1
+#define PCIE_XPCS1_MODE		2
+#define XPCSX2_MODE		3
+
+#endif
