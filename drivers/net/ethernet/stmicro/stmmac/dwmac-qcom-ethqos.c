7d9dc785cdefba8616d1ef1cd6475344496f4756
Sneh Shah <snehshah@codeaurora.org>
2021-03-25 22:37:14 +0530

    net: stmmac: Ensure to unregister from msm-bus
    Ensure stmmac driver unregisters from msm-bus module
    in case of module exit.
    
    Change-Id: I7aa4bee5c92c1782a42e4bc3ac5b9ab5fbda7d17
    Acked-by: Nagarjuna Chaganti <nchagant@qti.qualcomm.com>
    Signed-off-by: Sneh Shah <snehshah@codeaurora.org>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-ethqos.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-qcom-ethqos.c
@@ -3764,6 +3764,9 @@ static int qcom_ethqos_probe(struct platform_device *pdev)
 err_clk:
 	clk_disable_unprepare(ethqos->rgmii_clk);
 
+	if (ethqos->bus_hdl)
+		msm_bus_scale_unregister_client(ethqos->bus_hdl);
+
 err_mem:
 	stmmac_remove_config_dt(pdev, plat_dat);
 
@@ -3781,6 +3784,9 @@ static int qcom_ethqos_remove(struct platform_device *pdev)
 
 	ret = stmmac_pltfr_remove(pdev);
 	clk_disable_unprepare(ethqos->rgmii_clk);
+
+	if (ethqos->bus_hdl)
+		msm_bus_scale_unregister_client(ethqos->bus_hdl);
 
 	if (phy_intr_en)
 		free_irq(ethqos->phy_intr, ethqos);
