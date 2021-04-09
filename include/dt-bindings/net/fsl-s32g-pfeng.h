3ac7fdbbdeac611b88d006f342d19f5f6f21c80a
Jan Petrous <jan.petrous@nxp.com>
2021-04-09 14:36:56 +0200

    dts: s32g274a: pfe: enumerate HIF modes
    Create HIF mode constants to use in DTS:
     * PFENG_HIF_MODE_EXCLUSIVE
     * PFENG_HIF_MODE_SHARED
    
    Issue: ALB-6503
    Signed-off-by: Jan Petrous <jan.petrous@nxp.com>

---
+++ include/dt-bindings/net/fsl-s32g-pfeng.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright 2021 NXP
+ *
+ */
+
+#ifndef __DT_BINDINGS_NET_FSL_S32G_PFENG_H__
+#define __DT_BINDINGS_NET_FSL_S32G_PFENG_H__
+
+#define PFENG_HIF_MODE_EXCLUSIVE	0
+#define PFENG_HIF_MODE_SHARED		1
+
+#endif /* __DT_BINDINGS_NET_FSL_S32G_PFENG_H__ */
