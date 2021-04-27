c0530758b8e742ce7e23f943b703fcbf0e5ef215
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2021-04-27 09:48:30 +0300

    reboot: s32gen1: Correct the usage of RGM regmap
    This corrects a problem introduced by
    commit deaef88f4682 ("power-reset: Use "fsl,s32gen1-prstc" instead
    		     of "fsl,s32gen1-reset"")
    where of_iomap is replaced by syscon_regmap_lookup_by_compatible
    without adapting read and write accessories.
    
    Issue: ALB-7039
    Signed-off-by: Ghennadi Procopciuc <Ghennadi.Procopciuc@nxp.com>

--- drivers/power/reset/s32gen1-reboot.c
+++ drivers/power/reset/s32gen1-reboot.c
@@ -15,30 +15,14 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <linux/io.h>
-#include <linux/of_device.h>
-#include <linux/of_address.h>
 #include <asm/system_misc.h>
+#include <linux/io.h>
 #include <linux/mfd/syscon.h>
-
-/* MC_ME_CTL */
-#define MC_ME_CTL_KEY(mc_me)		((mc_me) + 0x00000000)
-#define MC_ME_CTL_KEY_KEY		(0x00005AF0)
-#define MC_ME_CTL_KEY_INVERTEDKEY	(0x0000A50F)
-
-/* MC_ME_MODE_CONF */
-#define MC_ME_MODE_CONF(mc_me)		((mc_me) + 0x00000004)
-#define MC_ME_MODE_CONF_FUNC_RST	(0x1 << 1)
-
-/* MC_ME_MODE_UPD */
-#define MC_ME_MODE_UPD(mc_me)		((mc_me) + 0x00000008)
-#define MC_ME_MODE_UPD_UPD		(0x1 << 0)
-
-
-/* MC_RGM_FRET */
-#define MC_RGM_FRET(mc_rgm)		((mc_rgm) + 0x18)
-
-#define MC_RGM_FRET_VALUE		(0xF)
+#include <linux/of_address.h>
+#include <linux/of_device.h>
+#include <linux/regmap.h>
+#include <s32/s32-gen1/mc_me.h>
+#include <s32/s32-gen1/rgm.h>
 
 static const struct of_device_id s32gen1_reboot_of_match[] = {
 	{ .compatible = "fsl,s32gen1-prstc", .data = (void *) 0 },
@@ -46,8 +30,8 @@ static const struct of_device_id s32gen1_reboot_of_match[] = {
 };
 
 struct	s32gen1_reboot_priv {
-	void __iomem *mc_me;
-	void __iomem *mc_rgm;
+	struct regmap *mc_me;
+	struct regmap *mc_rgm;
 };
 
 static struct s32gen1_reboot_priv	s32gen1_reboot_priv = {0};
@@ -57,23 +41,23 @@ static int s32gen1_reboot(struct notifier_block *this, unsigned long mode,
 {
 	unsigned long timeout;
 
-	if (s32gen1_reboot_priv.mc_rgm) {
-		writeb(MC_RGM_FRET_VALUE,
-			MC_RGM_FRET(s32gen1_reboot_priv.mc_rgm));
-	}
+	if (!s32gen1_reboot_priv.mc_rgm || !s32gen1_reboot_priv.mc_me)
+		return 0;
 
-	if (s32gen1_reboot_priv.mc_me) {
-		writel_relaxed(MC_ME_MODE_CONF_FUNC_RST,
-				MC_ME_MODE_CONF(s32gen1_reboot_priv.mc_me));
+	regmap_write(s32gen1_reboot_priv.mc_rgm,
+		     MC_RGM_FRET, MC_RGM_FRET_VALUE);
 
-		writel_relaxed(MC_ME_MODE_UPD_UPD,
-				MC_ME_MODE_UPD(s32gen1_reboot_priv.mc_me));
+	regmap_write(s32gen1_reboot_priv.mc_me,
+		     MC_ME_MODE_CONF, MC_ME_MODE_CONF_FUNC_RST);
 
-		writel_relaxed(MC_ME_CTL_KEY_KEY,
-				MC_ME_CTL_KEY(s32gen1_reboot_priv.mc_me));
-		writel_relaxed(MC_ME_CTL_KEY_INVERTEDKEY,
-				MC_ME_CTL_KEY(s32gen1_reboot_priv.mc_me));
-	}
+	regmap_write(s32gen1_reboot_priv.mc_me,
+		     MC_ME_MODE_UPD, MC_ME_MODE_UPD_UPD);
+
+	regmap_write(s32gen1_reboot_priv.mc_me,
+		     MC_ME_CTL_KEY, MC_ME_CTL_KEY_KEY);
+
+	regmap_write(s32gen1_reboot_priv.mc_me,
+		     MC_ME_CTL_KEY, MC_ME_CTL_KEY_INVERTEDKEY);
 
 	timeout = jiffies + HZ;
 	while (time_before(jiffies, timeout))
@@ -89,33 +73,26 @@ static struct notifier_block s32gen1_reboot_nb = {
 
 static int s32gen1_reboot_probe(struct platform_device *pdev)
 {
-	const struct of_device_id *of;
+	struct device *dev = &pdev->dev;
 	int err;
-
-	of = of_match_device(s32gen1_reboot_of_match, &pdev->dev);
-	if (of == NULL)
-		return -ENODEV;
 
 	s32gen1_reboot_priv.mc_me =
 		syscon_regmap_lookup_by_compatible("fsl,s32gen1-mc_me");
-	if (IS_ERR(s32gen1_reboot_priv.mc_rgm)) {
-		dev_err(&pdev->dev, "Can not map resource\n");
+	if (IS_ERR(s32gen1_reboot_priv.mc_me)) {
+		dev_err(dev, "Cannot map 'MC_ME' resource\n");
 		return -ENODEV;
 	}
 
 	s32gen1_reboot_priv.mc_rgm =
 		syscon_regmap_lookup_by_compatible("fsl,s32gen1-rgm");
 	if (IS_ERR(s32gen1_reboot_priv.mc_rgm)) {
-		iounmap(s32gen1_reboot_priv.mc_me);
-		dev_err(&pdev->dev, "Can not map resource\n");
+		dev_err(dev, "Cannot map 'RGM' resource\n");
 		return -ENODEV;
 	}
 
 	err = register_restart_handler(&s32gen1_reboot_nb);
 	if (err) {
-		iounmap(s32gen1_reboot_priv.mc_rgm);
-		iounmap(s32gen1_reboot_priv.mc_me);
-		dev_err(&pdev->dev, "Failed to register handler\n");
+		dev_err(dev, "Failed to register handler\n");
 		return err;
 	}
 
