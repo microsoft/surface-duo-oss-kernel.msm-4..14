53533f14d8ef344f1e6fd4045642b98c7ba3b3c8
Larisa Grigore <larisa.grigore@nxp.com>
2021-05-27 15:25:22 +0300

    serial: linflex: Clean SLEEP bit in LINCR1 after suspend
    When coming back from reset, we need to re-initialize LINCR1
    register. SLEEP bit should be cleared, otherwise we can't enter
    INIT mode.
    
    Issue: ALB-7139
    Signed-off-by: Larisa Grigore <larisa.grigore@nxp.com>

--- drivers/tty/serial/fsl_linflexuart.c
+++ drivers/tty/serial/fsl_linflexuart.c
@@ -971,8 +971,7 @@ linflex_set_termios(struct uart_port *port, struct ktermios *termios,
 	writel(old_cr, sport->port.membase + UARTCR);
 
 	/* Enter initialization mode by setting INIT bit */
-	cr1 = readl(sport->port.membase + LINCR1);
-	cr1 |= LINFLEXD_LINCR1_INIT;
+	cr1 = LINFLEXD_LINCR1_INIT | LINFLEXD_LINCR1_MME;
 	writel(cr1, sport->port.membase + LINCR1);
 
 	/* wait for init mode entry */
