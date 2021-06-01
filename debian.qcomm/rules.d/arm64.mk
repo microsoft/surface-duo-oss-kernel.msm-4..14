0bb45badce16a6021fb6745afe2e045577cf42e6
Shrirang Bagul <shrirang.bagul@canonical.com>
2021-06-01 08:52:52 +0800

    UBUNTU: [Packaging] generate linux-libc-dev package
    BugLink: https://bugs.launchpad.net/bugs/1929095
    
    Signed-off-by: Shrirang Bagul <shrirang.bagul@canonical.com>

--- debian.qcomm/rules.d/arm64.mk
+++ debian.qcomm/rules.d/arm64.mk
@@ -21,7 +21,7 @@ do_common_headers_indep=false
 
 do_dtbs		= true
 disable_d_i = true
-do_libc_dev_package=false
+do_libc_dev_package=true
 do_doc_package	= false
 do_source_package= false
 do_dkms_wireguard = true
--- debian/rules.d/2-binary-arch.mk
+++ debian/rules.d/2-binary-arch.mk
@@ -562,7 +562,7 @@ binary-arch-headers: install-arch-headers
 	dh_testdir
 	dh_testroot
 ifeq ($(do_libc_dev_package),true)
-ifneq ($(DEBIAN),debian.master)
+ifeq ($(filter debian.master debian.qcomm,$(DEBIAN)),)
 	echo "non-master branch building linux-libc-dev, aborting"
 	exit 1
 endif
